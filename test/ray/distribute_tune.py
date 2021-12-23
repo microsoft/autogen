300f28666716e659bc9e4b457ad00e92fd4ec80e
Chi Wang <wang.chi@microsoft.com>
2021-12-23 13:37:07 -0800

    azureml + ray (#344)
    * examples and documentation about how to use azureml + ray

---
+++ test/ray/distribute_tune.py
@@ -0,0 +1,49 @@
+import ray
+import lightgbm as lgb
+import numpy as np
+import sklearn.datasets
+import sklearn.metrics
+from sklearn.model_selection import train_test_split
+from flaml import tune
+from flaml.model import LGBMEstimator
+
+data, target = sklearn.datasets.load_breast_cancer(return_X_y=True)
+train_x, test_x, train_y, test_y = train_test_split(data, target, test_size=0.25)
+
+
+def train_breast_cancer(config):
+    params = LGBMEstimator(**config).params
+    num_boost_round = params.pop("n_estimators")
+    train_set = lgb.Dataset(train_x, label=train_y)
+    gbm = lgb.train(params, train_set, num_boost_round)
+    preds = gbm.predict(test_x)
+    pred_labels = np.rint(preds)
+    tune.report(
+        mean_accuracy=sklearn.metrics.accuracy_score(test_y, pred_labels), done=True
+    )
+
+
+if __name__ == "__main__":
+    ray.init(address="auto")
+    flaml_lgbm_search_space = LGBMEstimator.search_space(train_x.shape)
+    config_search_space = {
+        hp: space["domain"] for hp, space in flaml_lgbm_search_space.items()
+    }
+    low_cost_partial_config = {
+        hp: space["low_cost_init_value"]
+        for hp, space in flaml_lgbm_search_space.items()
+        if "low_cost_init_value" in space
+    }
+
+    analysis = tune.run(
+        train_breast_cancer,
+        metric="mean_accuracy",
+        mode="max",
+        config=config_search_space,
+        num_samples=-1,
+        time_budget_s=60,
+        use_ray=True,
+    )
+
+    # print("Best hyperparameters found were: ", analysis.best_config)
+    print("The best trial's result: ", analysis.best_trial.last_result)
--- website/docs/Use-Cases/Task-Oriented-AutoML.md
+++ website/docs/Use-Cases/Task-Oriented-AutoML.md
@@ -467,9 +467,9 @@ The curve suggests that increasing the time budget may further improve the accur
 1. set t1 as the time budget, and check the message in the console log in the end. If the budget is too small, you will see a warning like
 > WARNING - Time taken to find the best model is 91% of the provided time budget and not all estimators' hyperparameter search converged. Consider increasing the time budget.
 2. set t2 as the time budget, and also set `early_stop=True`. If the early stopping is triggered, you will see a warning like
-    > WARNING - All estimator hyperparameters local search has converged at least once, and the total search time exceeds 10 times the time taken to find the best model.
+> WARNING - All estimator hyperparameters local search has converged at least once, and the total search time exceeds 10 times the time taken to find the best model.
 
-    > WARNING - Stopping search as early_stop is set to True.
+> WARNING - Stopping search as early_stop is set to True.
 
 ### How much time is needed to find the best model
 
