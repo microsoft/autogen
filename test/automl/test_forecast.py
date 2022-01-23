6a7caa6a3d06b1d8253b250471eaa0d62c4d6df6
Chi Wang <wang.chi@microsoft.com>
2022-01-23 01:24:15 -0800

    max_iter < 2 -> no search; sign in metric constraints; test and example for forecasting (#415)
    * max_iter < 2 -> no search
    
    * use_ray in test
    
    * eval_method in ts example
    
    * check sign of constraints
    
    * test metric constraint sign

--- test/automl/test_forecast.py
+++ test/automl/test_forecast.py
@@ -95,34 +95,30 @@ def test_numpy():
     X_train = np.arange("2014-01", "2021-01", dtype="datetime64[M]")
     y_train = np.random.random(size=len(X_train))
     automl = AutoML()
-    try:
-        import prophet
+    automl.fit(
+        X_train=X_train[:72],  # a single column of timestamp
+        y_train=y_train[:72],  # value for each timestamp
+        period=12,  # time horizon to forecast, e.g., 12 months
+        task="ts_forecast",
+        time_budget=3,  # time budget in seconds
+        log_file_name="test/ts_forecast.log",
+        n_splits=3,  # number of splits
+    )
+    print(automl.predict(X_train[72:]))
 
-        automl.fit(
-            X_train=X_train[:72],  # a single column of timestamp
-            y_train=y_train[:72],  # value for each timestamp
-            period=12,  # time horizon to forecast, e.g., 12 months
-            task="ts_forecast",
-            time_budget=3,  # time budget in seconds
-            log_file_name="test/ts_forecast.log",
-            n_splits=3,  # number of splits
-        )
-        print(automl.predict(X_train[72:]))
-    except ImportError:
-        print("not using prophet due to ImportError")
-        automl = AutoML()
-        automl.fit(
-            X_train=X_train[:72],  # a single column of timestamp
-            y_train=y_train[:72],  # value for each timestamp
-            period=12,  # time horizon to forecast, e.g., 12 months
-            task="ts_forecast",
-            time_budget=1,  # time budget in seconds
-            estimator_list=["arima", "sarimax"],
-            log_file_name="test/ts_forecast.log",
-        )
-        print(automl.predict(X_train[72:]))
-        # an alternative way to specify predict steps for arima/sarimax
-        print(automl.predict(12))
+    automl = AutoML()
+    automl.fit(
+        X_train=X_train[:72],  # a single column of timestamp
+        y_train=y_train[:72],  # value for each timestamp
+        period=12,  # time horizon to forecast, e.g., 12 months
+        task="ts_forecast",
+        time_budget=1,  # time budget in seconds
+        estimator_list=["arima", "sarimax"],
+        log_file_name="test/ts_forecast.log",
+    )
+    print(automl.predict(X_train[72:]))
+    # an alternative way to specify predict steps for arima/sarimax
+    print(automl.predict(12))
 
 
 def load_multi_dataset():
