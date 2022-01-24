4814091d87ce00c8c231fc94ebd1c4c2f18c880b
Xueqing Liu <liususan091219@users.noreply.github.com>
2022-01-24 17:24:14 -0500

    remove redundant imports (#426)
    * remove redundant imports
    
    * getting ride of hf dataset

--- test/nlp/test_autohf_classificationhead.py
+++ test/nlp/test_autohf_classificationhead.py
@@ -1,17 +1,36 @@
 def test_classification_head():
     from flaml import AutoML
-    import requests
-    from datasets import load_dataset
+    import pandas as pd
 
-    try:
-        train_dataset = (
-            load_dataset("emotion", split="train[:1%]").to_pandas().iloc[0:10]
-        )
-        dev_dataset = (
-            load_dataset("emotion", split="train[1%:2%]").to_pandas().iloc[0:10]
-        )
-    except requests.exceptions.ConnectionError:
-        return
+    train_data = {
+        "text": [
+            "i didnt feel humiliated",
+            "i can go from feeling so hopeless to so damned hopeful just from being around someone who cares and is awake",
+            "im grabbing a minute to post i feel greedy wrong",
+            "i am ever feeling nostalgic about the fireplace i will know that it is still on the property",
+            "i am feeling grouchy",
+            "ive been feeling a little burdened lately wasnt sure why that was",
+            "ive been taking or milligrams or times recommended amount and ive fallen asleep a lot faster but i also feel like so funny",
+            "i feel as confused about life as a teenager or as jaded as a year old man",
+            "i have been with petronas for years i feel that petronas has performed well and made a huge profit",
+            "i feel romantic too",
+            "i feel like i have to make the suffering i m seeing mean something",
+            "i do feel that running is a divine experience and that i can expect to have some type of spiritual encounter",
+        ],
+        "label": [0, 0, 3, 2, 3, 0, 5, 4, 1, 2, 0, 1],
+    }
+    train_dataset = pd.DataFrame(train_data)
+
+    dev_data = {
+        "text": [
+            "i think it s the easiest time of year to feel dissatisfied",
+            "i feel low energy i m just thirsty",
+            "i have immense sympathy with the general point but as a possible proto writer trying to find time to write in the corners of life and with no sign of an agent let alone a publishing contract this feels a little precious",
+            "i do not feel reassured anxiety is on each side",
+        ],
+        "label": [3, 0, 1, 1],
+    }
+    dev_dataset = pd.DataFrame(dev_data)
 
     custom_sent_keys = ["text"]
     label_key = "label"
--- test/nlp/test_autohf_cv.py
+++ test/nlp/test_autohf_cv.py
@@ -5,15 +5,25 @@ import pytest
 @pytest.mark.skipif(sys.platform == "darwin", reason="do not run on mac os")
 def test_cv():
     from flaml import AutoML
-    import requests
-    from datasets import load_dataset
+    import pandas as pd
 
-    try:
-        train_dataset = (
-            load_dataset("glue", "mrpc", split="train[:1%]").to_pandas().iloc[0:4]
-        )
-    except requests.exceptions.ConnectionError:
-        return
+    train_data = {
+        "sentence1": [
+            'Amrozi accused his brother , whom he called " the witness " , of deliberately distorting his evidence .',
+            "Yucaipa owned Dominick 's before selling the chain to Safeway in 1998 for $ 2.5 billion .",
+            "They had published an advertisement on the Internet on June 10 , offering the cargo for sale , he added .",
+            "Around 0335 GMT , Tab shares were up 19 cents , or 4.4 % , at A $ 4.56 , having earlier set a record high of A $ 4.57 .",
+        ],
+        "sentence2": [
+            'Referring to him as only " the witness " , Amrozi accused his brother of deliberately distorting his evidence .',
+            "Yucaipa bought Dominick 's in 1995 for $ 693 million and sold it to Safeway for $ 1.8 billion in 1998 .",
+            "On June 10 , the ship 's owners had published an advertisement on the Internet , offering the explosives for sale .",
+            "Tab shares jumped 20 cents , or 4.6 % , to set a record closing high at A $ 4.57 .",
+        ],
+        "label": [1, 0, 1, 0],
+        "idx": [0, 1, 2, 3],
+    }
+    train_dataset = pd.DataFrame(train_data)
 
     custom_sent_keys = ["sentence1", "sentence2"]
     label_key = "label"
