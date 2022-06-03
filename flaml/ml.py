e0e317bfb1bef6dbaa267ffd6c37f4ecd4f502b1
Xueqing Liu <liususan091219@users.noreply.github.com>
2022-06-03 15:19:22 -0400

    fixing trainable and update function, completing NOTE (#566)
    * fix checkpoint naming + trial id for non-ray mode, fix the bug in running test mode, delete all the checkpoints in non-ray mode
    
    * finished testing for checkpoint naming, delete checkpoint, ray, max iter = 1

--- flaml/ml.py
+++ flaml/ml.py
@@ -420,7 +420,6 @@ def get_val_loss(
     #     fit_kwargs['groups_val'] = groups_val
     #     fit_kwargs['X_val'] = X_val
     #     fit_kwargs['y_val'] = y_val
-
     estimator.fit(X_train, y_train, budget, **fit_kwargs)
     val_loss, metric_for_logging, pred_time, _ = _eval_estimator(
         config,
