e3aa7ea9d13082a615169bf434043c576d193bbf
skzhang1 <shaokunzhang529@gmail.com>
2022-08-21 12:52:27 +0000

    update

--- flaml/ml.py
+++ flaml/ml.py
@@ -541,7 +541,6 @@ def evaluate_model_CV(
         total_fold_num += 1
         val_loss_folds.append(val_loss_i)
         if log_training_metric or not isinstance(eval_metric, str):
-            if isinstance(metric_i, dict):
                 log_metric_folds.append(metric_i)
         train_time += train_time_i
         pred_time += pred_time_i
