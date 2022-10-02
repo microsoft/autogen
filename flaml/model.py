0df54ec3f3ec08cd2ae17dea66f1bf670e0c906a
Vijaya Lakshmi Venkatraman <vijaya-lakshmi.venkatraman@socgen.com>
2022-10-02 05:55:53 +0530

    Update model.py (#739)
    * Update model.py
    
    * Fix review comment

--- flaml/model.py
+++ flaml/model.py
@@ -2287,7 +2287,7 @@ class TemporalFusionTransformerEstimator(SKLearnEstimator):
             kwargs.get("log_dir", "lightning_logs")
         )  # logging results to a tensorboard
         default_trainer_kwargs = dict(
-            gpus=self._kwargs.get("gpu_per_trial", [0])
+            gpus=kwargs.get("gpu_per_trial", [0])
             if torch.cuda.is_available()
             else None,
             max_epochs=max_epochs,
