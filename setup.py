8c6d60807d23f26055482246600ec5a0b45c1691
Li Jiang <lijiang1@microsoft.com>
2023-02-18 00:38:55 +0800

    Added extras for synapse (#916)
    * Added extras for synapse
    
    * Update Installation doc

--- setup.py
+++ setup.py
@@ -114,6 +114,7 @@ setuptools.setup(
         ],
         "benchmark": ["catboost>=0.26", "psutil==5.8.0", "xgboost==1.3.3"],
         "openai": ["openai==0.23.1", "diskcache", "optuna==2.8.0"],
+        "synapse": ["joblibspark>=0.5.0", "optuna==2.8.0", "pyspark>=3.0.0"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
