2e43509690988e306206ebff6a9063e3bb2cddb3
Qingyun Wu <qingyun.wu@psu.edu>
2023-05-15 20:37:38 -0400

    Human agent (#1025)
    * add human agent and chat agent
    
    * feedback msg
    
    * clean print
    
    * remove redundant import
    
    * make coding agent work
    
    * import check
    
    * terminate condition
    
    * rename
    
    * add docstr
    
    * exitcode to str
    
    * print
    
    * save and execute code
    
    * add max_turn_num
    
    * add max_turn_num in test_agent.py
    
    * reduce max_turn_num in the test
    
    * change max_turn_num to max_consecutive_auto_reply
    
    * update human proxy agent
    
    * remove execution agent and dated docstr
    
    * clean doc
    
    * add back work_dir
    
    * add is_termination_msg when mode is NEVER
    
    * revise stop condition
    
    * remove work_dir in coding agent
    
    * human_proxy_agent docstr
    
    * auto_reply
    
    * clean auto_reply
    
    ---------
    
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>

--- flaml/autogen/agent/coding_agent.py
+++ flaml/autogen/agent/coding_agent.py
@@ -1,24 +1,28 @@
 from .agent import Agent
-from .execution_agent import ExecutionAgent
-from flaml.autogen.code_utils import generate_code, DEFAULT_MODEL
+from flaml.autogen.code_utils import DEFAULT_MODEL
 from flaml import oai
 
 
 class PythonAgent(Agent):
     """(Experimental) Suggest code blocks."""
 
-    DEFAULT_SYSTEM_MESSAGE = """You are a coding agent. You suggest python code for a user to execute for a given task. Don't suggest shell command. Output the code in a coding block. Check the execution result. If the result indicates there is an error, fix the error and output the code again.
+    DEFAULT_SYSTEM_MESSAGE = """You suggest python code (in a python coding block) for a user to execute for a given task. If you want the user to save the code in a file before executing it, put # filename: <filename> inside the code block as the first line. Finish the task smartly. Don't suggest shell command. Don't include multiple code blocks in one response. Use 'print' function for the output when relevant. Check the execution result returned by the user.
+    If the result indicates there is an error, fix the error and output the code again.
+    Reply "TERMINATE" in the end when the task is done.
     """
 
     DEFAULT_CONFIG = {
         "model": DEFAULT_MODEL,
     }
-    EXECUTION_AGENT_PREFIX = "execution_agent4"
-    SUCCESS_EXIT_CODE = "exitcode: 0\n"
 
-    def __init__(self, name, system_message=DEFAULT_SYSTEM_MESSAGE, work_dir=None, **config):
+    def __init__(self, name, system_message=DEFAULT_SYSTEM_MESSAGE, **config):
+        """
+        Args:
+            name (str): agent name
+            system_message (str): system message to be sent to the agent
+            config (dict): other configurations.
+        """
         super().__init__(name, system_message)
-        self._work_dir = work_dir
         self._config = self.DEFAULT_CONFIG.copy()
         self._config.update(config)
         self._sender_dict = {}
@@ -28,26 +32,10 @@ class PythonAgent(Agent):
             self._sender_dict[sender.name] = sender
             self._conversations[sender.name] = [{"content": self._system_message, "role": "system"}]
         super().receive(message, sender)
-        if sender.name.startswith(self.EXECUTION_AGENT_PREFIX) and message.startswith(self.SUCCESS_EXIT_CODE):
-            # the code is correct, respond to the original sender
-            name = sender.name[len(self.EXECUTION_AGENT_PREFIX) :]
-            original_sender = self._sender_dict[name]
-            output = message[len(self.SUCCESS_EXIT_CODE) :]
-            if output:
-                self._send(f"{output}", original_sender)
-            else:
-                self._send("Done. No output.", original_sender)
-            return
         responses = oai.ChatCompletion.create(messages=self._conversations[sender.name], **self._config)
-        # cost = oai.ChatCompletion.cost(responses)
         response = oai.ChatCompletion.extract_text(responses)[0]
-        if sender.name.startswith(self.EXECUTION_AGENT_PREFIX):
-            execution_agent = sender
-        else:
-            # create an execution agent
-            execution_agent = ExecutionAgent(f"{self.EXECUTION_AGENT_PREFIX}{sender.name}", work_dir=self._work_dir)
-            # initialize the conversation
-            self._conversations[execution_agent.name] = self._conversations[sender.name].copy()
-            self._sender_dict[execution_agent.name] = execution_agent
-        # send the response to the execution agent
-        self._send(response, execution_agent)
+        self._send(response, sender)
+
+    def reset(self):
+        self._sender_dict.clear()
+        self._conversations.clear()
