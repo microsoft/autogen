c0db556cc2ba5964f0840661b73df5e800117e85
Kosta Petan <kostapetan@gmail.com>
2023-06-15 12:34:32 +0000

    add envvars in all projects

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -4,4 +4,5 @@ curl -k https://localhost:8081/_explorer/emulator.pem > ~/emulatorcert.crt
 sudo cp ~/emulatorcert.crt /usr/local/share/ca-certificates/
 sudo update-ca-certificates
 sleep 10
+dotnet restore sk-dev-team.sln
 dotnet build util/seed-memory/seed-memory.csproj && dotnet util/seed-memory/bin/Debug/net7.0/seed-memory.dll
