7dd9831857226b50d1dfb5db96616938e4a3dad2
Chi Wang <wang.chi@microsoft.com>
2023-09-21 15:20:10 +0000

    cleanup

--- test/test_notebook.py
+++ test/test_notebook.py
@@ -56,7 +56,7 @@ def test_agentchat_auto_feedback_from_code(save=False):
     skip or not sys.version.startswith("3.10"),
     reason="do not run if openai is not installed or py!=3.10",
 )
-def test_openai_completion(save=False):
+def test_oai_completion(save=False):
     run_notebook("oai_completion.ipynb", save=save)
 
 
@@ -80,13 +80,13 @@ def test_agentchat_MathChat(save=False):
     skip or not sys.version.startswith("3.11"),
     reason="do not run if openai is not installed or py!=3.11",
 )
-def test_chatgpt_gpt4(save=False):
+def test_oai_chatgpt_gpt4(save=False):
     run_notebook("oai_chatgpt_gpt4.ipynb", save=save)
 
 
 if __name__ == "__main__":
     test_agentchat_auto_feedback_from_code(save=True)
-    # test_chatgpt_gpt4(save=True)
-    # test_openai_completion(save=True)
+    # test_oai_chatgpt_gpt4(save=True)
+    # test_oai_completion(save=True)
     # test_agentchat_MathChat(save=True)
     # test_agentchat_function_call(save=True)
