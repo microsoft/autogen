fa033c0e6153e51eeb1a21d732dac18fa073cb85
rickyloynd-microsoft <riloynd@microsoft.com>
2023-10-03 12:58:47 -0700

    Add teachability option to setup.py

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setuptools.setup(
         "blendsearch": ["flaml[blendsearch]"],
         "mathchat": ["sympy", "pydantic==1.10.9", "wolframalpha"],
         "retrievechat": ["chromadb", "tiktoken", "sentence_transformers", "pypdf"],
+        "teachability": ["chromadb"],
     },
     classifiers=[
         "Programming Language :: Python :: 3",
