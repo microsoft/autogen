165d5cef8c34da81f52b84a250048bbd87476f05
rickyloynd-microsoft <riloynd@microsoft.com>
2023-10-03 12:59:48 -0700

    Modify test to use OAI_CONFIG_LIST as suggested in the docs.

--- test/agentchat/test_teachable_agent.py
+++ test/agentchat/test_teachable_agent.py
@@ -1,6 +1,14 @@
-from autogen import UserProxyAgent
+from autogen import UserProxyAgent, config_list_from_json
 from autogen.agentchat.contrib.teachable_agent import TeachableAgent
 
-assistant = TeachableAgent("assistant")
+# Load LLM inference endpoints from an env variable or a file
+# See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
+# and OAI_CONFIG_LIST_sample
+config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
+
+# Create the agents.
+assistant = TeachableAgent("assistant", llm_config={"config_list": config_list})
 user_proxy = UserProxyAgent("user_proxy")
+
+# Start the chat.
 user_proxy.initiate_chat(assistant, message="Hi")
