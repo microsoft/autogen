6176792764be18a7de38af08e15f6b0a044c0f4d
XiaoYun Zhang <xiaoyuz@microsoft.com>
2023-10-04 01:49:04 -0700

    update groupchat with role_play self correctness

--- autogen/agentchat/groupchat.py
+++ autogen/agentchat/groupchat.py
@@ -3,61 +3,105 @@ import sys
 from typing import Dict, List, Optional, Union
 from .agent import Agent
 from .conversable_agent import ConversableAgent
-
-
+from .. import oai
+import random
 @dataclass
 class GroupChat:
     """A group chat class that contains a list of agents and the maximum number of rounds."""
-
-    agents: List[Agent]
+    agents: List[ConversableAgent]
     messages: List[Dict]
     max_round: int = 10
-    admin_name: str = "Admin"  # the name of the admin agent
+    llm_config: Optional[Dict] = None
+    admin_name: Optional[str] = None
+
+    @property
+    def admin(self):
+        '''
+        if admin_name is None, then return the first agent in the list
+        otherwise, return the agent with the name admin_name
+        '''
+        if self.admin_name is None:
+            return self.agents[0]
+        else:
+            return self.agent_by_name(self.admin_name)
 
     @property
     def agent_names(self) -> List[str]:
         """Return the names of the agents in the group chat."""
-        return [agent.name for agent in self.agents]
+        return [agent.name.lower() for agent in self.agents]
 
     def reset(self):
         """Reset the group chat."""
         self.messages.clear()
 
+    def process_role_play_msgs(self, messages: List[Dict]) -> List[Dict]:
+        return [{
+            "role": "user",
+            "content": f'''From {message["name"]}:
+{message["content"]}''',
+        } for message in messages]
+    
     def agent_by_name(self, name: str) -> Agent:
         """Find the next speaker based on the message."""
-        return self.agents[self.agent_names.index(name)]
+        return self.agents[self.agent_names.index(name.lower())]
 
     def next_agent(self, agent: Agent) -> Agent:
         """Return the next agent in the list."""
-        return self.agents[(self.agent_names.index(agent.name) + 1) % len(self.agents)]
+        return self.agents[(self.agent_names.index(agent.name.lower()) + 1) % len(self.agents)]
 
-    def select_speaker_msg(self):
+    def select_speaker_msgs(self) -> List[Dict]:
         """Return the message for selecting the next speaker."""
-        return f"""You are in a role play game. The following roles are available:
-{self._participant_roles()}.
+        msgs = [
+            {
+                "role": "system",
+                "content": f"You are in a role play game. Each conversation must start with 'From {{name}}:', e.g: From admin: //your message//.",
+            }
+        ]
 
-Read the following conversation.
-Then select the next role from {self.agent_names} to play. Only return the role."""
+#         # process role information
+#         # each agent introduce the next agent
+#         for i in range(len(self.agents)):
+#             current_agent = self.agents[i]
+#             next_agent = self.next_agent(current_agent)
+#             msgs.append({
+#                 "role": "user",
+#                 "content": f'''From {current_agent.name}:
+# {next_agent.name}, {next_agent.system_message}''',
+#             })
 
-    def select_speaker(self, last_speaker: Agent, selector: ConversableAgent):
+        return msgs
+
+    def select_speaker(self, last_speaker: Agent):
         """Select the next speaker."""
-        selector.update_system_message(self.select_speaker_msg())
-        final, name = selector.generate_oai_reply(
-            self.messages
-            + [
-                {
-                    "role": "system",
-                    "content": f"Read the above conversation. Then select the next role from {self.agent_names} to play. Only return the role.",
-                }
-            ]
-        )
-        if not final:
-            # i = self._random.randint(0, len(self._agent_names) - 1)  # randomly pick an id
+        llm_config = self.llm_config
+
+        # if self.llm_config is None, randomly select 
+        if llm_config is None:
+            # search through its agents and randomly select a llm_config from one of them if it exists
+            # shuffle the agents
+            llm_configs = [agent.llm_config.copy() for agent in self.agents if isinstance(agent.llm_config, dict)]
+            if len(llm_configs) > 0:
+                llm_config = random.choice(llm_configs)
+            else:
+                llm_config = None
+        
+        # if llm_config is still None, then return the next agent
+        if llm_config is None:
             return self.next_agent(last_speaker)
+        
+        system_messages = self.select_speaker_msgs()
+        chat_messages = self.process_role_play_msgs(self.messages)
+        llm_config["stop"] = [':']
+        msgs = system_messages + chat_messages
+        reply = oai.ChatCompletion.create(
+            messages=msgs, **llm_config
+        )
+        msg = reply['choices'][0]['message']['content']
+        name = msg.split(":")[0].split("From ")[1]
         try:
             return self.agent_by_name(name)
         except ValueError:
-            return self.next_agent(last_speaker)
+            return self.admin
 
     def _participant_roles(self):
         return "\n".join([f"{agent.name}: {agent.system_message}" for agent in self.agents])
@@ -84,8 +128,27 @@ class GroupChatManager(ConversableAgent):
             system_message=system_message,
             **kwargs,
         )
+        self.groupchat = groupchat
         self.register_reply(Agent, GroupChatManager.run_chat, config=groupchat, reset_config=GroupChat.reset)
         # self._random = random.Random(seed)
+
+    def _process_received_message(self, message, sender, silent):
+        super()._process_received_message(message, sender, silent)
+        msg = {
+            "content": message,
+            "name": sender.name if sender is not None else "Unknown",
+        }
+        self.groupchat.messages.append(msg)
+
+        # distribute the message to all agents
+        msg_with_name = {
+            'content': f'''From {msg["name"]}<eof_name>:
+{msg["content"]}''',
+            'role': 'user',
+        }
+        for agent in self.groupchat.agents:
+            if agent != sender:
+                self.send(msg_with_name, agent, request_reply=False, silent=True)
 
     def run_chat(
         self,
@@ -98,24 +161,38 @@ class GroupChatManager(ConversableAgent):
             messages = self._oai_messages[sender]
         message = messages[-1]
         speaker = sender
-        groupchat = config
+        groupchat = self.groupchat
         for i in range(groupchat.max_round):
             # set the name to speaker's name if the role is not function
             if message["role"] != "function":
                 message["name"] = speaker.name
-            groupchat.messages.append(message)
-            # broadcast the message to all agents except the speaker
-            for agent in groupchat.agents:
-                if agent != speaker:
-                    self.send(message, agent, request_reply=False, silent=True)
+            # self._process_received_message(message, speaker, silent=True)
             if i == groupchat.max_round - 1:
                 # the last round
                 break
             try:
                 # select the next speaker
-                speaker = groupchat.select_speaker(speaker, self)
+                speaker = groupchat.select_speaker(speaker)
+
+                # add <eof_name>: as stop sequence if llm_config is not None
+                if isinstance(speaker, ConversableAgent) and isinstance(speaker.llm_config, dict):
+                    if "stop" in speaker.llm_config:
+                        speaker.llm_config["stop"].append("<eof_name>:")
+                    else:
+                        speaker.llm_config["stop"] = ["<eof_name>:"]
                 # let the speaker speak
                 reply = speaker.generate_reply(sender=self)
+                # restore the stop sequence
+                if isinstance(speaker, ConversableAgent) and isinstance(speaker.llm_config, dict):
+                    if "stop" in speaker.llm_config:
+                        speaker.llm_config["stop"].remove("<eof_name>:")
+                # if reply is 'From xxx', then set reply to xxx, it's your turn to speak
+                if reply.startswith("From ") and reply.split("From ")[1].lower() in groupchat.agent_names:
+                    name = reply.split("From ")[1]
+                    if name.lower() == speaker.name.lower():
+                        agents_except_speaker = [agent for agent in groupchat.agents if agent != speaker]
+                        speaker = random.choice(agents_except_speaker)
+                    reply = f"{name}, it's your turn to speak."
             except KeyboardInterrupt:
                 # let the admin agent speak if interrupted
                 if groupchat.admin_name in groupchat.agent_names:
--- test/agentchat/test_groupchat.py
+++ test/agentchat/test_groupchat.py
@@ -1,35 +1,123 @@
 import autogen
+import random
 
 
-def test_chat_manager():
-    agent1 = autogen.ConversableAgent(
+def test_group_chat_when_group_chat_llm_config_is_none():
+    '''
+    Test group chat when groupchat's llm config is None.
+    In this case, the group chat manager will simply select the next agent.
+    '''
+    alice = autogen.ConversableAgent(
         "alice",
         max_consecutive_auto_reply=2,
         human_input_mode="NEVER",
         llm_config=False,
         default_auto_reply="This is alice sepaking.",
     )
-    agent2 = autogen.ConversableAgent(
+    bob = autogen.ConversableAgent(
         "bob",
         max_consecutive_auto_reply=2,
         human_input_mode="NEVER",
         llm_config=False,
         default_auto_reply="This is bob speaking.",
     )
-    groupchat = autogen.GroupChat(agents=[agent1, agent2], messages=[], max_round=2)
+    groupchat = autogen.GroupChat(
+        agents=[alice, bob],
+        messages=[],
+        max_round=2)
     group_chat_manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=False)
-    agent1.initiate_chat(group_chat_manager, message="hello")
+    alice.initiate_chat(group_chat_manager, message="hello")
 
-    assert len(agent1.chat_messages[group_chat_manager]) == 2
     assert len(groupchat.messages) == 2
+    assert len(alice.chat_messages[group_chat_manager]) == 2
+
+    assert alice.chat_messages[group_chat_manager][0]['content'] == 'hello'
+    assert alice.chat_messages[group_chat_manager][0]['role'] == 'assistant'
+    assert alice.chat_messages[group_chat_manager][1]['content'] == 'From bob<eof_name>:\nThis is bob speaking.'
+    assert alice.chat_messages[group_chat_manager][1]['role'] == 'user'
+    assert bob.chat_messages[group_chat_manager][0]['content'] == 'From alice<eof_name>:\nhello'
+    assert bob.chat_messages[group_chat_manager][0]['role'] == 'user'
+    assert bob.chat_messages[group_chat_manager][1]['content'] == 'This is bob speaking.'
+    assert bob.chat_messages[group_chat_manager][1]['role'] == 'assistant'
 
     group_chat_manager.reset()
     assert len(groupchat.messages) == 0
-    agent1.reset()
-    agent2.reset()
-    agent2.initiate_chat(group_chat_manager, message="hello")
+    alice.reset()
+    bob.reset()
+    bob.initiate_chat(group_chat_manager, message="hello")
     assert len(groupchat.messages) == 2
 
+def test_group_chat_research():
+    config_list_gpt4 = autogen.config_list_from_json(
+    "C:\\Users\\xiaoyuz\\source\\repos\\autogen\\notebook\\OAI_CONFIG_LIST.json",
+    filter_dict={
+        "model": ["JulyChat"],
+    },
+    )
+    gpt4_config = {
+        "model": "JulyChat",
+        "seed": random.randint(0, 100),  # change the seed for different trials
+        "temperature": 0,
+        "config_list": config_list_gpt4,
+        "request_timeout": 120,
+    }
+    user_proxy = autogen.UserProxyAgent(
+       name="Admin",
+       system_message="You Interact with the planner to discuss the plan and approve the plan from planner. You say TERMINATE to when task completed.",
+       code_execution_config=False,
+       llm_config=gpt4_config,
+       human_input_mode="TERMINATE"
+    )
+    engineer = autogen.AssistantAgent(
+        name="Engineer",
+        llm_config=gpt4_config,
+        system_message='''You are Engineer. You follow an approved plan. You write python/shell code to solve tasks. Wrap the code in a code block that specifies the script type. The user can't modify your code. So do not suggest incomplete code which requires others to modify. Don't use a code block if it's not intended to be executed by the executor.
+Don't include multiple code blocks in one response. Do not ask others to copy and paste the result. Check the execution result returned by the executor.
+If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
+'''
+    )
+    scientist = autogen.AssistantAgent(
+        name="Scientist",
+        llm_config=gpt4_config,
+        system_message="""You follow an approved plan. You are able to categorize papers after seeing their abstracts printed. You don't write code."""
+    )
+    planner = autogen.AssistantAgent(
+        name="Planner",
+        system_message='''You suggest a plan. Revise the plan based on feedback from admin and critic, until admin approval.
+    The plan may involve an engineer who can write code and a scientist who doesn't write code.
+    Explain the plan first. Be clear which step is performed by an engineer, and which step is performed by a scientist.
+    ''',
+        llm_config=gpt4_config,
+    )
+    executor = autogen.UserProxyAgent(
+        name="Executor",
+        system_message="You execute the code written by the engineer and report the result.",
+        human_input_mode="NEVER",
+        default_auto_reply="no code provided, please provide code to execute",
+        code_execution_config={"last_n_messages": 3, "work_dir": "paper"},
+    )
+    critic = autogen.AssistantAgent(
+        name="Critic",
+        system_message="you double check plan, claims, code from other agents and provide feedback. Check whether the plan includes adding verifiable info such as source URL.",
+        llm_config=gpt4_config,
+    )
+    groupchat = autogen.GroupChat(agents=[user_proxy, engineer, scientist, planner, executor, critic], messages=[], max_round=50)
+    manager = autogen.GroupChatManager(groupchat=groupchat, llm_config=gpt4_config)
+    user_proxy.send("welcome to the group, work together to resolve my task, I'll say TERMINATE when task is completed", manager)
+    engineer.send("I'll write code to solve the task", manager)
+    planner.send("I'll suggest a plan for other agents to follow", manager)
+    scientist.send("I'll categorize papers after engineer downloads paper.", manager)
+    executor.send("I'll execute the code from engineer", manager)
+    critic.send("I'll double check the plan, claims, code from other agents and provide feedback", manager)
+
+    user_proxy.initiate_chat(
+        manager,
+        message="""
+    find papers on LLM applications from arxiv in the last week, create a markdown table of different domains.
+    planner, you suggest a plan first.
+    Other agents, please follow the plan.
+    """,
+    )
 
 def test_plugin():
     # Give another Agent class ability to manage group chat
