eb51ee792d24e31d2213d25291aa6e7b8528c0e6
Gourav2609 <herculeswarrior.in@gmail.com>
2023-10-14 01:30:37 +0530

    docstr updated

--- autogen/code_utils.py
+++ autogen/code_utils.py
@@ -243,6 +243,9 @@ def execute_code(
             with the first image successfully pulled.
             If None, False or empty, the code will be executed in the current environment.
             Default is True, which will be converted into a list.
+            Note: In cases where the Docker package is missing and the `use_docker` parameter is explicitly set to True,
+            an error will be raised. If the Docker package is missing and the `use_docker` parameter is not explicitly set,
+            a warning will be displayed, and the code will run natively.
             If the code is executed in the current environment,
             the code must be trusted.
         lang (Optional, str): The language of the code. Default is "python".
