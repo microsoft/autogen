c75b6b4e1e88b49359d781d1a27200677f295c12
binary-husky <qingxu.fu@outlook.com>
2023-10-23 21:29:55 +0800

    update void-terminal = 0.0.7

--- setup.py
+++ setup.py
@@ -47,7 +47,7 @@ setuptools.setup(
             "nbconvert",
             "nbformat",
             "pre-commit",
-            "void-terminal==0.0.6",
+            "void-terminal==0.0.7",
             "pydantic==1.10.9",
             "pytest-asyncio",
             "pytest>=6.1.1",
