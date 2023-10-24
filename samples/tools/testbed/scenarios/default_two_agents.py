df27d75ca6774b2f6eed592bcbd1570e86003d0c
Adam Fourney <adamfo@microsoft.com>
2023-10-24 15:53:20 -0700

    Added some stricter termination logic to the two_agent scenario, and swiched the logo task from finding Autogen's logo, to finding Microsoft's (it's easier)

--- samples/tools/testbed/scenarios/default_two_agents.py
+++ samples/tools/testbed/scenarios/default_two_agents.py
@@ -11,15 +11,21 @@ config_list = config_list_from_json(
     filter_dict={"model": ["__MODEL__"]},
 )
 
-assistant = AssistantAgent("assistant", llm_config={"request_timeout": 180, "config_list": config_list})
+assistant = AssistantAgent(
+    "assistant",
+    is_termination_msg=lambda x: x.get("content", "").rstrip().find("TERMINATE") >= 0,
+    llm_config={"request_timeout": 180, "config_list": config_list},
+)
 user_proxy = UserProxyAgent(
     "user_proxy",
     human_input_mode="NEVER",
+    is_termination_msg=lambda x: x.get("content", "").rstrip().find("TERMINATE") >= 0,
     code_execution_config={
         "work_dir": "coding",
         "use_docker": False,
     },
     max_consecutive_auto_reply=10,
+    default_auto_reply="TERMINATE",
 )
 user_proxy.initiate_chat(assistant, message="__PROMPT__")
 
