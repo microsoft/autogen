6c008348c6baeff948d252e0daf95270cbafb74d
kevin666aa <yrwu000627@gmail.com>
2023-10-25 15:06:22 -0400

    remove token_count from retrieval util

--- autogen/token_count_utils.py
+++ autogen/token_count_utils.py
@@ -12,8 +12,11 @@ def get_max_token_limit(model="gpt-3.5-turbo-0613"):
         "gpt-3.5-turbo": 4096,
         "gpt-3.5-turbo-0301": 4096,
         "gpt-3.5-turbo-0613": 4096,
+        "gpt-3.5-turbo-instruct": 4096,
         "gpt-3.5-turbo-16k": 16384,
         "gpt-35-turbo": 4096,
+        "gpt-35-turbo-16k": 16384,
+        "gpt-35-turbo-instruct": 4096,
         "gpt-4": 8192,
         "gpt-4-32k": 32768,
         "gpt-4-32k-0314": 32768,  # deprecate in Sep
@@ -28,11 +31,28 @@ def percentile_used(input, model="gpt-3.5-turbo-0613"):
     return count_token(input) / get_max_token_limit(model)
 
 
-def token_left(input, model="gpt-3.5-turbo-0613"):
+def token_left(input: Union[str, List, Dict], model="gpt-3.5-turbo-0613") -> int:
+    """Count number of tokens left for an OpenAI model.
+
+    Args:
+        input: (str, list, dict): Input to the model.
+        model: (str): Model name.
+    
+    Returns:
+        int: Number of tokens left that the model can use for completion.
+    """
     return get_max_token_limit(model) - count_token(input, model=model)
 
 
-def count_token(input: Union[str, List, Dict], model: str = "gpt-3.5-turbo-0613"):
+def count_token(input: Union[str, List, Dict], model: str = "gpt-3.5-turbo-0613") -> int:
+    """Count number of tokens used by an OpenAI model.
+    Args:
+        input: (str, list, dict): Input to the model.
+        model: (str): Model name.
+    
+    Returns:
+        int: Number of tokens from the input.
+    """
     if isinstance(input, str):
         return _num_token_from_text(input, model=model)
     elif isinstance(input, list) or isinstance(input, dict):
@@ -42,15 +62,16 @@ def count_token(input: Union[str, List, Dict], model: str = "gpt-3.5-turbo-0613"
 
 
 def _num_token_from_text(text: str, model: str = "gpt-3.5-turbo-0613"):
+    """Return the number of tokens used by a string."""
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
-        print("Warning: model not found. Using cl100k_base encoding.")
+        logger.warning(f"Model {model} not found. Using cl100k_base encoding.")
         encoding = tiktoken.get_encoding("cl100k_base")
     return len(encoding.encode(text))
 
 
-def _num_token_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0613", verbose=False):
+def _num_token_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0613"):
     """Return the number of tokens used by a list of messages.
 
     retrieved from https://github.com/openai/openai-cookbook/blob/main/examples/How_to_count_tokens_with_tiktoken.ipynb/
@@ -77,16 +98,14 @@ def _num_token_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0
         tokens_per_message = 4  # every message follows <|start|>{role/name}\n{content}<|end|>\n
         tokens_per_name = -1  # if there's a name, the role is omitted
     elif "gpt-3.5-turbo" in model:
-        if verbose:
-            print("Warning: gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
+        logger.info("gpt-3.5-turbo may update over time. Returning num tokens assuming gpt-3.5-turbo-0613.")
         return _num_token_from_messages(messages, model="gpt-3.5-turbo-0613")
     elif "gpt-4" in model:
-        if verbose:
-            print("Warning: gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
+        logger.info("gpt-4 may update over time. Returning num tokens assuming gpt-4-0613.")
         return _num_token_from_messages(messages, model="gpt-4-0613")
     else:
         raise NotImplementedError(
-            f"""num_tokens_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
+            f"""_num_token_from_messages() is not implemented for model {model}. See https://github.com/openai/openai-python/blob/main/chatml.md for information on how messages are converted to tokens."""
         )
     num_tokens = 0
     for message in messages:
@@ -101,7 +120,7 @@ def _num_token_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0
                     value = json.dumps(value)
                 except TypeError:
                     logger.warning(
-                        f"Warning: value {value} is not a string and cannot be converted to json. It is a type: {type(value)} Skipping."
+                        f"Value {value} is not a string and cannot be converted to json. It is a type: {type(value)} Skipping."
                     )
                     continue
 
@@ -112,8 +131,16 @@ def _num_token_from_messages(messages: Union[List, Dict], model="gpt-3.5-turbo-0
     return num_tokens
 
 
-def num_tokens_from_functions(functions, model="gpt-3.5-turbo-0613"):
-    """Return the number of tokens used by a list of functions."""
+def num_tokens_from_functions(functions, model="gpt-3.5-turbo-0613") -> int:
+    """Return the number of tokens used by a list of functions.
+    
+    Args:
+        functions: (list): List of function descriptions that will be passed in model.
+        model: (str): Model name.
+    
+    Returns:
+        int: Number of tokens from the function descriptions.
+    """
     try:
         encoding = tiktoken.encoding_for_model(model)
     except KeyError:
