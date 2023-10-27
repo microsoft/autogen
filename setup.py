6c72ed916117cae49eea4250f440cd68c6f126f7
Chi Wang <wang.chi@microsoft.com>
2023-10-27 21:37:44 +0000

    add ipython to retrievechat dependency

--- setup.py
+++ setup.py
@@ -57,7 +57,7 @@ setuptools.setup(
         ],
         "blendsearch": ["flaml[blendsearch]"],
         "mathchat": ["sympy", "pydantic==1.10.9", "wolframalpha"],
-        "retrievechat": ["chromadb", "tiktoken", "sentence_transformers", "pypdf"],
+        "retrievechat": ["chromadb", "tiktoken", "sentence_transformers", "pypdf", "ipython"],
         "teachable": ["chromadb"],
     },
     classifiers=[
