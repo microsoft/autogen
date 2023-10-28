86c6cc4f5f311c7fd9eeede3f5c7f2c38d9c292d
Cesar <cesar.bonadio@gmail.com>
2023-10-28 16:48:20 -0300

    Adjust the test_async_get_human_input.py for pre-commit-check error v2

--- test/agentchat/test_async_get_human_input.py
+++ test/agentchat/test_async_get_human_input.py
@@ -3,6 +3,7 @@ import autogen
 import pytest
 from test_assistant_agent import KEY_LOC, OAI_CONFIG_LIST
 
+
 @pytest.mark.asyncio
 async def test_async_get_human_input():
     try:
@@ -15,19 +16,11 @@ async def test_async_get_human_input():
     assistant = autogen.AssistantAgent(
         name="assistant",
         max_consecutive_auto_reply=2,
-        llm_config={
-            "request_timeout": 600,
-            "seed": 41,
-            "config_list": config_list,
-            "temperature": 0
-        }
+        llm_config={"request_timeout": 600, "seed": 41, "config_list": config_list, "temperature": 0},
     )
 
     user_proxy = autogen.UserProxyAgent(
-        name="user",
-        human_input_mode="ALWAYS",
-        max_consecutive_auto_reply=1,
-        code_execution_config=False
+        name="user", human_input_mode="ALWAYS", max_consecutive_auto_reply=1, code_execution_config=False
     )
 
     async def custom_a_get_human_input(prompt):
@@ -41,11 +34,8 @@ async def test_async_get_human_input():
     user_proxy.register_reply([autogen.Agent, None], autogen.ConversableAgent.generate_function_call_reply)
     user_proxy.register_reply([autogen.Agent, None], autogen.ConversableAgent.a_check_termination_and_human_reply)
 
-    await user_proxy.a_initiate_chat(
-        assistant,
-        clear_history=True,
-        message="""Hello."""
-    )
+    await user_proxy.a_initiate_chat(assistant, clear_history=True, message="Hello.")
+
 
 if __name__ == "__main__":
     test_async_get_human_input()
