fbc9f6c5f67f61ed5002090adbf16236578919ec
Cesar <cesar.bonadio@gmail.com>
2023-10-28 18:47:17 -0300

    Adjust remove unnecessary register_reply

--- test/agentchat/test_async_get_human_input.py
+++ test/agentchat/test_async_get_human_input.py
@@ -19,19 +19,13 @@ async def test_async_get_human_input():
         llm_config={"request_timeout": 600, "seed": 41, "config_list": config_list, "temperature": 0},
     )
 
-    user_proxy = autogen.UserProxyAgent(
-        name="user", human_input_mode="ALWAYS", max_consecutive_auto_reply=1, code_execution_config=False
-    )
+    user_proxy = autogen.UserProxyAgent(name="user", human_input_mode="ALWAYS", code_execution_config=False)
 
     async def custom_a_get_human_input(prompt):
         return "This is a test"
 
     user_proxy.a_get_human_input = custom_a_get_human_input
 
-    user_proxy._reply_func_list = []
-    user_proxy.register_reply([autogen.Agent, None], autogen.ConversableAgent.generate_oai_reply)
-    user_proxy.register_reply([autogen.Agent, None], autogen.ConversableAgent.generate_code_execution_reply)
-    user_proxy.register_reply([autogen.Agent, None], autogen.ConversableAgent.generate_function_call_reply)
     user_proxy.register_reply([autogen.Agent, None], autogen.ConversableAgent.a_check_termination_and_human_reply)
 
     await user_proxy.a_initiate_chat(assistant, clear_history=True, message="Hello.")
