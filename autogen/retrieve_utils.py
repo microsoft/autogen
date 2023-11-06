8ca010e07533bc2e00252eaf0c3c7b58b52e0422
Li Jiang <bnujli@gmail.com>
2023-11-06 12:27:57 +0800

    Update qdrant, Add pdf to unstructured

--- autogen/retrieve_utils.py
+++ autogen/retrieve_utils.py
@@ -40,7 +40,7 @@ TEXT_FORMATS = [
     "yml",
     "pdf",
 ]
-UNSTRUCTURED_FORMATS = ["docx", "doc", "odt", "pptx", "ppt", "xlsx", "eml", "msg", "epub"]
+UNSTRUCTURED_FORMATS = ["docx", "doc", "odt", "pptx", "ppt", "xlsx", "eml", "msg", "epub", "pdf"]
 if HAS_UNSTRUCTURED:
     TEXT_FORMATS += UNSTRUCTURED_FORMATS
     TEXT_FORMATS = list(set(TEXT_FORMATS))
