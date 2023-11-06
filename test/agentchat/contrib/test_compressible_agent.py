87e464b48f94dc3168a28eae8c43c8802c2e4808
kevin666aa <yrwu000627@gmail.com>
2023-11-06 02:18:28 -0500

    Fix bug

--- test/agentchat/contrib/test_compressible_agent.py
+++ test/agentchat/contrib/test_compressible_agent.py
@@ -13,7 +13,7 @@ config_list = autogen.config_list_from_json(
     OAI_CONFIG_LIST,
     file_location=KEY_LOC,
     filter_dict={
-        "model": ["gpt-4", "gpt4", "gpt-4-32k", "gpt-4-32k-0314"],
+        "model": ["gpt-3.5-turbo", "gpt-35-turbo", "gpt-3.5-turbo-16k", "gpt-35-turbo-16k"],
     },
 )
 
@@ -29,7 +29,7 @@ except ImportError:
     sys.platform in ["darwin", "win32"] or not OPENAI_INSTALLED,
     reason="do not run on MacOS or windows or dependency is not installed",
 )
-def test_compressible_agent():
+def test_mode_compress():
     conversations = {}
 
     assistant = CompressibleAgent(
@@ -68,6 +68,76 @@ def test_compressible_agent():
     sys.platform in ["darwin", "win32"] or not OPENAI_INSTALLED,
     reason="do not run on MacOS or windows or dependency is not installed",
 )
+def test_mode_customized():
+    try:
+        assistant = CompressibleAgent(
+            name="assistant",
+            llm_config={
+                "request_timeout": 600,
+                "seed": 43,
+                "config_list": config_list,
+            },
+            compress_config={
+                "mode": "CUSTOMIZED",
+            },
+        )
+    except ValueError:
+        print("ValueError raised as expected.")
+
+    def constrain_num_messages(messages):
+        """Constrain the number of messages to 3.
+
+        This is an example of a customized compression function.
+
+        Returns:
+            bool: whether the compression is successful.
+            list: the compressed messages.
+        """
+        if len(messages) <= 3:
+            # do nothing
+            return False, None
+
+        # save the first and last two messages
+        return True, messages[:1] + messages[-2:]
+
+    # create a CompressibleAgent instance named "assistant"
+    assistant = CompressibleAgent(
+        name="assistant",
+        llm_config={
+            "timeout": 600,
+            "seed": 43,
+            "config_list": config_list,
+            "model": "gpt-3.5-turbo",
+        },
+        compress_config={
+            "mode": "CUSTOMIZED",
+            "compress_function": constrain_num_messages,  # this is required for customized compression
+            "trigger_count": 1000,
+        },
+    )
+
+    # create a UserProxyAgent instance named "user_proxy"
+    user_proxy = autogen.UserProxyAgent(
+        name="user_proxy",
+        human_input_mode="NEVER",
+        max_consecutive_auto_reply=5,
+        is_termination_msg=lambda x: x.get("content", "").rstrip().endswith("TERMINATE")
+        or x.get("content", "").rstrip().endswith("TERMINATE."),
+        code_execution_config={"work_dir": "web"},
+        system_message="""Reply TERMINATE if the task has been solved at full satisfaction.
+    Otherwise, reply CONTINUE, or the reason why the task is not solved yet.""",
+    )
+
+    user_proxy.initiate_chat(
+        assistant,
+        message="""Show me the YTD gain of 10 largest technology companies as of today.""",
+    )
+
+
+@pytest.mark.skipif(
+    sys.platform in ["darwin", "win32"] or not OPENAI_INSTALLED,
+    reason="do not run on MacOS or windows or dependency is not installed",
+)
 def test_compress_messsage():
     assistant = CompressibleAgent(
         name="assistant",
@@ -80,6 +150,7 @@ def test_compress_messsage():
             "mode": "COMPRESS",
             "trigger_count": 600,
             "verbose": True,
+            "leave_last_n": 0,
         },
     )
 
@@ -87,7 +158,8 @@ def test_compress_messsage():
         False,
         None,
     ), "Single message should not be compressed"
-    is_success, compressed = assistant.compress_messages(
+
+    is_success, _ = assistant.compress_messages(
         [
             {"content": "Hello!", "role": "user"},
             {"content": "How can I help you today?", "role": "assistant"},
@@ -97,6 +169,38 @@ def test_compress_messsage():
     assert is_success, "Compression should be successful"
 
 
+def test_mode_terminate():
+    assistant = CompressibleAgent(
+        name="assistant",
+        llm_config={
+            "request_timeout": 600,
+            "seed": 43,
+            "config_list": config_list,
+        },
+        compress_config=True,
+    )
+
+    user_proxy = autogen.UserProxyAgent(
+        name="user_proxy",
+        is_termination_msg=lambda x: x.get("content", "") and x.get("content", "").rstrip().endswith("TERMINATE"),
+        human_input_mode="NEVER",
+        max_consecutive_auto_reply=5,
+        code_execution_config={"work_dir": "coding"},
+    )
+
+    final, _ = assistant.on_oai_token_limit(
+        [
+            {"content": "Hello!", "role": "user"},
+            {"content": "How can I help you today?", "role": "assistant"},
+            {"content": "1&" * 5000, "role": "assistant"},
+        ],
+        sender=user_proxy,
+    )
+    assert final, "Terminating the conversation at max token limit is not working."
+
+
 if __name__ == "__main__":
+    test_mode_compress()
+    test_mode_customized()
     test_compress_messsage()
-    test_compressible_agent()
+    test_mode_terminate()
