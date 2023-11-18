32525b9a2e491ccf631af29890224cb34a2b3b40
cozypet <yanhan860711@gmail.com>
2023-11-18 13:25:37 +0100

    format again!!

--- autogen/agentchat/contrib/mongoDB_retrieve_user_proxy_agent.py
+++ autogen/agentchat/contrib/mongoDB_retrieve_user_proxy_agent.py
@@ -13,7 +13,11 @@ config_list = config_list_from_json(env_or_file="llm_config")
 
 class MongoDBRetrieveUserProxyAgent(RetrieveUserProxyAgent):
     def query_vector_db(
-        self, query_texts: List[str], n_results: int = 10, search_string: str = "", **kwargs,
+        self,
+        query_texts: List[str],
+        n_results: int = 10,
+        search_string: str = "",
+        **kwargs,
     ) -> Dict[str, Union[List[str], List[List[str]]]]:
         # Concatenate all strings in query_texts into one string
         concatenated_text = " ".join(query_texts)
@@ -27,19 +31,25 @@ class MongoDBRetrieveUserProxyAgent(RetrieveUserProxyAgent):
         document_contents = [document.get("text_chunks", "") for document in documents]
         return {
             "ids": [ids],  # Wrap ids in a list
-            # Wrap document_contents in a list
-            "documents": [document_contents],
+            "documents": [document_contents],  # Wrap document_contents in a list
         }
 
     def retrieve_docs(self, problem: str, n_results: int = 20, search_string: str = "", **kwargs):
         # Query for similar documents
         results = self.query_vector_db(
-            query_texts=[problem], n_results=n_results, search_string=search_string, **kwargs,
+            query_texts=[problem],
+            n_results=n_results,
+            search_string=search_string,
+            **kwargs,
         )
         self._results = results
 
         # Check if results is a dictionary and "documents" is a list
-        if isinstance(results["ids"], list) and len(results["ids"]) > 0 and isinstance(results["ids"][0], str):
+        if (
+            isinstance(results["ids"], list)
+            and len(results["ids"]) > 0
+            and isinstance(results["ids"][0], str)
+        ):
             doc_id = results["ids"][0]
             if doc_id in self._doc_ids:
                 # Accessing the "documents" key in the results dictionary
@@ -60,16 +70,15 @@ class MongoDBRetrieveUserProxyAgent(RetrieveUserProxyAgent):
 
     def connect_mongodb(self):
         """
-        Connect to the MongoDB Atlas server and return the collection. 
+        Connect to the MongoDB Atlas server and return the collection.
 
-        Initiate your connection to a MongoDB Atlas server and access your desired collection directly. 
-        I recommend getting started with an M0 tier of MongoDB which is free of charge and fully managed. You can sign up here: <https://www.mongodb.com/cloud/atlas/register>.
+        Initiate your connection to a MongoDB Atlas server and access your desired collection directly.
+        I recommend getting started with an M0 tier of MongoDB, which is free of charge and fully managed. You can sign up here: <https://www.mongodb.com/cloud/atlas/register>.
 
         Using a managed MongoDB with Autogen brings significant benefits:
         1. It's fully managed, which means local installation is not required.
-        2. It's cloud-agnostic, as it's available on multiple platforms including AWS, Azure, and GCP. 
+        2. It's cloud-agnostic, as it's available on multiple platforms including AWS, Azure, and GCP.
         3. It offers enterprise-level vector storage, making it suitable for production environments.
-
         """
         mongo_url = "mongodb+srv://your_login:your_password@your_cluster?retryWrites=true&w=majority"
         client = pymongo.MongoClient(mongo_url)
@@ -80,7 +89,7 @@ class MongoDBRetrieveUserProxyAgent(RetrieveUserProxyAgent):
     def find_similar_documents(self, embedding):
         """
         Find similar documents in MongoDB based on the provided embedding.
-        Prerequisites: 
+        Prerequisites:
         1. Ensure that your data with the embedding is already stored in MongoDB.
         2. Have a vector index defined in MongoDB. For guidance on how to create your vector index, you can refer to the following link: <https://www.mongodb.com/docs/atlas/atlas-search/field-types/knn-vector/>.
         3. Understand the details of a Vector Query, such as what 'numCandidates' and 'limit' signify. For more information on this, please consult: <https://www.mongodb.com/docs/atlas/atlas-vector-search/vector-search-stage/>.
@@ -107,16 +116,24 @@ class MongoDBRetrieveUserProxyAgent(RetrieveUserProxyAgent):
 
 # Instantiate the Assistant Agent with provided configuration
 assistant = RetrieveAssistantAgent(
-    name="assistant", system_message="You are a helpful assistant.", llm_config=config_list,
+    name="assistant",
+    system_message="You are a helpful assistant.",
+    llm_config=config_list,
 )
 
 # Instantiate the User Proxy Agent with MongoDB functionality
 ragproxyagent = MongoDBRetrieveUserProxyAgent(
-    name="MongoDB_RAG_Agent", human_input_mode="NEVER", max_consecutive_auto_reply=2, retrieve_config={"task": "qa",},
+    name="MongoDB_RAG_Agent",
+    human_input_mode="NEVER",
+    max_consecutive_auto_reply=2,
+    retrieve_config={
+        "task": "qa",
+    },
 )
 
 # Reset the assistant and retrieve documents for a specific problem
 assistant.reset()
 ragproxyagent.initiate_chat(
-    assistant, problem="What is the average price of client A's expense based on his purchase history provided?"
+    assistant,
+    problem="What is the average price of client A's expense based on his purchase history provided?",
 )
