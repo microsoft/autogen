5126168c8fd4336c6e33ab5dc5dbfb7c4ac14647
cozypet <yanhan860711@gmail.com>
2023-11-19 18:33:20 +0100

    test update

--- test/agentchat/contrib/test_mongoDB_retrieve.py
+++ test/agentchat/contrib/test_mongoDB_retrieve.py
@@ -1,12 +1,10 @@
 import os
 import sys
 import pytest
-import pymongo
 
 from autogen import config_list_from_json
 from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
 from autogen.agentchat.contrib.mongoDB_retrieve_user_proxy_agent import MongoDBRetrieveUserProxyAgent, MongoDBConfig
-    
 
 # Define MongoDB configuration (replace with your own configuration)
 """ mongo_config = MongoDBConfig(
