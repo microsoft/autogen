ba2d85b801458a786dba467390fde99a48a409c1
cozypet <yanhan860711@gmail.com>
2023-11-19 16:57:46 +0100

    format

--- test/agentchat/contrib/test_mongoDB_retrieve.py
+++ test/agentchat/contrib/test_mongoDB_retrieve.py
@@ -4,7 +4,6 @@ import pytest
 
 from autogen import config_list_from_json
 from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
-#from autogen.agentchat.contrib.mongoDB_retrieve_user_proxy_agent import MongoDBRetrieveUserProxyAgent
 
 # Import the classes and functions from the refactored code
 try:
