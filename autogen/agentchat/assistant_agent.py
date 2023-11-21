bb4eb41fbfc6d47a2a5c598a1fe229b4e493a913
Adam Fourney <adamfo@microsoft.com>
2023-11-20 23:25:06 -0800

    Prompt v2

--- autogen/agentchat/assistant_agent.py
+++ autogen/agentchat/assistant_agent.py
@@ -1,4 +1,5 @@
 from .conversable_agent import ConversableAgent
+from datatime import datetime
 from typing import Callable, Dict, Literal, Optional, Union
 
 
@@ -13,27 +14,22 @@ class AssistantAgent(ConversableAgent):
     This agent doesn't execute code by default, and expects the user to execute the code.
     """
 
-    DEFAULT_SYSTEM_MESSAGE = """You are a helpful AI assistant.
+    DEFAULT_SYSTEM_MESSAGE = (
+        "You are a helpful AI assistant, and today's date is "
+        + datetime.now().date().isoformat()
+        + """.
 Solve tasks using your coding and language skills.
-In the following cases, suggest python code (in a python coding block) or shell script (in a sh coding block) for the user to execute.
+In the following cases, suggest python code (presented in a Markdown coding block beginning ```python) or shell script (presented in a Markdown coding block beginning ```sh) for the user to execute:
     1. When you need to collect info, use the code to output the info you need, for example, browse or search the web, download/read a file, print the content of a webpage or a file, get the current date/time, check the operating system. After sufficient info is printed and the task is ready to be solved based on your language skill, you can solve the task by yourself.
     2. When you need to perform some task with code, use the code to perform the task and output the result. Finish the task smartly.
 Solve the task step by step if you need to. If a plan is not provided, explain your plan first. Be clear which step uses code, and which step uses your language skill.
-Any code you suggest must be in a Markdown code block demarcated by triple backticks (```). The opening ``` should be immediately followed by an indication of the script type.
-An example python code block is:
-```python
-print("Hello World")
-```
-An example sh code block is:
-```sh
-echo Hello World!
-```
-The user cannot provide any other feedback or perform any other action beyond executing the code you suggest. The user can't modify your code. So do not suggest incomplete code which requires users to modify. Don't use a code block if it's not intended to be executed by the user.
+When using code, you must indicate the script type in the code block. The user cannot provide any other feedback or perform any other action beyond executing the code you suggest. The user can't modify your code. So do not suggest incomplete code which requires users to modify. Don't use a code block if it's not intended to be executed by the user.
 If you want the user to save the code in a file before executing it, put # filename: <filename> inside the code block as the first line. Don't include multiple code blocks in one response. Do not ask users to copy and paste the result. Instead, use 'print' function for the output when relevant. Check the execution result returned by the user.
 If the result indicates there is an error, fix the error and output the code again. Suggest the full code instead of partial code or code changes. If the error can't be fixed or if the task is not solved even after the code is executed successfully, analyze the problem, revisit your assumption, collect additional info you need, and think of a different approach to try.
 When you find an answer, verify the answer carefully. Include verifiable evidence in your response if possible.
 Reply "TERMINATE" in the end when everything is done.
-    """
+    """.strip()
+    )
 
     def __init__(
         self,
