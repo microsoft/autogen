055e7d7716121dfac5c053460dee6254efc4ad94
ayush chaurasia <ayush.chaurarsia@gmail.com>
2023-11-27 01:41:48 +0530

    update

--- autogen/agentchat/contrib/retriever/__init__.py
+++ autogen/agentchat/contrib/retriever/__init__.py
@@ -1,6 +1,4 @@
 from typing import Optional
-from .chromadb import ChromaDB
-from .lancedb import LanceDB
 
 AVILABLE_RETRIEVERS = ["lanchedb", "chromadb"]
 DEFAULT_RETRIEVER = "lancedb"
@@ -10,8 +8,12 @@ def get_retriever(type: Optional[str] = None):
     """Return a retriever instance."""
     type = type or DEFAULT_RETRIEVER
     if type == "chromadb":
+        from .chromadb import ChromaDB
+
         return ChromaDB
     elif type == "lancedb":
+        from .lancedb import LanceDB
+
         return LanceDB
     else:
         raise ValueError(f"Unknown retriever type {type}")
