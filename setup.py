10f1b233486741a5fb3e63390c92443dcd76c14d
Ayush Chaurasia <ayush.chaurarsia@gmail.com>
2023-11-28 23:46:30 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ setuptools.setup(
         "blendsearch": ["flaml[blendsearch]"],
         "mathchat": ["sympy", "pydantic==1.10.9", "wolframalpha"],
         "retrievechat": ["lancedb", "sentence_transformers", "pypdf", "ipython"],
-        "teachable": ["lancedb"],
+        "teachable": ["chromadb"],
         "lmm": ["replicate", "pillow"],
         "graphs": ["networkx~=3.2.1", "matplotlib~=3.8.1"],
     },
