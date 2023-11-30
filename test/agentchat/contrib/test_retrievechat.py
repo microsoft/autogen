ff0220e85cb0d8785235045aa101d1bc35829c8a
Li Jiang <lijiang1@microsoft.com>
2023-11-30 07:59:37 +0000

    Add a warning message if docs_path not explicitly set

--- test/agentchat/contrib/test_retrievechat.py
+++ test/agentchat/contrib/test_retrievechat.py
@@ -27,7 +27,6 @@ except ImportError:
     reason="do not run on MacOS or windows or dependency is not installed",
 )
 def test_retrievechat():
-    conversations = {}
     # autogen.ChatCompletion.start_logging(conversations)  # deprecated in v0.2
 
     config_list = autogen.config_list_from_json(
@@ -65,8 +64,31 @@ def test_retrievechat():
     code_problem = "How can I use FLAML to perform a classification task, set use_spark=True, train 30 seconds and force cancel jobs if time limit is reached."
     ragproxyagent.initiate_chat(assistant, problem=code_problem, search_string="spark", silent=True)
 
-    print(conversations)
+
+def test_retrieve_config(caplog):
+    # test warning message when no docs_path is provided
+    ragproxyagent = RetrieveUserProxyAgent(
+        name="ragproxyagent",
+        human_input_mode="NEVER",
+        max_consecutive_auto_reply=2,
+        retrieve_config={
+            "chunk_token_size": 2000,
+            "get_or_create": True,
+        },
+    )
+
+    # Capture the printed content
+    captured_logs = caplog.records[0]
+    print(captured_logs)
+
+    # Assert on the printed content
+    assert (
+        f"docs_path is not provided in retrieve_config. Will raise ValueError if the collection `{ragproxyagent._collection_name}` doesn't exist."
+        in captured_logs.message
+    )
+    assert captured_logs.levelname == "WARNING"
 
 
 if __name__ == "__main__":
     test_retrievechat()
+    # test_retrieve_config()
