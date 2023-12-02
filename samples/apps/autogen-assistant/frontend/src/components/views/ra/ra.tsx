a5c92a6817b6e876060421c8b0e465019f57726b
Victor Dibia <victordibia@microsoft.com>
2023-12-01 16:22:02 -0800

    Improvements to AutoGen Assistant (#828)
    * improve template for files, integreate files in db
    
    * ui update, improvements to file display grid
    
    * add new global skill for image generation
    
    * update readme to address #739
    
    * utils.py refactor, separate db uitls for ease of development
    
    * db utils
    
    * add support for sessions both in backend api and ui
    
    * improve implementation for session support
    
    * add early v1 support for a gallery and publishing to a gallery
    
    * rewrite logic for file storage representation. Store only file references on in db
    
    * update generate image logic
    
    * update ui layout
    
    * fix light dark mode bug
    
    * v1 support for showing items added to gallery
    
    * remove viewer as it is merged in gallery
    
    * formatting updates
    
    * QOL refactoring
    
    * readme and general updates
    
    * add example notebook on assistant api
    
    * imporve naming conventions and formatting
    
    * readme update
    
    * Update samples/apps/autogen-assistant/pyproject.toml
    
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>
    
    * Update samples/apps/autogen-assistant/notebooks/tutorial.ipynb
    
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>
    
    ---------
    
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>

--- samples/apps/autogen-assistant/frontend/src/components/views/ra/ra.tsx
+++ samples/apps/autogen-assistant/frontend/src/components/views/ra/ra.tsx
@@ -1,14 +1,16 @@
 import * as React from "react";
-import { IMessage, IStatus } from "../../types";
+import { IChatSession, IMessage, IStatus } from "../../types";
 import { fetchJSON, getServerUrl, setLocalStorage } from "../../utils";
 import ChatBox from "./chatbox";
 import { appContext } from "../../../hooks/provider";
 import { message } from "antd";
 import SideBarView from "./sidebar";
+import { useConfigStore } from "../../../hooks/store";
 
 const RAView = () => {
+  const session: IChatSession | null = useConfigStore((state) => state.session);
   const [loading, setLoading] = React.useState(false);
-  const [messages, setMessages] = React.useState<IMessage[]>([]);
+  const [messages, setMessages] = React.useState<IMessage[] | null>(null);
   const [skillUpdated, setSkillUpdated] = React.useState("default");
 
   const skillup = {
@@ -29,11 +31,12 @@ const RAView = () => {
 
   const { user } = React.useContext(appContext);
   const serverUrl = getServerUrl();
-  const fetchMessagesUrl = `${serverUrl}/messages?user_id=${user?.email}`;
+  const fetchMessagesUrl = `${serverUrl}/messages?user_id=${user?.email}&session_id=${session?.session_id}`;
 
   const fetchMessages = () => {
     setError(null);
     setLoading(true);
+    setMessages(null);
     // const fetch;
     const payLoad = {
       method: "GET",
@@ -62,11 +65,11 @@ const RAView = () => {
   };
 
   React.useEffect(() => {
-    if (user) {
+    if (user && session) {
       // console.log("fetching messages", messages);
       fetchMessages();
     }
-  }, []);
+  }, [session]);
 
   return (
     <div className="h-full   ">
