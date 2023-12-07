a49775fa37a54b2db3bf608a3ebb5134ad1198aa
jevenson76 <134688866+jevenson76@users.noreply.github.com>
2023-12-07 22:53:15 +0000

    modified:   notebook/agentchat_planning.ipynb 	new file:   notebook/planning/load_data.py

---
+++ notebook/planning/load_data.py
@@ -0,0 +1,9 @@
+# filename: load_data.py
+
+import pandas as pd
+
+# Replace 'your_file.csv' with your actual file name or path
+data = pd.read_csv("your_file.csv")
+
+# Print the first few rows of the data
+print(data.head())
