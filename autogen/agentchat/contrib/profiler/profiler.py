50d70b4f284ee6d528c6e97babaede3ad8d727e7
gagb <gagb@users.noreply.github.com>
2023-12-22 05:19:34 +0000

    Generalize the state space

--- autogen/agentchat/contrib/profiler/profiler.py
+++ autogen/agentchat/contrib/profiler/profiler.py
@@ -1,40 +1,54 @@
 import json
 import copy
-from typing import Dict, List, Optional
+from typing import Dict, List, Optional, Union
+import logging
 import autogen
 
 
-EXAMPLE_CODES = """Available codes:
-USER_REQUEST: The message shows the *user* requesting a task that needs to be completed
-SUGGESTING-CODE: The assistant is suggesting code
-CODE-EXECUTION: The user shared results of code execution, e.g., results, logs, error trace
-TASK-COMPLETED: The message from either user or assistant indicates that provided task was
-completed or the goal was accomplished, e.g., this maybe indicated through successful code
-execution, TERMINATE messsages, etc"
-"""
+EXAMPLE_STATE_SPACE = [
+    {
+        "name": "USER_REQUEST",
+        "description": "The message shows the *user* requesting a task that needs to be completed",
+        "valid_roles": ["user"],
+    },
+    {
+        "name": "SUGGESTING-CODE",
+        "description": "The assistant is suggesting code",
+        "valid_roles": ["assistant"],
+    },
+    {
+        "name": "CODE-EXECUTION",
+        "description": "The user shared results of code execution, e.g., results, logs, error trace",
+        "valid_roles": ["user"],
+    },
+    {
+        "name": "TASK-COMPLETED",
+        "description": "The message from either user or assistant indicates that provided task was completed or the goal was accomplished, e.g., this maybe indicated through successful code execution, TERMINATE messsages, etc",
+        "valid_roles": ["user", "assistant"],
+    },
+    {
+        "name": "EMPTY",
+        "description": "The message is empty",
+        "valid_roles": ["user"],
+    },
+]
 
-# maps codes to descriptions
-# the keys define the label space
-# and the descriptions define the label descriptions
-EXAMPLE_STATE_SPACE = {
-    "USER_REQUEST": "The message shows the *user* requesting a task that needs to be completed",
-    "SUGGESTING-CODE": "The assistant is suggesting code",
-    "CODE-EXECUTION": "The user shared results of code execution, e.g., results, logs, error trace",
-    "TASK-COMPLETED": "The message from either user or assistant indicates that provided task was completed or the goal was accomplished, e.g., this maybe indicated through successful code execution, TERMINATE messsages, etc",
-}
 
-
-def state_space_to_str(state_space: Dict[str, str]) -> str:
+def state_space_to_str(state_space: List[Dict[str, Union[str, List[str]]]], filter_by_role: str = None) -> str:
     """
     Converts a state space to a string.
 
     Args:
-        state_space (Dict[str, str]): A dictionary mapping codes to descriptions.
+        state_space: A list of dictionaries representing the state space. Each dictionary should have 'name', 'description'.
+        filter_by_role: A string representing the role to filter by.
 
     Returns:
         str: A string representation of the state space.
     """
-    return "\n".join([f"{k}: {v}" for k, v in state_space.items()])
+    # get states that are valid for the given role
+    if filter_by_role is not None:
+        state_space = [state for state in state_space if filter_by_role in state["valid_roles"]]
+    return "\n".join([f"{state['name']}: {state['description']}" for state in state_space])
 
 
 def annotate_message(
@@ -57,17 +71,20 @@ def annotate_message(
     if state_space is None:
         state_space = EXAMPLE_STATE_SPACE
 
-    state_space_str = state_space_to_str(state_space)
+    state_space_str = state_space_to_str(state_space, filter_by_role=role)
 
     prompt = f"""Which of the following codes apply to the message:
-codes: {state_space_str}
 
-role: {role}
+List of codes:
+{state_space_str}
 
-content: {content}
+Message
+    role: "{role}"
+    content: "{content}"
 
 Only respond with codes that apply. Codes should be separated by commas.
 """
+    logging.debug(prompt)
     if llm_config is not None:
         client = autogen.OpenAIWrapper(**llm_config)
     else:
--- test/agentchat/contrib/test_profiler.py
+++ test/agentchat/contrib/test_profiler.py
@@ -15,7 +15,10 @@ from test_assistant_agent import OAI_CONFIG_LIST, KEY_LOC  # noqa: E402
 
 def test_state_space_to_str():
     # Test the state_space_to_str function
-    result = state_space_to_str(EXAMPLE_STATE_SPACE)
+    result = state_space_to_str(EXAMPLE_STATE_SPACE, filter_by_role=None)
+    assert isinstance(result, str), "Expected result to be a string"
+
+    result = state_space_to_str(EXAMPLE_STATE_SPACE, filter_by_role="user")
     assert isinstance(result, str), "Expected result to be a string"
 
 
@@ -31,7 +34,7 @@ def test_annotate_message():
 
 def test_annotate_chat_history():
     # Test the annotate_chat_history function
-    chat_history = [{"role": "user", "content": "Hello, world!"}]
+    chat_history = [{"role": "user", "content": "Write a program to print hello world"}]
     state_space = EXAMPLE_STATE_SPACE
     llm_config = config_list_from_json(OAI_CONFIG_LIST, file_location=KEY_LOC)[0]
     result = annotate_chat_history(chat_history, state_space, llm_config=llm_config)
