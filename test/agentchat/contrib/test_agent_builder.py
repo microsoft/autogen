8c3d71e5f3699db89d2a43c03b6cdc7aed843e4c
Linxin Song <rm.social.song1@gmail.com>
2023-12-27 03:59:39 +0900

    add dependency installation in github workflow

--- test/agentchat/contrib/test_agent_builder.py
+++ test/agentchat/contrib/test_agent_builder.py
@@ -2,9 +2,7 @@ import pytest
 import os
 import json
 import sys
-from packaging.requirements import Requirement
 from autogen.agentchat.contrib.agent_builder import AgentBuilder
-from autogen import UserProxyAgent
 
 sys.path.append(os.path.join(os.path.dirname(__file__), ".."))
 from test_assistant_agent import KEY_LOC, OAI_CONFIG_LIST  # noqa: E402
