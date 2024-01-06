dcf5f82f65a27bdfe35c24eeab48b3533a9c15e9
Davor Runje <davor@airt.ai>
2024-01-06 01:07:35 +0000

    Add support for MD files in Docusaurus

--- website/docusaurus.config.js
+++ website/docusaurus.config.js
@@ -12,6 +12,9 @@ module.exports = {
   favicon: "img/ag.ico",
   organizationName: "Microsoft", // Usually your GitHub org/user name.
   projectName: "AutoGen", // Usually your repo name.
+  markdown: {
+    format: 'detect',
+  },
   themeConfig: {
     navbar: {
       title: "AutoGen",
