75b6e0ae3d0f398b79c1088442f79e3160799546
bitnom <14287229+bitnom@users.noreply.github.com>
2024-01-07 02:21:40 -0500

    - #1167 use most generous py pin without changing matplotlib version. - track pdm.lock - update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 import os
-
 import setuptools
 
+# Base directory
 here = os.path.abspath(os.path.dirname(__file__))
 
+# Read the long description from README
 with open("README.md", "r", encoding="UTF-8") as fh:
     long_description = fh.read()
 
@@ -13,15 +14,35 @@ with open(os.path.join(here, "autogen/version.py")) as fp:
     exec(fp.read(), version)
 __version__ = version["__version__"]
 
+# Dependencies
 install_requires = [
-    "openai>=1.3",
     "diskcache",
-    "termcolor",
     "flaml",
+    "openai~=1.3",
     "python-dotenv",
+    "termcolor",
     "tiktoken",
-    "pydantic>=1.10,<3",  # could be both V1 and V2
+    "pydantic>=1.10,<3",
 ]
+
+# Extras requirements
+extras_require = {
+    "test": [
+        "coverage>=5.3",
+        "ipykernel",
+        "nbconvert",
+        "nbformat",
+        "pre-commit",
+        "pytest-asyncio",
+        "pytest>=6.1.1",
+    ],
+    "blendsearch": ["flaml[blendsearch]"],
+    "mathchat": ["pydantic==1.10.9", "sympy", "wolframalpha"],
+    "retrievechat": ["chromadb", "sentence_transformers", "pypdf", "ipython"],
+    "teachable": ["chromadb"],
+    "lmm": ["pillow", "replicate"],
+    "graphs": ["matplotlib~=3.8.1", "networkx~=3.2.1"],
+}
 
 setuptools.setup(
     name="pyautogen",
@@ -33,33 +54,12 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/microsoft/autogen",
     packages=setuptools.find_packages(include=["autogen*"], exclude=["test"]),
-    # package_data={
-    #     "autogen.default": ["*/*.json"],
-    # },
-    # include_package_data=True,
     install_requires=install_requires,
-    extras_require={
-        "test": [
-            "coverage>=5.3",
-            "ipykernel",
-            "nbconvert",
-            "nbformat",
-            "pre-commit",
-            "pytest-asyncio",
-            "pytest>=6.1.1",
-        ],
-        "blendsearch": ["flaml[blendsearch]"],
-        "mathchat": ["sympy", "pydantic==1.10.9", "wolframalpha"],
-        "retrievechat": ["chromadb", "sentence_transformers", "pypdf", "ipython"],
-        "autobuild": ["chromadb", "sentence-transformers", "huggingface-hub"],
-        "teachable": ["chromadb"],
-        "lmm": ["replicate", "pillow"],
-        "graphs": ["networkx~=3.2.1", "matplotlib~=3.8.1"],
-    },
+    extras_require=extras_require,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.8, <3.12",
+    python_requires=">=3.9, <3.13",
 )
