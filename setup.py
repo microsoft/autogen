5e9fdc3a5871ba8c5ecea23dfb444398848c33c4
bitnom <14287229+bitnom@users.noreply.github.com>
2024-01-07 23:50:03 -0500

    don't support 3.12 yet https://github.com/microsoft/autogen/pull/1155#discussion_r1444098777

--- setup.py
+++ setup.py
@@ -61,5 +61,5 @@ setuptools.setup(
         "License :: OSI Approved :: MIT License",
         "Operating System :: OS Independent",
     ],
-    python_requires=">=3.9, <3.13",
+    python_requires=">=3.9, <3.12",
 )
