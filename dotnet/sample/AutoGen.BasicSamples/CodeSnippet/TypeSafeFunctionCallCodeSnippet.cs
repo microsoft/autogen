65beb33640278a7d3e49945489977624a5c2f698
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-01-09 11:49:08 -0800

    add type safe function call document

---
+++ dotnet/sample/AutoGen.BasicSamples/CodeSnippet/TypeSafeFunctionCallCodeSnippet.cs
@@ -0,0 +1,80 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// TypeSafeFunctionCall.cs
+
+using System.Text.Json;
+using AutoGen;
+using Azure.AI.OpenAI;
+
+#region code_snippet_3
+public partial class TypeSafeFunctionCall
+{
+    // file: FunctionCall.cs
+    [Function]
+    public async Task<string> UpperCase(string input)
+    {
+        var result = input.ToUpper();
+        return result;
+    }
+}
+#endregion code_snippet_3
+
+public class TypeSafeFunctionCallCodeSnippet
+{
+    public async Task<string> UpperCase(string input)
+    {
+        var result = input.ToUpper();
+        return result;
+    }
+
+    #region code_snippet_1
+    // file: FunctionDefinition.generated.cs
+    public FunctionDefinition UpperCaseFunction
+    {
+        get => new FunctionDefinition
+        {
+            Name = @"UpperCase",
+            Description = "convert input to upper case",
+            Parameters = BinaryData.FromObjectAsJson(new
+            {
+                Type = "object",
+                Properties = new
+                {
+                    input = new
+                    {
+                        Type = @"string",
+                        Description = @"input",
+                    },
+                },
+                Required = new[]
+                {
+                        "input",
+                    },
+            },
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            })
+        };
+    }
+    #endregion code_snippet_1
+
+    #region code_snippet_2
+    // file: FunctionDefinition.generated.cs
+    private class UpperCaseSchema
+    {
+        public string input { get; set; }
+    }
+
+    public Task<string> UpperCaseWrapper(string arguments)
+    {
+        var schema = JsonSerializer.Deserialize<UpperCaseSchema>(
+            arguments,
+            new JsonSerializerOptions
+            {
+                PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+            });
+
+        return UpperCase(schema.input);
+    }
+    #endregion code_snippet_2
+}
--- dotnet/website/articles/Create-type-safe-function-call.md
+++ dotnet/website/articles/Create-type-safe-function-call.md
@@ -1,1 +1,35 @@
-## TBD
+## Type-safe function call
+
+`AutoGen` provides a source generator to easness the trouble of manually craft function definition and function call wrapper from a function. To use this feature, simply add the `AutoGen.SourceGenerator` package to your project and decorate your function with [`Function`](../api/AutoGen.FunctionAttribute.yml) attribute.
+
+```xml
+<ItemGroup>
+    <PackageReference Include="AutoGen.SourceGenerator" />
+</ItemGroup>
+```
+
+```xml
+<PropertyGroup>
+    <!-- This enables structural xml document support -->
+    <GenerateDocumentationFile>true</GenerateDocumentationFile>
+</PropertyGroup>
+```
+
+> [!NOTE]
+> It's recommended to enable structural xml document support by setting `GenerateDocumentationFile` property to true in your project file. This allows source generator to leverage the documentation of the function when generating the function definition.
+
+Then, for the methods you want to generate function definition and function call wrapper, mark them with `Function` attribute:
+
+[!code-csharp[](../../sample/AutoGen.BasicSamples/CodeSnippet/TypeSafeFunctionCallCodeSnippet.cs?name=code_snippet_3)]
+
+> [!NOTE]
+> A `public partial` class is required for the source generator to generate code.
+
+> [!TIP]
+> For the best of performance, try using primitive types for the parameters and return type.
+
+The source generator will generate the following code based on the method signature and documentation. It helps you save the effort of writing function definition and keep it up to date with the actual method signature.
+
+[!code-csharp[](../../sample/AutoGen.BasicSamples/CodeSnippet/TypeSafeFunctionCallCodeSnippet.cs?name=code_snippet_1)]
+
+[!code-csharp[](../../sample/AutoGen.BasicSamples/CodeSnippet/TypeSafeFunctionCallCodeSnippet.cs?name=code_snippet_2)]
