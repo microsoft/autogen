58150a7a8fe7337d06f8107aa8a79f8777023aea
Chi Wang <wang.chi@microsoft.com>
2024-01-15 23:06:14 +0000

    None check

--- autogen/oai/client.py
+++ autogen/oai/client.py
@@ -229,7 +229,8 @@ class OpenAIWrapper:
             full_config = {**config, **self._config_list[i]}
             # separate the config into create_config and extra_kwargs
             create_config, extra_kwargs = self._separate_create_config(full_config)
-            if extra_kwargs.get("api_type").startswith("azure") and "model" in create_config:
+            api_type = extra_kwargs.get("api_type")
+            if api_type and api_type.startswith("azure") and "model" in create_config:
                 create_config["model"] = create_config["model"].replace(".", "")
             # construct the create params
             params = self._construct_create_params(create_config, extra_kwargs)
