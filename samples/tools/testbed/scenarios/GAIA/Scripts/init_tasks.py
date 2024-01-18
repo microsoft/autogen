ec16e52d406eb62d0340a199ba2a2b8c9c74c43e
Adam Fourney <adamfo@microsoft.com>
2024-01-18 15:10:00 -0800

    Added SocietyOfMind scenario to GAIA.

--- samples/tools/testbed/scenarios/GAIA/Scripts/init_tasks.py
+++ samples/tools/testbed/scenarios/GAIA/Scripts/init_tasks.py
@@ -62,7 +62,9 @@ def create_jsonl(name, tasks, files_dir, template):
                         "__FILE_NAME__": task["file_name"],
                         "__PROMPT__": task["Question"],
                     },
-                    "expected_answer.txt": {"__EXPECTED_ANSWER__": task["Final answer"]},
+                    "expected_answer.txt": {
+                        "__EXPECTED_ANSWER__": task["Final answer"]
+                    },
                 },
             }
 
@@ -77,7 +79,9 @@ def main():
     gaia_test_files = os.path.join(REPO_DIR, "2023", "test")
 
     if not os.path.isdir(gaia_validation_files) or not os.path.isdir(gaia_test_files):
-        sys.exit(f"Error: '{REPO_DIR}' does not appear to be a copy of the GAIA repository.")
+        sys.exit(
+            f"Error: '{REPO_DIR}' does not appear to be a copy of the GAIA repository."
+        )
 
     # Load the GAIA data
     gaia_validation_tasks = [[], [], []]
@@ -92,7 +96,10 @@ def main():
             data = json.loads(line)
             gaia_test_tasks[data["Level"] - 1].append(data)
 
-    templates = {"two_agents": os.path.join(TEMPLATES_DIR, "BasicTwoAgents")}
+    templates = {
+        "two_agents": os.path.join(TEMPLATES_DIR, "BasicTwoAgents"),
+        "soc": os.path.join(TEMPLATES_DIR, "SocietyOfMind"),
+    }
 
     # Add coding directories if needed (these are usually empty and left out of the repo)
     for template in templates.values():
