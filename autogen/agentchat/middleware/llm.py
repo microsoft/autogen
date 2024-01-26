658262d2519bb3cd3596768c2ec123d5324a4ade
Davor Runje <davor@airt.ai>
2024-01-26 14:55:21 +0100

    test coverage imporoved on llm middleware

--- autogen/agentchat/middleware/llm.py
+++ autogen/agentchat/middleware/llm.py
@@ -9,6 +9,16 @@ from autogen.agentchat.agent import Agent
 from autogen.cache.cache import Cache
 from autogen.oai.client import OpenAIWrapper
 
+try:
+    import openai
+except ImportError:  # pragma: no cover
+    ERROR: Optional[ImportError] = ImportError("Please install openai>=1 and diskcache to use autogen.OpenAIWrapper.")
+    ChatCompletionMessage = object
+else:  # pragma: no cover
+    # raises exception if openai>=1 is installed and something is wrong with imports
+    from openai.types.chat.chat_completion import ChatCompletionMessage  # type: ignore [attr-defined,assignment]
+
+
 logger = logging.getLogger(__name__)
 
 
@@ -30,27 +40,22 @@ class LLMMiddleware:
         system_message: Union[str, List[Dict[str, str]]] = "You are a helpful AI Assistant.",
         cache: Optional[Cache] = None,
     ) -> None:
-        if isinstance(system_message, str):
-            self._oai_system_messages: List[Dict[str, str]] = [{"content": system_message, "role": "system"}]
-        elif isinstance(system_message, list):
-            self._oai_system_messages = system_message
-        else:
-            raise ValueError(f"system_message must be a string or a list of messages, but got {system_message}")
+        self.system_messages = system_message  # type: ignore[assignment]
         if llm_config is None:
             raise ValueError("llm_config must be provided")
-        if not (isinstance(llm_config, dict) or isinstance(llm_config, bool)):
-            raise ValueError(f"llm_config must be a dict or bool, but got {llm_config}")
+        if not (isinstance(llm_config, dict)) and llm_config is not False:
+            raise ValueError(f"llm_config must be a dict or False, but got {llm_config}")
         self._name = name
         self._llm_config = llm_config
-        self._client = OpenAIWrapper(**self._llm_config) if llm_config else None  # type: ignore[arg-type]
-        self._client_cache = cache
+        self.client = OpenAIWrapper(**self._llm_config) if llm_config else None  # type: ignore[arg-type]
+        self.client_cache = cache
 
     @property
     def system_messages(self) -> List[Dict[str, str]]:
         return self._oai_system_messages
 
     @system_messages.setter
-    def system_messages(self, system_message: List[Dict[str, str]]) -> None:
+    def system_messages(self, system_message: Union[str, List[Dict[str, str]]]) -> None:
         if isinstance(system_message, str):
             self._oai_system_messages = [{"content": system_message, "role": "system"}]
         elif isinstance(system_message, list):
@@ -97,7 +102,7 @@ class LLMMiddleware:
             if final:
                 return reply
             else:
-                return next(messages, sender)  # type: ignore[no-any-return, misc]
+                return next(messages, sender)  # type: ignore[no-any-return, misc]  # pragma: no cover
 
     async def a_call(
         self,
@@ -122,9 +127,9 @@ class LLMMiddleware:
             if final:
                 return reply
             else:
-                return await next(messages, sender)  # type: ignore[no-any-return, misc]
+                return await next(messages, sender)  # type: ignore[no-any-return, misc]  # pragma: no cover
 
-    def update_function_signature(self, func_sig: Union[str, Dict[str, Any]], is_remove: None) -> None:
+    def update_function_signature(self, func_sig: Union[str, Dict[str, Any]], is_remove: bool) -> None:
         """update a function_signature in the LLM configuration for function_call.
 
         Args:
@@ -138,13 +143,13 @@ class LLMMiddleware:
         if not isinstance(self._llm_config, dict):
             error_msg = "To update a function signature, agent must have an llm_config"
             logger.error(error_msg)
-            raise AssertionError(error_msg)
+            raise ValueError(error_msg)
 
         if is_remove:
             if "functions" not in self._llm_config.keys():
                 error_msg = "The agent config doesn't have function {name}.".format(name=func_sig)
                 logger.error(error_msg)
-                raise AssertionError(error_msg)
+                raise ValueError(error_msg)
             else:
                 self._llm_config["functions"] = [
                     func for func in self._llm_config["functions"] if func["name"] != func_sig
@@ -171,16 +176,16 @@ class LLMMiddleware:
             is_remove: whether removing the tool from llm_config with name 'tool_sig'
         """
 
-        if not self._llm_config:
+        if not isinstance(self._llm_config, dict):
             error_msg = "To update a tool signature, agent must have an llm_config"
             logger.error(error_msg)
-            raise AssertionError(error_msg)
+            raise ValueError(error_msg)
 
         if is_remove:
             if "tools" not in self._llm_config.keys():
                 error_msg = "The agent config doesn't have tool {name}.".format(name=tool_sig)
                 logger.error(error_msg)
-                raise AssertionError(error_msg)
+                raise ValueError(error_msg)
             else:
                 self._llm_config["tools"] = [
                     tool for tool in self._llm_config["tools"] if tool["function"]["name"] != tool_sig
@@ -201,17 +206,9 @@ class LLMMiddleware:
 
         self._client = OpenAIWrapper(**self._llm_config)
 
-    def _generate_oai_reply(
-        self,
-        messages: List[Dict[str, Any]],
-        config: Optional[OpenAIWrapper] = None,
-    ) -> Tuple[bool, Optional[Union[str, Dict[str, Any]]]]:
-        """Generate a reply using autogen.oai."""
-        client = self._client if config is None else config
-        if client is None:
-            return False, None
-
-        # unroll tool_responses
+    @staticmethod
+    def _unroll_tool_responses(messages: List[Dict[str, Any]]) -> List[Dict[str, Any]]:
+        """Unroll tool_responses in messages."""
         all_messages = []
         for message in messages:
             tool_responses = message.get("tool_responses", [])
@@ -219,30 +216,57 @@ class LLMMiddleware:
                 all_messages += tool_responses
                 # tool role on the parent message means the content is just concatenation of all of the tool_responses
                 if message.get("role") != "tool":
+                    # todo: test this line, it is not covered by any tests
                     all_messages.append({key: message[key] for key in message if key != "tool_responses"})
             else:
                 all_messages.append(message)
+        return all_messages
+
+    @staticmethod
+    def _ensure_function_n_tool_calls_are_valid(
+        extracted_response: Union[str, ChatCompletionMessage]
+    ) -> Union[str, Dict[str, Any], ChatCompletionMessage]:
+        # get dictionary
+        extracted_response = extracted_response if isinstance(extracted_response, str) else model_dump(extracted_response)  # type: ignore[arg-type]
+
+        if isinstance(extracted_response, dict):
+            # todo: this is not covered by any tests
+            extracted_response = extracted_response
+            if extracted_response.get("function_call"):
+                extracted_response["function_call"]["name"] = LLMMiddleware._normalize_name(
+                    extracted_response["function_call"]["name"]
+                )
+            for tool_call in extracted_response.get("tool_calls") or []:
+                tool_call["function"]["name"] = LLMMiddleware._normalize_name(tool_call["function"]["name"])
+
+        return extracted_response
+
+    def _generate_oai_reply(
+        self,
+        messages: List[Dict[str, Any]],
+        config: Optional[OpenAIWrapper] = None,
+    ) -> Tuple[bool, Optional[Union[str, Dict[str, Any]]]]:
+        """Generate a reply using autogen.oai."""
+        client = self.client if config is None else config
+        if client is None:
+            return False, None
+
+        all_messages = LLMMiddleware._unroll_tool_responses(messages)
 
         # TODO: #1143 handle token limit exceeded error
         response = client.create(
             context=messages[-1].pop("context", None),
             messages=self._oai_system_messages + all_messages,
-            cache=self._client_cache,
+            cache=self.client_cache,
         )
 
         extracted_response = client.extract_text_or_completion_object(response)[0]
 
-        # ensure function and tool calls will be accepted when sent back to the LLM
-        if not isinstance(extracted_response, str):
-            extracted_response = model_dump(extracted_response)  # type: ignore[assignment]
-        if isinstance(extracted_response, dict):
-            if extracted_response.get("function_call"):
-                extracted_response["function_call"]["name"] = self._normalize_name(
-                    extracted_response["function_call"]["name"]
-                )
-            for tool_call in extracted_response.get("tool_calls") or []:
-                tool_call["function"]["name"] = self._normalize_name(tool_call["function"]["name"])
-        return True, extracted_response  # type: ignore[return-value]
+        extracted_response_valid: Union[
+            str, Dict[str, Any], ChatCompletionMessage
+        ] = LLMMiddleware._ensure_function_n_tool_calls_are_valid(extracted_response)
+
+        return True, extracted_response_valid  # type: ignore[return-value]
 
     async def _a_generate_oai_reply(
         self,
@@ -256,25 +280,19 @@ class LLMMiddleware:
 
     def print_usage_summary(self, mode: Union[str, List[str]] = ["actual", "total"]) -> None:
         """Print the usage summary."""
-        if self._client is None:
+        if self.client is None:
             print(f"No cost incurred from agent '{self._name}'.")
         else:
             print(f"Agent '{self._name}':")
-            self._client.print_usage_summary(mode)
+            self.client.print_usage_summary(mode)
 
-    def get_actual_usage(self) -> Union[None, Dict[str, int]]:
+    def get_actual_usage(self) -> Optional[Dict[str, int]]:
         """Get the actual usage summary."""
-        if self._client is None:
-            return None
-        else:
-            return self._client.actual_usage_summary
+        return self.client if self.client is None else self._client.actual_usage_summary  # type: ignore[union-attr]
 
-    def get_total_usage(self) -> Union[None, Dict[str, int]]:
+    def get_total_usage(self) -> Optional[Dict[str, int]]:
         """Get the total usage summary."""
-        if self._client is None:
-            return None
-        else:
-            return self._client.total_usage_summary
+        return self.client if self.client is None else self.client.total_usage_summary
 
     @staticmethod
     def _normalize_name(name: str) -> str:
--- test/agentchat/middleware/test_llm.py
+++ test/agentchat/middleware/test_llm.py
@@ -1,6 +1,11 @@
-from typing import Any, Dict
+import inspect
+from typing import Any, Dict, List, Optional, Union
+import unittest
+from unittest.mock import AsyncMock, MagicMock
+from openai import completions
 
 import pytest
+from autogen.oai.client import OpenAIWrapper
 from conftest import skip_openai
 
 import autogen
@@ -8,10 +13,14 @@ from autogen.agentchat.middleware.llm import LLMMiddleware
 
 try:
     import openai
-except ImportError:
+except ImportError:  # pragma: no cover
     skip = True
-else:
+    Completions = object
+    CompletionChoice = object
+else:  # pragma: no cover
     skip = False or skip_openai
+    from openai.types.completion import Completion, CompletionChoice  # type: ignore [assignment, attr-defined]
+
 
 KEY_LOC = "notebook"
 OAI_CONFIG_LIST = "OAI_CONFIG_LIST"
@@ -67,40 +76,108 @@ _tools = [
     },
 ]
 
+funny_system_message = "You are a funny AI Assistant."
+
+
+@pytest.mark.parametrize(
+    "system_message",
+    [
+        None,
+        funny_system_message,
+        [{"content": funny_system_message, "role": "system"}],
+        {"content": "should fail on dict", "role": "system"},
+    ],
+)
+def test_init_system_message(system_message: Optional[Union[str, List[Dict[str, str]]]]) -> None:
+    if isinstance(system_message, dict):
+        with pytest.raises(ValueError) as e:
+            md = LLMMiddleware(name="assistant", llm_config=False, system_message=system_message)
+        assert "system_message must be a string or a list of messages, but got" in str(e.value)
+    elif system_message is None:
+        md = LLMMiddleware(name="assistant", llm_config=False)
+
+        assert md.system_messages == [{"content": "You are a helpful AI Assistant.", "role": "system"}]
+
+        with pytest.raises(ValueError) as e:
+            md = LLMMiddleware(name="assistant", llm_config=False, system_message=None)  # type: ignore[arg-type]
+        assert "system_message must be a string or a list of messages, but got" in str(e.value)
+    else:
+        for llm_config in [False, {}]:
+            md = LLMMiddleware(name="assistant", llm_config=llm_config, system_message=system_message)  # type: ignore[arg-type]
+
+        assert md.system_messages == [{"content": funny_system_message, "role": "system"}]
+
+        with pytest.raises(ValueError) as e:
+            md = LLMMiddleware(name="assistant", llm_config=None, system_message=system_message)
+        assert "llm_config must be provided" in str(e.value)
+
+        with pytest.raises(ValueError) as e:
+            md = LLMMiddleware(name="assistant", llm_config=True, system_message=system_message)  # type: ignore[arg-type]
+        assert "llm_config must be a dict or False, but got" in str(e.value)
+
+        with pytest.raises(ValueError) as e:
+            md = LLMMiddleware(name="assistant", llm_config="whatever", system_message=system_message)  # type: ignore[arg-type]
+        assert "llm_config must be a dict or False, but got" in str(e.value)
+
+
+_config_list = autogen.config_list_from_json(
+    env_or_file=OAI_CONFIG_LIST,
+    file_location=KEY_LOC,
+    filter_dict={"model": ["gpt-3.5-turbo", "gpt-35-turbo"]},
+)
+
 
 @pytest.mark.skipif(skip, reason="openai not installed or requested to skip")
-def test_llm() -> None:
-    config_list = autogen.config_list_from_json(
-        env_or_file=OAI_CONFIG_LIST,
-        file_location=KEY_LOC,
-        filter_dict={"model": ["gpt-3.5-turbo", "gpt-35-turbo"]},
-    )
-    llm_config = {
-        "cache_seed": None,
-        "config_list": config_list,
-    }
+@pytest.mark.parametrize(
+    "llm_config, messages, retval, next_called",
+    [
+        # should call next
+        (False, [{"role": "user", "content": "1+1="}], "Hello world!", True),
+        # should get response from openai and not call next
+        ({"cache_seed": None, "config_list": _config_list}, [{"role": "user", "content": "1+1="}], "2", False),
+    ],
+)
+def test_llm(llm_config: Dict[str, Any], messages: List[Dict[str, str]], retval: str, next_called: bool) -> None:
     mw = LLMMiddleware(name="assistant", llm_config=llm_config, system_message="You are a helpful assistant.")
-    messages = [{"role": "user", "content": "1+1="}]
-    reply: Dict[str, Any] = mw.call(messages=messages)  # type: ignore[assignment]
-    assert "2" in reply
+
+    next = MagicMock(return_value="Hello world!")
+
+    reply: Dict[str, Any] = mw.call(messages=messages, next=next)  # type: ignore[assignment]
+
+    assert retval in reply
+
+    if next_called:
+        next.assert_called_once()
+    else:
+        next.assert_not_called()
 
 
 @pytest.mark.asyncio()
 @pytest.mark.skipif(skip, reason="openai not installed or requested to skip")
-async def test_llm_async() -> None:
-    config_list = autogen.config_list_from_json(
-        env_or_file=OAI_CONFIG_LIST,
-        file_location=KEY_LOC,
-        filter_dict={"model": ["gpt-3.5-turbo", "gpt-35-turbo"]},
-    )
-    llm_config = {
-        "cache_seed": None,
-        "config_list": config_list,
-    }
+@pytest.mark.parametrize(
+    "llm_config, messages, retval, next_awaited",
+    [
+        # should call next
+        (False, [{"role": "user", "content": "1+1="}], "Hello world!", True),
+        # should get response from openai and not call next
+        ({"cache_seed": None, "config_list": _config_list}, [{"role": "user", "content": "1+1="}], "2", False),
+    ],
+)
+async def test_llm_async(
+    llm_config: Dict[str, Any], messages: List[Dict[str, str]], retval: str, next_awaited: bool
+) -> None:
     mw = LLMMiddleware(name="assistant", llm_config=llm_config, system_message="You are a helpful assistant.")
-    messages = [{"role": "user", "content": "1+1="}]
-    reply: Dict[str, Any] = await mw.a_call(messages=messages)  # type: ignore[assignment]
-    assert "2" in reply
+
+    next = AsyncMock(return_value="Hello world!")
+
+    reply: Dict[str, Any] = await mw.a_call(messages=messages, next=next)  # type: ignore[assignment]
+
+    assert retval in reply
+
+    if next_awaited:
+        next.assert_awaited_once()
+    else:
+        next.assert_not_awaited()
 
 
 @pytest.mark.skipif(skip, reason="openai not installed or requested to skip")
@@ -149,3 +226,183 @@ def test_llm_tool_calls_parallel() -> None:
     assert len(reply["tool_calls"]) == 2  # type: ignore[index]
     assert reply["tool_calls"][0]["function"]["name"] == "get_current_weather"  # type: ignore[index]
     assert reply["tool_calls"][1]["function"]["name"] == "get_current_weather"  # type: ignore[index]
+
+
+_exec_python_function_sig = {
+    "description": "run cell in ipython and return the execution result.",
+    "name": "python",
+    "parameters": {
+        "properties": {"cell": {"description": "Valid Python cell to execute.", "type": "string"}},
+        "required": ["cell"],
+        "type": "object",
+    },
+}
+
+_exec_python_tool_sig = {
+    "type": "function",
+    "function": _exec_python_function_sig,
+}
+
+_exec_sh_function_sig = {
+    "name": "sh",
+    "description": "run a shell script and return the execution result.",
+    "parameters": {
+        "type": "object",
+        "properties": {
+            "script": {
+                "type": "string",
+                "description": "Valid shell script to execute.",
+            }
+        },
+        "required": ["script"],
+    },
+}
+
+_exec_sh_tool_sig = {
+    "type": "function",
+    "function": _exec_sh_function_sig,
+}
+
+
+def test_update_function_signature() -> None:
+    md = LLMMiddleware(name="agent1", llm_config=False)
+    with pytest.raises(ValueError) as e:
+        md.update_function_signature({}, is_remove=False)
+
+    assert "To update a function signature, agent must have an llm_config" in str(e.value)
+
+    with pytest.MonkeyPatch.context() as mp:
+        mp.setenv("OPENAI_API_KEY", "mock")
+        md = LLMMiddleware(name="agent1", llm_config={})
+
+        md.update_function_signature(_exec_python_function_sig, is_remove=False)
+        assert md.client._config_list[0]["functions"] == [_exec_python_function_sig]  # type: ignore[union-attr]
+
+        md.update_function_signature(_exec_sh_function_sig, is_remove=False)
+        assert md.client._config_list[0]["functions"] == [_exec_python_function_sig, _exec_sh_function_sig]  # type: ignore[union-attr]
+
+        md.update_function_signature(_exec_sh_function_sig, is_remove=False)
+        assert md.client._config_list[0]["functions"] == [_exec_python_function_sig, _exec_sh_function_sig]  # type: ignore[union-attr]
+
+        md.update_function_signature(_exec_sh_function_sig["name"], is_remove=True)  # type: ignore[arg-type]
+        assert md.client._config_list[0]["functions"] == [_exec_python_function_sig]  # type: ignore[union-attr]
+
+        md.update_function_signature(_exec_python_function_sig["name"], is_remove=True)  # type: ignore[arg-type]
+        assert "functions" not in md.client._config_list[0]  # type: ignore[union-attr]
+
+        with pytest.raises(ValueError) as e:
+            md.update_function_signature(_exec_python_function_sig["name"], is_remove=True)  # type: ignore[arg-type]
+        assert "The agent config doesn't have function" in str(e.value)
+
+        md.update_function_signature(_exec_python_function_sig, is_remove=False)
+        assert md.client._config_list[0]["functions"] == [_exec_python_function_sig]  # type: ignore[union-attr]
+
+        # I guess this is fine, but it's not what I expected
+        md.update_function_signature("some_random_name", is_remove=True)
+        assert md.client._config_list[0]["functions"] == [_exec_python_function_sig]  # type: ignore[union-attr]
+
+
+def test_update_tools_signature() -> None:
+    md = LLMMiddleware(name="agent1", llm_config=False)
+    with pytest.raises(ValueError) as e:
+        md.update_tool_signature({}, is_remove=False)
+
+    assert "To update a tool signature, agent must have an llm_config" in str(e.value)
+
+    with pytest.MonkeyPatch.context() as mp:
+        mp.setenv("OPENAI_API_KEY", "mock")
+        md = LLMMiddleware(name="agent1", llm_config={})
+
+        md.update_tool_signature(_exec_python_tool_sig, is_remove=False)
+        assert md.client._config_list[0]["tools"] == [_exec_python_tool_sig]  # type: ignore[union-attr]
+
+        md.update_tool_signature(_exec_sh_tool_sig, is_remove=False)
+        assert md.client._config_list[0]["tools"] == [_exec_python_tool_sig, _exec_sh_tool_sig]  # type: ignore[union-attr]
+
+        md.update_tool_signature(_exec_sh_tool_sig, is_remove=False)
+        assert md.client._config_list[0]["tools"] == [_exec_python_tool_sig, _exec_sh_tool_sig]  # type: ignore[union-attr]
+
+        md.update_tool_signature(_exec_sh_tool_sig["function"]["name"], is_remove=True)  # type: ignore[index]
+        assert md.client._config_list[0]["tools"] == [_exec_python_tool_sig]  # type: ignore[union-attr]
+
+        md.update_tool_signature(_exec_python_tool_sig["function"]["name"], is_remove=True)  # type: ignore[index]
+        assert "tools" not in md.client._config_list[0]  # type: ignore[union-attr]
+
+        with pytest.raises(ValueError) as e:
+            md.update_tool_signature(_exec_python_tool_sig["function"]["name"], is_remove=True)  # type: ignore[index]
+        assert "The agent config doesn't have tool" in str(e.value)
+
+        md.update_tool_signature(_exec_python_tool_sig, is_remove=False)
+        assert md.client._config_list[0]["tools"] == [_exec_python_tool_sig]  # type: ignore[union-attr]
+
+        # I guess this is fine, but it's not what I expected
+        md.update_tool_signature("some_random_name", is_remove=True)  # type: ignore[arg-type]
+        assert md.client._config_list[0]["tools"] == [_exec_python_tool_sig]  # type: ignore[union-attr]
+
+
+_tool_response = {
+    "role": "tool",
+    "tool_responses": [
+        {"tool_call_id": "tool_1", "role": "tool", "content": "hello world"},
+        {
+            "tool_call_id": "tool_2",
+            "role": "tool",
+            "content": "goodbye and thanks for all the fish",
+        },
+        {
+            "tool_call_id": "tool_3",
+            "role": "tool",
+            "content": "Error: Function multi_tool_call_echo not found.",
+        },
+    ],
+    "content": inspect.cleandoc(
+        """
+        Tool Call Id: tool_1
+        hello world
+
+        Tool Call Id: tool_2
+        goodbye and thanks for all the fish
+
+        Tool Call Id: tool_3
+        Error: Function multi_tool_call_echo not found.
+        """
+    ),
+}
+
+
+@pytest.mark.parametrize(
+    "llm_config, messages, config, expected_final, expected_retval",
+    [
+        (False, [], None, False, None),
+        ({"cache_seed": None, "config_list": _config_list}, [{"role": "user", "content": "1+1="}], None, True, "2"),
+        ({"cache_seed": None, "config_list": _config_list}, [_tool_response], None, True, "Good job with tool!"),
+        ({"cache_seed": None, "config_list": _config_list}, [_tool_response], None, True, "Good job with tool!"),
+    ],
+)
+def test_generate_oai_reply_mocked_openai(
+    llm_config: Dict[str, Any],
+    messages: List[Dict[str, Any]],
+    config: Optional[OpenAIWrapper],
+    expected_final: bool,
+    expected_retval: str,
+) -> None:
+    md = LLMMiddleware(name="agent1", llm_config=llm_config)
+    if len(messages) and ("tool_responses" in messages[-1]):
+        with unittest.mock.patch("autogen.oai.client.OpenAIWrapper.create") as client_create_mock:
+            choices = [CompletionChoice(text="Good job with tool!", index=0, finish_reason="stop")]  # type: ignore[call-arg]
+            completion = Completion(
+                choices=choices, id="completion_id", created=0, model="model", object="text_completion"
+            )
+            client_create_mock.return_value = completion
+            final, retval = md._generate_oai_reply(messages, config)
+            assert final == expected_final
+            assert retval == expected_retval
+            client_create_mock.assert_called_once()
+        pass
+    else:
+        final, retval = md._generate_oai_reply(messages, config)
+        assert final == expected_final
+        if isinstance(retval, str):
+            assert expected_retval in retval
+        else:
+            assert retval == expected_retval  # type: ignore[comparison-overlap]
