cd199c7ab7e86fab747dac2d3cceb0ea9e5e97c6
afourney <adamfo@microsoft.com>
2024-01-25 16:46:58 -0800

    Introduces AutoGenBench (#1048)
    * Initial commit of AutoGenBench
    
    * wording
    
    * typo
    
    * pre-commit reformulation
    
    * Updated README to point to contributor's guide earlier.
    
    * Simplified the description of the JSON format.
    
    * Added print statements to indicate when run.sh and scenario.py are starting.
    
    * Added SocietyOfMind scenario to GAIA.
    
    * Pointing autogenbench clone command to the latest branch.
    
    * Temporarily disable subsample option.
    
    * Updated the GAIA readme to specify how to define a BING API key.
    
    * Fixed and re-enabled the subsample option.
    
    * Added a draft of a blog post.
    
    * Updated authors.
    
    * Incorporating Gagan's feedback.
    
    * Fixed code formatting.
    
    * Updated the help string in the docs.
    
    * Light editing of the AutoGenBench blogpost.
    
    * Support filtering on model tags.
    
    * Added websurfer dependencies to Dockerfile.
    
    * Renamed testbed -> autogenbench
    
    * Attempting to fix formatting.
    
    * Added more gracefull handling of task timeouts (the script is allowed to terminate before Docker is stopped).
    
    * Updated the blogpost based on Saleema's and Julia's feedback.
    
    * Fixed formatting... again.
    
    * Added a main MANIFEST to list available scenarios.
    
    * Limit main manifest to directories.
    
    * Manifests now use relative paths.
    
    * All manifests are now relative.
    
    * Updated the contributing guide, and address windows path issues.
    
    * Updated the version. Fixed formatting.
    
    * Fixed formatting.
    
    * De-listing Examples, since it has no clear tabulate criteria.
    
    * Updated email in pyproject
    
    * typo in blogpost
    
    * wording
    
    ---------
    
    Co-authored-by: Qingyun Wu <qingyun.wu@psu.edu>
    Co-authored-by: Qingyun Wu <qingyun0327@gmail.com>

---
+++ samples/tools/autogenbench/scenarios/HumanEval/Templates/TwoAgents/coding/my_tests.py
@@ -0,0 +1,13 @@
+# Disable ruff linter for template files
+# ruff: noqa: F821 E722
+
+__TEST__
+
+
+def run_tests(candidate):
+    try:
+        check(candidate)
+        # We can search for this string in the output
+        print("ALL TESTS PASSED !#!#\nTERMINATE")
+    except:
+        print("SOME TESTS FAILED - TRY AGAIN !#!#")
