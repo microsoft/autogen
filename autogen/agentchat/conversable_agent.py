cb42cdb3f6af09c0aa9c5c10a7580c54836aeff9
Qingyun Wu <qingyun0327@gmail.com>
2024-01-27 23:16:35 -0500

    remove condition on reply

--- autogen/agentchat/conversable_agent.py
+++ autogen/agentchat/conversable_agent.py
@@ -597,7 +597,7 @@ class ConversableAgent(Agent):
         if request_reply is False or request_reply is None and self.reply_at_receive[sender] is False:
             return
         reply = self.generate_reply(messages=self.chat_messages[sender], sender=sender)
-        if reply is not None and reply:
+        if reply is not None:
             self.send(reply, sender, silent=silent)
 
     async def a_receive(
