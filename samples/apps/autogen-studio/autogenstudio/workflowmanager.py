0981e3e6ccfd64af94d10f1d36e4136f60ccd051
Victor Dibia <victordibia@microsoft.com>
2024-01-27 20:22:00 -0800

    fix bug on workflowmanager

--- samples/apps/autogen-studio/autogenstudio/workflowmanager.py
+++ samples/apps/autogen-studio/autogenstudio/workflowmanager.py
@@ -121,7 +121,7 @@ class AutoGenWorkFlowManager:
                 print(" >> llm config", llm)
                 # check if api_key is present either in llm or env variable
                 if "api_key" not in llm and "OPENAI_API_KEY" not in os.environ:
-                    error_message = f"api_key is not present in llm_config or OPENAI_API_KEY env variable for agent ** {agent_spec.name}**. Update your workflow to provide an api_key to use the LLM."
+                    error_message = f"api_key is not present in llm_config or OPENAI_API_KEY env variable for agent ** {agent_spec.config.name}**. Update your workflow to provide an api_key to use the LLM."
                     raise ValueError(error_message)
 
                 # only add key if value is not None
