3dd126344ec1e7eca7a74bdaedc1fd139b5bcfec
Eric Zhu <ekzhu@users.noreply.github.com>
2024-01-30 17:31:12 -0800

    update tests

--- autogen/code_utils.py
+++ autogen/code_utils.py
@@ -459,6 +459,8 @@ def execute_code(
         f'{_cmd(lang)} "{filename}"; exit_code=$?; echo -n {exit_code_str}; echo -n $exit_code; echo {exit_code_str}',
     ]
     # create a docker container
+    from docker.types import Mount
+
     container = client.containers.run(
         image,
         command=cmd,
@@ -466,6 +468,7 @@ def execute_code(
         detach=True,
         # get absolute path to the working directory
         volumes={abs_path: {"bind": "/workspace", "mode": "rw"}},
+        mounts=[Mount("/workspace", abs_path, type="bind")],
     )
     start_time = time.time()
     while container.status != "exited" and time.time() - start_time < timeout:
