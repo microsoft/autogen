1193a5683952d686754d5ef8e01836caa758be7a
Jack Gerrits <jack@jackgerrits.com>
2024-02-05 08:53:00 -0500

    Add mypy config to pyproject.toml

--- setup.py
+++ setup.py
@@ -20,7 +20,8 @@ install_requires = [
     "flaml",
     "python-dotenv",
     "tiktoken",
-    "pydantic>=1.10,<3",  # could be both V1 and V2
+    # Disallowing 2.6.0 can be removed when this is fixed https://github.com/pydantic/pydantic/issues/8705
+    "pydantic>=1.10,<3,!=2.6.0",  # could be both V1 and V2
     "docker",
 ]
 
