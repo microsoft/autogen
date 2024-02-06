cc8330c0c7e44e53fe6de74b9f4f227a83a846a0
freedeaths <register917@gmail.com>
2024-02-06 10:53:15 +0800

    modify setup option from graphs to graph and add texts in optional-dependencies.md

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setuptools.setup(
         "autobuild": ["chromadb", "sentence-transformers", "huggingface-hub"],
         "teachable": ["chromadb"],
         "lmm": ["replicate", "pillow"],
-        "graphs": ["networkx", "matplotlib"],
+        "graph": ["networkx", "matplotlib"],
         "websurfer": ["beautifulsoup4", "markdownify", "pdfminer.six", "pathvalidate"],
         "redis": ["redis"],
     },
