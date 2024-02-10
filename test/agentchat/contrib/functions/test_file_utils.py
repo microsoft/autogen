084ab2e31fc9cb2ad8a6a23f6f7d15d427968b7e
gagb <gagb@users.noreply.github.com>
2024-02-10 10:27:47 +0000

    Improve test

--- test/agentchat/contrib/functions/test_file_utils.py
+++ test/agentchat/contrib/functions/test_file_utils.py
@@ -1,5 +1,17 @@
+import sys
 import os
+import pytest
 from autogen.agentchat.contrib.functions import file_utils as fu
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "..", ".."))
+from conftest import skip_openai  # noqa: E402
+
+try:
+    from openai import OpenAI
+except ImportError:
+    skip = True
+else:
+    skip = False or skip_openai
 
 TESTDIR = os.path.join(os.path.join(os.path.dirname(__file__), "..", "..", ".."), "test_files")
 
@@ -38,6 +50,10 @@ def test_read_text_from_xlsx():
 #     assert isinstance(text, str)
 
 
+@pytest.mark.skipif(
+    sys.platform in ["darwin", "win32"] or skip,
+    reason="do not run on MacOS or windows OR openai not installed OR requested to skip",
+)
 def test_caption_image_using_gpt4v():
     for file in ["example.jpg", "example.png"]:
         text = fu.caption_image_using_gpt4v(os.path.join(TESTDIR, file))
