f6865d8acd29296a1c647667a98c4b19585fa18f
Kosta Petan <Kosta.Petan@microsoft.com>
2024-02-21 11:38:17 +0100

    add Huber agent

---
+++ src/libs/Microsoft.AI.DevTeam/Actors/Hubber/Hubber.cs
@@ -0,0 +1,50 @@
+﻿using Orleans.Concurrency;
+using Orleans.Runtime;
+using Orleans.Streams;
+using Orleans.Timers;
+
+namespace Microsoft.AI.DevTeam;
+
+[StatelessWorker]
+[ImplicitStreamSubscription("Hubber")]
+public class Hubber : Grain, IGrainWithStringKey
+{
+    private readonly IManageGithub _ghService;
+
+
+    public Hubber( IManageGithub ghService)
+    {
+        _ghService = ghService;
+    }
+
+    public async override Task OnActivateAsync(CancellationToken cancellationToken)
+    {
+        var streamProvider = this.GetStreamProvider("StreamProvider");
+        var streamId = StreamId.Create("Hubber", this.GetPrimaryKeyString());
+        var stream = streamProvider.GetStream<Event>(streamId);
+
+        await stream.SubscribeAsync(HandleEvent);
+    }
+
+    public async Task HandleEvent(Event item, StreamSequenceToken? token)
+    {
+        switch (item.Type)
+        {
+            case EventType.NewAsk:
+                
+                break;
+            case EventType.NewAskReadme:
+                
+                break;
+            case EventType.ChainClosed:
+                
+                break;
+            default:
+                break;
+        }
+    }
+    public async Task ScheduleCommitSandboxRun(CommitRequest commitRequest, MarkTaskCompleteRequest markTaskCompleteRequest, SandboxRequest sandboxRequest)
+    {
+        
+    }
+}
