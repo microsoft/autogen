88c5fcae3e0dc5d71f51958fd437f4bbff2cd14c
Crimson206 <sisung.kim1@gmail.com>
2024-02-23 09:13:47 +0100

    replaced "powershell" by powershell_command in ...
    check_and_update_powershell_execution_policy()
    
    ------------- history --------------
    
    Note:
    test_execute_code_no_docker passed
     - if Win32 and powershell_command == "powershell" and not is_docker_running():
    
    test_execute_code_no_docker failed
     - if Win32 and not is_docker_running():
     - if Win32 and powershell_command == "powershell":
    
    common condition
    
    in check_and_update_powershell_execution_policy
     passed "powershell" only instead of powershell_command

--- autogen/code_utils.py
+++ autogen/code_utils.py
@@ -237,7 +237,7 @@ powershell_command = get_powershell_command()
 
 
 def check_and_update_powershell_execution_policy():
-    policy_check_command = ["powershell", "Get-ExecutionPolicy", "-Scope", "CurrentUser"]
+    policy_check_command = [powershell_command, "Get-ExecutionPolicy", "-Scope", "CurrentUser"]
     policy = subprocess.check_output(policy_check_command, text=True).strip()
 
     if policy == "Restricted":
