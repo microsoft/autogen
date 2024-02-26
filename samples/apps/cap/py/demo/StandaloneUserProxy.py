644f6e50583ae3ea651e12e84b8b5b5ff49c8b86
Rajan <rajan.chari@yahoo.com>
2024-02-26 17:38:26 -0500

    rename user_proxy -> user_proxy_conn

--- samples/apps/cap/py/demo/StandaloneUserProxy.py
+++ samples/apps/cap/py/demo/StandaloneUserProxy.py
@@ -22,11 +22,10 @@ class StandaloneUserProxy:
         user_proxy_adptr = CAP2AG(ag_agent=user_proxy, the_other_name="assistant", init_chat=True, self_recursive=True)
         network.register(user_proxy_adptr)
         network.connect()
-        
 
         # Send a message to the user_proxy
-        user_proxy = network.lookup_actor("user_proxy")
-        user_proxy.send_txt_msg("Plot a chart of MSFT daily closing prices for last 1 Month.")
+        user_proxy_conn = network.lookup_actor("user_proxy")
+        user_proxy_conn.send_txt_msg("Plot a chart of MSFT daily closing prices for last 1 Month.")
 
         # Hang around for a while
         while user_proxy_adptr.run:
