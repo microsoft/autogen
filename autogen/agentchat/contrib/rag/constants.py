8a107cdd9c4a7e37ba4550e28aa5adcb19b72d66
Li Jiang <bnujli@gmail.com>
2024-02-29 18:22:46 +0800

    Update TERMINATE_TRIGGER_WORDS

--- autogen/agentchat/contrib/rag/constants.py
+++ autogen/agentchat/contrib/rag/constants.py
@@ -3,4 +3,4 @@ import os
 RAG_MINIMUM_MESSAGE_LENGTH = os.environ.get("RAG_MINIMUM_MESSAGE_LENGTH", 5)
 CHROMADB_MAX_BATCH_SIZE = os.environ.get("CHROMADB_MAX_BATCH_SIZE", 40000)
 UPDATE_CONTEXT_TRIGGER_WORDS = ["update context", "however", "could you provide"]
-TERMINATE_TRIGGER_WORDS = ["terminate", "goodbye", "bye", "exit", "quit", "stop", "end"]
+TERMINATE_TRIGGER_WORDS = ["terminate", "exit", "quit"]
