077cb46e49be7282738cc0d1a5f7ee0fe92c91ee
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-02-29 00:10:09 -0800

    fix test

--- dotnet/sample/AutoGen.BasicSamples/Example04_Dynamic_GroupChat_Coding_Task.cs
+++ dotnet/sample/AutoGen.BasicSamples/Example04_Dynamic_GroupChat_Coding_Task.cs
@@ -4,7 +4,7 @@
 using AutoGen;
 using AutoGen.DotnetInteractive;
 using FluentAssertions;
-using autogen = AutoGen.Core.API.LLMConfigAPI;
+using autogen = AutoGen.LLMConfigAPI;
 
 public partial class Example04_Dynamic_GroupChat_Coding_Task
 {
