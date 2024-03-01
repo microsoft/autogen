f43a5b9787433f87dd32cc7bb4a296f1337ee805
Rajan <rajan.chari@yahoo.com>
2024-03-01 13:01:47 -0500

    Created an encapsulated CAP Pair for AutoGen pair communication

--- samples/apps/cap/py/demo/CAPAutoGenPairDemo.py
+++ samples/apps/cap/py/demo/CAPAutoGenPairDemo.py
@@ -1,8 +1,8 @@
 import time
 from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
 from autogencap.DebugLog import Info
+from autogencap.ag_adapter.Pair import Pair
 from autogencap.LocalActorNetwork import LocalActorNetwork
-from autogencap.ag_adapter.CAP2AG import CAP2AG
 
 
 def cap_ag_pair_demo():
@@ -14,28 +14,18 @@ def cap_ag_pair_demo():
         is_termination_msg=lambda x: "TERMINATE" in x.get("content"),
     )
 
-    # Composable Agent Network adapter
-
+    # Composable Agent Platform AutoGen Pair adapter
     network = LocalActorNetwork()
-    user_proxy_adptr = CAP2AG(ag_agent=user_proxy, the_other_name="assistant", init_chat=True, self_recursive=True)
-    assistant_adptr = CAP2AG(ag_agent=assistant, the_other_name="user_proxy", init_chat=False, self_recursive=True)
-
-    network.register(user_proxy_adptr)
-    network.register(assistant_adptr)
-    network.connect()
-
-    # Send a message to the user_proxy
-    user_proxy = network.lookup_actor("user_proxy")
-    user_proxy.send_txt_msg("Plot a chart of MSFT daily closing prices for last 1 Month.")
-
-    # Hang around for a while
-    try: 
-        while True:
+    
+    pair = Pair(network, user_proxy, assistant)
+    pair.initiate_chat("Plot a chart of MSFT daily closing prices for last 1 Month.")
+    
+    # Wait for the pair to finish
+    try:
+        while pair.running():
             time.sleep(0.5)
-            if (not user_proxy_adptr.run 
-                and not assistant_adptr.run):
-                break
     except KeyboardInterrupt:
-        print("Interrupted by user, shutting down.")
+        print("Interrupted by user, shutting down.")    
+    
     network.disconnect()
     Info("App", "App Exit")
