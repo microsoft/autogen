4787d6d65c51a8ba9532d98fb5c345c9b6230c72
Jack Gerrits <jackgerrits@users.noreply.github.com>
2024-03-02 14:45:28 -0500

    revert quarto upgrade (#1840)
    * revert quarto upgrade
    
    * fix ruff

--- website/process_notebooks.py
+++ website/process_notebooks.py
@@ -67,11 +67,11 @@ class Result:
 def check_quarto_bin(quarto_bin: str = "quarto") -> None:
     """Check if quarto is installed."""
     try:
-        version = subprocess.check_output([quarto_bin, "--version"], text=True).strip()
-        version = tuple(map(int, version.split(".")))
-        if version < (1, 5, 23):
-            print("Quarto version is too old. Please upgrade to 1.5.23 or later.")
-            sys.exit(1)
+        subprocess.check_output([quarto_bin, "--version"], text=True).strip()
+        # version = tuple(map(int, version.split(".")))
+        # if version < (1, 5, 23):
+        #     print("Quarto version is too old. Please upgrade to 1.5.23 or later.")
+        #     sys.exit(1)
 
     except FileNotFoundError:
         print("Quarto is not installed. Please install it from https://quarto.org")
