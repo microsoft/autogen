a663cf6cea659c1c441c8677b4a9a2c8aa43c8aa
Chi Wang <wang.chi@microsoft.com>
2024-03-03 03:16:17 +0000

    remove unnecessary imports

--- autogen/agentchat/contrib/capabilities/teachability.py
+++ autogen/agentchat/contrib/capabilities/teachability.py
@@ -2,7 +2,7 @@ import os
 from autogen.agentchat.assistant_agent import ConversableAgent
 from autogen.agentchat.contrib.capabilities.agent_capability import AgentCapability
 from autogen.agentchat.contrib.text_analyzer_agent import TextAnalyzerAgent
-from typing import Dict, Optional, Union, List, Tuple, Any
+from typing import Dict, Optional, Union
 import chromadb
 from chromadb.config import Settings
 import pickle
