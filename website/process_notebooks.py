6eeccccfbef43b1c89ad0592fb16c5255d77d25a
Jack Gerrits <jack@jackgerrits.com>
2024-03-04 08:43:09 -0500

    test paths

--- website/process_notebooks.py
+++ website/process_notebooks.py
@@ -318,6 +318,9 @@ def post_process_mdx(rendered_mdx: Path, source_notebooks: Path, front_matter: D
         front_matter = yaml.safe_load(content[4:front_matter_end])
         content = content[front_matter_end + 3 :]
 
+    print(Path(__file__).resolve())
+    print(Path(__file__).parent.resolve())
+    print(Path(__file__).parent.parent.resolve())
     repo_root = Path(__file__).parent.parent.resolve()
     repo_relative_notebook = source_notebooks.resolve().relative_to(repo_root)
     print(repo_relative_notebook)
