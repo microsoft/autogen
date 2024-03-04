8dc22205beb2f24ee8306c3f93318d1054de2845
Adam Fourney <adamfo@microsoft.com>
2024-03-04 09:21:22 -0800

    Added an initial POC with Playwright.

--- autogen/browser_utils/__init__.py
+++ autogen/browser_utils/__init__.py
@@ -1,10 +1,12 @@
 from .simple_text_browser import SimpleTextBrowser
 from .selenium_chrome_browser import SeleniumChromeBrowser
+from .playwright_chrome_browser import PlaywrightChromeBrowser
 from .mdconvert import MarkdownConverter, UnsupportedFormatException, FileConversionException, DocumentConverterResult
 
 __all__ = (
     "SimpleTextBrowser",
     "SeleniumChromeBrowser",
+    "PlaywrightChromeBrowser",
     "MarkdownConverter",
     "UnsupportedFormatException",
     "FileConversionException",
