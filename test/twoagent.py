bdeaaecb792ef33746b042992e4f704a533d4346
Jack Gerrits <jack@jackgerrits.com>
2024-03-06 11:33:31 -0500

    precommit

--- test/twoagent.py
+++ test/twoagent.py
@@ -3,12 +3,7 @@ from autogen import AssistantAgent, UserProxyAgent, config_list_from_json
 # Load LLM inference endpoints from an env variable or a file
 # See https://microsoft.github.io/autogen/docs/FAQ#set-your-api-endpoints
 # and OAI_CONFIG_LIST_sample
-config_list = [
-  {
-    "model": "huggingface/bigcode/starcoder",
-    "base_url": "http://localhost:8000"
-  }
-]
+config_list = [{"model": "huggingface/bigcode/starcoder", "base_url": "http://localhost:8000"}]
 assistant = AssistantAgent("assistant", llm_config={"config_list": config_list})
 user_proxy = UserProxyAgent(
     "user_proxy", code_execution_config={"work_dir": "coding", "use_docker": False}
