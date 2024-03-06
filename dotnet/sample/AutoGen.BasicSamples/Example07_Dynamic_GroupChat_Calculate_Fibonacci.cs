0295ea9defb57ee4474e2ac25a0c95e2bf21b49a
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-03-05 17:57:28 -0800

    add doc

--- dotnet/sample/AutoGen.BasicSamples/Example07_Dynamic_GroupChat_Calculate_Fibonacci.cs
+++ dotnet/sample/AutoGen.BasicSamples/Example07_Dynamic_GroupChat_Calculate_Fibonacci.cs
@@ -46,22 +46,102 @@ public partial class Example07_Dynamic_GroupChat_Calculate_Fibonacci
     }
     #endregion reviewer_function
 
-    public static async Task RunAsync()
+    public static async Task<IAgent> CreateCoderAgentAsync()
     {
-        var functions = new Example07_Dynamic_GroupChat_Calculate_Fibonacci();
-        long the39thFibonacciNumber = 63245986;
-        var workDir = Path.Combine(Path.GetTempPath(), "InteractiveService");
-        if (!Directory.Exists(workDir))
-            Directory.CreateDirectory(workDir);
-
-        using var service = new InteractiveService(workDir);
-        var dotnetInteractiveFunctions = new DotnetInteractiveFunction(service);
-
-        await service.StartAsync(workDir, default);
-
+        #region create_coder
         var gpt3Config = LLMConfiguration.GetAzureOpenAIGPT3_5_Turbo();
+        var coder = new GPTAgent(
+            name: "coder",
+            systemMessage: @"You act as dotnet coder, you write dotnet code to resolve task. Once you finish writing code, ask runner to run the code for you.
 
+            Here're some rules to follow on writing dotnet code:
+            - put code between ```csharp and ```
+            - Avoid adding `using` keyword when creating disposable object. e.g `var httpClient = new HttpClient()`
+            - Try to use `var` instead of explicit type.
+            - Try avoid using external library, use .NET Core library instead.
+            - Use top level statement to write code.
+            - Always print out the result to console. Don't write code that doesn't print out anything.
+            
+            If you need to install nuget packages, put nuget packages in the following format:
+            ```nuget
+            nuget_package_name
+            ```
+            
+            If your code is incorrect, runner will tell you the error message. Fix the error and send the code again.",
+            config: gpt3Config,
+            temperature: 0.4f)
+            .RegisterPrintFormatMessageHook();
+
+        return coder;
+        #endregion create_coder
+    }
+
+    public static async Task<IAgent> CreateRunnerAgentAsync(InteractiveService service)
+    {
+        #region create_runner
+        var runner = new AssistantAgent(
+            name: "runner",
+            systemMessage: "You run dotnet code",
+            defaultReply: "No code available.")
+            .RegisterDotnetCodeBlockExectionHook(interactiveService: service)
+            .RegisterReply(async (msgs, _) =>
+            {
+                if (msgs.Count() == 0)
+                {
+                    return new TextMessage(Role.Assistant, "No code available. Coder please write code");
+                }
+
+                return null;
+            })
+            .RegisterPreProcess(async (msgs, _) =>
+            {
+                // retrieve the most recent message from coder
+                var coderMsg = msgs.LastOrDefault(msg => msg.From == "coder");
+                if (coderMsg is null)
+                {
+                    return Enumerable.Empty<IMessage>();
+                }
+                else
+                {
+                    return new[] { coderMsg };
+                }
+            })
+            .RegisterPrintFormatMessageHook();
+
+        return runner;
+        #endregion create_runner
+    }
+
+    public static async Task<IAgent> CreateAdminAsync()
+    {
+        #region create_admin
+        var gpt3Config = LLMConfiguration.GetAzureOpenAIGPT3_5_Turbo();
+        var admin = new GPTAgent(
+            name: "admin",
+            systemMessage: "You are group admin, terminate the group chat once task is completed by saying [TERMINATE] plus the final answer",
+            temperature: 0,
+            config: gpt3Config)
+            .RegisterPostProcess(async (_, reply, _) =>
+            {
+                if (reply is TextMessage textMessage && textMessage.Content.Contains("TERMINATE") is true)
+                {
+                    var content = $"{textMessage.Content}\n\n {GroupChatExtension.TERMINATE}";
+
+                    return new TextMessage(Role.Assistant, content, from: reply.From);
+                }
+
+                return reply;
+            });
+
+        return admin;
+        #endregion create_admin
+    }
+
+    public static async Task<IAgent> CreateReviewerAgentAsync()
+    {
         #region create_reviewer
+        var gpt3Config = LLMConfiguration.GetAzureOpenAIGPT3_5_Turbo();
+        var functions = new Example07_Dynamic_GroupChat_Calculate_Fibonacci();
         var reviewer = new GPTAgent(
             name: "code_reviewer",
             systemMessage: @"You review code block from coder",
@@ -132,8 +212,8 @@ public partial class Example07_Dynamic_GroupChat_Calculate_Fibonacci
                         var originalContent = reply.GetContent();
                         var prompt = $@"Please convert the content to ReviewCodeBlock function arguments.
 
-## Original Content
-{originalContent}";
+        ## Original Content
+        {originalContent}";
 
                         reply = await innerAgent.SendAsync(prompt, msgs, ct);
                     }
@@ -144,80 +224,24 @@ public partial class Example07_Dynamic_GroupChat_Calculate_Fibonacci
             .RegisterPrintFormatMessageHook();
         #endregion create_reviewer
 
-        #region create_coder
-        var coder = new GPTAgent(
-            name: "coder",
-            systemMessage: @"You act as dotnet coder, you write dotnet code to resolve task. Once you finish writing code, ask runner to run the code for you.
+        return reviewer;
+    }
 
-            Here're some rules to follow on writing dotnet code:
-            - put code between ```csharp and ```
-            - Avoid adding `using` keyword when creating disposable object. e.g `var httpClient = new HttpClient()`
-            - Try to use `var` instead of explicit type.
-            - Try avoid using external library, use .NET Core library instead.
-            - Use top level statement to write code.
-            - Always print out the result to console. Don't write code that doesn't print out anything.
-            
-            If you need to install nuget packages, put nuget packages in the following format:
-            ```nuget
-            nuget_package_name
-            ```
-            
-            If your code is incorrect, runner will tell you the error message. Fix the error and send the code again.",
-            config: gpt3Config,
-            temperature: 0.4f)
-            .RegisterPrintFormatMessageHook();
-        #endregion create_coder
+    public static async Task RunAsync()
+    {
+        long the39thFibonacciNumber = 63245986;
+        var workDir = Path.Combine(Path.GetTempPath(), "InteractiveService");
+        if (!Directory.Exists(workDir))
+            Directory.CreateDirectory(workDir);
 
-        #region create_runner
-        var runner = new AssistantAgent(
-            name: "runner",
-            systemMessage: "You run dotnet code",
-            defaultReply: "No code available.")
-            .RegisterDotnetCodeBlockExectionHook(interactiveService: service)
-            .RegisterReply(async (msgs, _) =>
-            {
-                if (msgs.Count() == 0)
-                {
-                    return new TextMessage(Role.Assistant, "No code available. Coder please write code");
-                }
+        using var service = new InteractiveService(workDir);
+        var dotnetInteractiveFunctions = new DotnetInteractiveFunction(service);
 
-                return null;
-            })
-            .RegisterPreProcess(async (msgs, _) =>
-            {
-                // retrieve the most recent message from coder
-                var coderMsg = msgs.LastOrDefault(msg => msg.From == "coder");
-                if (coderMsg is null)
-                {
-                    return Enumerable.Empty<IMessage>();
-                }
-                else
-                {
-                    return new[] { coderMsg };
-                }
-            })
-            .RegisterPrintFormatMessageHook();
-        #endregion create_runner
-
-        #region create_admin
-        var admin = new GPTAgent(
-            name: "admin",
-            systemMessage: "You are group admin, terminate the group chat once task is completed by saying [TERMINATE] plus the final answer",
-            temperature: 0,
-            config: gpt3Config)
-            .RegisterPostProcess(async (_, reply, _) =>
-            {
-                if (reply is TextMessage textMessage && textMessage.Content.Contains("TERMINATE") is true)
-                {
-                    var content = $"{textMessage.Content}\n\n {GroupChatExtension.TERMINATE}";
-
-                    return new TextMessage(Role.Assistant, content, from: reply.From);
-                }
-
-                return reply;
-            })
-            .RegisterPrintFormatMessageHook();
-        #endregion create_admin
+        await service.StartAsync(workDir, default);
+        var reviewer = await CreateReviewerAgentAsync();
+        var coder = await CreateCoderAgentAsync();
+        var runner = await CreateRunnerAgentAsync(service);
+        var admin = await CreateAdminAsync();
 
         #region create_group_chat
         var groupChat = new GroupChat(
@@ -236,9 +260,6 @@ public partial class Example07_Dynamic_GroupChat_Calculate_Fibonacci
         runner.AddInitializeMessage("I will run dotnet code once the review is done", groupChat);
 
         var groupChatManager = new GroupChatManager(groupChat);
-        #endregion create_group_chat
-
-        #region start_group_chat
         var conversationHistory = await admin.InitiateChatAsync(groupChatManager, "What's the 39th of fibonacci number?", maxRound: 10);
 
         // the last message is from admin, which is the termination message
@@ -247,6 +268,6 @@ public partial class Example07_Dynamic_GroupChat_Calculate_Fibonacci
         lastMessage.IsGroupChatTerminateMessage().Should().BeTrue();
         lastMessage.Should().BeOfType<TextMessage>();
         lastMessage.GetContent().Should().Contain(the39thFibonacciNumber.ToString());
-        #endregion start_group_chat
+        #endregion create_group_chat
     }
 }
--- dotnet/sample/AutoGen.BasicSamples/Program.cs
+++ dotnet/sample/AutoGen.BasicSamples/Program.cs
@@ -1,9 +1,5 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // Program.cs
 
-using AutoGen.BasicSample.CodeSnippet;
-
-var codeSnippet = new PrintMessageMiddlewareCodeSnippet();
-// wait user to click enter to continue
 Console.ReadLine();
-await codeSnippet.PrintMessageStreamingMiddlewareAsync();
+await Example07_Dynamic_GroupChat_Calculate_Fibonacci.RunAsync();
