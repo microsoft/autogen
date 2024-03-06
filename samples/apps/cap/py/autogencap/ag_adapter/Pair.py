fb1569e0e89cdf96186223bfae77eccd57f17f26
Rajan <rajan.chari@yahoo.com>
2024-03-06 09:32:18 -0800

    Pair should not make assumptions about who is first and who is second

--- samples/apps/cap/py/autogencap/ag_adapter/Pair.py
+++ samples/apps/cap/py/autogencap/ag_adapter/Pair.py
@@ -1,30 +1,33 @@
 from autogencap.ag_adapter.CAP2AG import CAP2AG
-import time
-
 
 class Pair:
     def __init__(self, network, first, second):
         self._network = network
-        self._first = first
-        self._second = second
-        self._user_proxy_adptr = None
-        self._assistant_adptr = None
+        self._first_ag_agent = first
+        self._second_ag_agent = second
+        self._first_adptr = None
+        self._second_adptr = None
 
     def initiate_chat(self, message: str):
-        self._user_proxy_adptr = CAP2AG(
-            ag_agent=self._first, the_other_name="assistant", init_chat=True, self_recursive=True
+        self._first_adptr = CAP2AG(
+            ag_agent=self._first_ag_agent, 
+            the_other_name=self._second_ag_agent.name, 
+            init_chat=True, 
+            self_recursive=True
         )
-
-        self._assistant_adptr = CAP2AG(
-            ag_agent=self._second, the_other_name="user_proxy", init_chat=False, self_recursive=True
+        self._second_adptr = CAP2AG(
+            ag_agent=self._second_ag_agent, 
+            the_other_name=self._first_ag_agent.name, 
+            init_chat=False, 
+            self_recursive=True
         )
-        self._network.register(self._user_proxy_adptr)
-        self._network.register(self._assistant_adptr)
+        self._network.register(self._first_adptr)
+        self._network.register(self._second_adptr)
         self._network.connect()
 
         # Send a message to the user_proxy
-        user_proxy_connection = self._network.lookup_actor("user_proxy")
-        user_proxy_connection.send_txt_msg("Plot a chart of MSFT daily closing prices for last 1 Month.")
+        agent_connection = self._network.lookup_actor(self._first_ag_agent.name)
+        agent_connection.send_txt_msg("Plot a chart of MSFT daily closing prices for last 1 Month.")
 
     def running(self):
-        return self._user_proxy_adptr.run and self._assistant_adptr.run
+        return self._first_adptr.run and self._second_adptr.run
