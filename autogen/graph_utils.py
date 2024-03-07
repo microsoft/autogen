2403c1b4e6242b866b919e0fe14009905efd8e89
Wael Karkoub <wael.karkoub96@gmail.com>
2024-03-07 21:17:45 +0100

    fix circular imports

--- autogen/graph_utils.py
+++ autogen/graph_utils.py
@@ -1,7 +1,7 @@
-from typing import Dict, List, Optional, Union
+from typing import Dict, List
 import logging
 
-from autogen.agentchat.groupchat import Agent
+from .agentchat.groupchat import Agent
 
 
 def has_self_loops(allowed_speaker_transitions: Dict) -> bool:
