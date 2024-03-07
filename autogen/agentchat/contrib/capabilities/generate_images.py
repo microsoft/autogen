5cfada71e0005407082abffc4684ee5e803d9112
Wael Karkoub <wael.karkoub96@gmail.com>
2024-03-07 19:14:09 +0100

    wip

--- autogen/agentchat/contrib/capabilities/generate_images.py
+++ autogen/agentchat/contrib/capabilities/generate_images.py
@@ -1,12 +1,13 @@
-import abc
-from typing import Any, Dict, List, Literal, Optional, Tuple, Union
+from typing import Any, Dict, List, Literal, Optional, Protocol, Tuple, Union
 from diskcache import Cache
 import re
+
 
 from openai import OpenAI
 from autogen.agentchat.agent import Agent
 from autogen.agentchat.contrib import img_utils
-from .agent_capability import AgentCapability
+from autogen.agentchat.contrib.capabilities.agent_capability import AgentCapability
+from autogen import code_utils
 from autogen.agentchat.contrib.text_analyzer_agent import TextAnalyzerAgent
 
 from PIL.Image import Image
@@ -16,20 +17,34 @@ SYSTEM_MESSAGE = "You've been given the special ability to generate images."
 DESCRIPTION_MESSAGE = "This agent has the ability to generate images."
 
 
-class ImageGenerator(abc.ABC):
-    @abc.abstractmethod
+class ImageGenerator(Protocol):
+    """This class defines an interface for image generators.
+
+    Concrete implementations of this protocol must provide a `generate_image` method that takes a string prompt as
+    input and returns a PIL Image object.
+    """
+
     def generate_image(self, prompt: str) -> Image:
-        raise NotImplementedError()
+        ...
 
 
-class DalleImageGenerator(ImageGenerator):
+class DalleImageGenerator:
     def __init__(
         self,
         llm_config: Dict,
         resolution: Literal["256x256", "512x512", "1024x1024", "1792x1024", "1024x1792"] = "1024x1024",
         quality: Literal["standard", "hd"] = "standard",
         num_images: int = 1,
+        cache_settings: Dict = {},
     ):
+        """
+        Args:
+            llm_config (dict): llm config, must contain a valid dalle model and OpenAI API key in config_list.
+            resolution (str): The resolution of the image you want to generate. Must be one of "256x256", "512x512", "1024x1024", "1792x1024", "1024x1792".
+            quality (str): The quality of the image you want to generate. Must be one of "standard", "hd".
+            num_images (int): The number of images to generate.
+            cache_settings (dict): The settings for the cache. Defaults to {}. Cache is implemented with the diskcache library.
+        """
         config_list = llm_config["config_list"]
         _validate_dalle_model(config_list[0]["model"])
         _validate_resolution_format(resolution)
@@ -38,16 +53,17 @@ class DalleImageGenerator(ImageGenerator):
         self._resolution = resolution
         self._quality = quality
         self._num_images = num_images
+        self._cache_settings = cache_settings
         self._dalle_client = OpenAI(api_key=config_list[0]["api_key"])
 
     def generate_image(self, prompt: str) -> Image:
-        """Got this from Beibin Li's work in agentchat_dalle_and_gpt4v.ipynb"""
-        # Function implementation...
-        cache = Cache(".cache/")  # Create a cache directory
-        key = (self._model, prompt, self._resolution, self._quality, self._num_images)
+        # Use cache if user has specified a cache directory
+        if self._cache_settings:
+            cache = Cache(**self._cache_settings)
+            key = (self._model, prompt, self._resolution, self._quality, self._num_images)
 
-        # if key in cache:
-        #     return img_utils._to_pil(cache[key])
+            if key in cache:
+                return img_utils._to_pil(cache[key])
 
         # If not in cache, compute and store the result
         response = self._dalle_client.images.generate(
@@ -57,6 +73,7 @@ class DalleImageGenerator(ImageGenerator):
             quality=self._quality,
             n=self._num_images,
         )
+
         image_url = response.data[0].url
         img_data = img_utils.get_image_data(image_url)
         cache[key] = img_data
@@ -67,6 +84,13 @@ class ImageGeneration(AgentCapability):
     def __init__(
         self, image_generator: ImageGenerator, text_analyzer_llm_config: Optional[Dict] = None, verbosity: int = 0
     ):
+        """
+        Args:
+            image_generator (ImageGenerator): The image generator you would like to use to generate images.
+            text_analyzer_llm_config (Dict or None): The LLM config for the text analyzer. Defaults to None.
+            verbosity (int): The verbosity level. Defaults to 0 and must be greater than or equal to 0. The text
+            analyzer llm calls will be silent if verbosity is less than 2.
+        """
         self._image_generator = image_generator
         self._text_analyzer_llm_config = text_analyzer_llm_config
         self._verbosity = verbosity
@@ -96,9 +120,7 @@ class ImageGeneration(AgentCapability):
         if messages is None:
             return False, None
 
-        last_message = messages[-1]["content"]
-        if isinstance(last_message, dict):
-            last_message = last_message.get("text", "")
+        last_message = code_utils.content_str([messages[-1]])
 
         if not last_message:
             return False, None
@@ -141,7 +163,11 @@ class ImageGeneration(AgentCapability):
             recipient=self._text_analyzer, message=analysis_instructions, request_reply=True, silent=self._verbosity < 2
         )
 
-        return self._agent.last_message(self._text_analyzer)["content"]
+        last_message = self._agent.last_message(self._text_analyzer)
+        if last_message is None:
+            return ""
+
+        return code_utils.content_str([last_message])
 
 
 ### Helpers
