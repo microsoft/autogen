0e55327011015f29dff531a8de45611e7227856d
Davor Runje <davor@airt.ai>
2024-03-13 10:57:44 +0000

    fix type and default value of the code_execution_config of UserProxAgent

--- autogen/agentchat/user_proxy_agent.py
+++ autogen/agentchat/user_proxy_agent.py
@@ -29,7 +29,7 @@ class UserProxyAgent(ConversableAgent):
         is_termination_msg: Optional[Callable[[Dict], bool]] = None,
         max_consecutive_auto_reply: Optional[int] = None,
         human_input_mode: Literal["ALWAYS", "TERMINATE", "NEVER"] = "ALWAYS",
-        function_map: Dict[str, Callable] = {},
+        function_map: Optional[Dict[str, Callable]] = None,
         code_execution_config: Union[Dict, Literal[False]] = {},
         default_auto_reply: Optional[Union[str, Dict, None]] = "",
         llm_config: Optional[Union[Dict, Literal[False]]] = False,
