4429d4d19f806ecbe4cb19f6e70f9e0341dd25fc
Victor Dibia <victordibia@microsoft.com>
2024-03-15 19:44:47 -0700

    Autogenstudio Updates [Upload/Dowload of Skills/Workflows, Streaming Agent Replies, Agent Message Summarization] (#1801)
    * update default agent system message
    
    * session friendly name functionality
    
    * minor formatting
    
    * fix issues with groupchat and version bump
    
    * fix issues with groupchat and version bump. address #1580
    
    * update groupchat system message
    
    * add support for copying message in chatbox
    
    * rewrite how agent history is maintained in workflow manager. Directly extend GroupChat and Conversable agent and override process_message method. Allow passing a message processor.
    Fixes bug where the last message from a groupchat manager does not get called via register_reply.
    
    * general qol updates
    
    * add support for downloading + copying skills, models and agents from UI
    
    * add regex check to agent name, address #1507
    
    * add support for uploading workflow files
    
    * refactor, add support for streaming intermediate agent response to ui
    
    * improve streaming ux
    
    * add support for uploading  skills, models, agents, workflows
    
    * add datamodel for socket message
    
    * version update
    
    * fix chatbox height bug
    
    * fix csv pagination issue
    
    * improve hidden menu for uploading entities
    
    * fix minor issue with animation timing on chat interface
    
    * version bump, css fixes
    
    * use description field in autogen conversable class for description
    
    * add implementation for llm summarization of agent chat
    
    * support for llm summary of agent history
    
    * formatting fixes
    
    * formatting updates
    
    * add dockerfile to run autogenstudio in a docker contailer
    
    * autogenstudio docker container
    
    * updates to websockets
    
    * update socket connection logic,
    
    * support using socket for passing message requests where a socket is available
    
    * improve command for building frontend
    
    * formatting updates
    
    * remove duplicated code
    
    # overwrite skills.py in work_dir is duplicated
    
    * update description location
    
    as Where the code calls is like  config.description
    
    * version bump
    
    * refactor to ensure each session and call within a session has an independent working directory
    
    * support use of socket for sending messages where available
    
    * use rsync to copy built files to ui direction instead of cp -rT
    
    * spelling correctino
    
    * readme update
    
    * fix numpy version
    
    * version bump
    
    * add support for dot env variables and updating default app dir to /home/<user>/.autogenstudio
    
    * formatting update
    
    * update gitignore
    
    * formatting updates
    
    ---------
    
    Co-authored-by: James Woffinden-Luey <jluey@microsoft.com>
    Co-authored-by: cillyfly <cillyfly@gmail.com>

--- samples/apps/autogen-studio/autogenstudio/datamodel.py
+++ samples/apps/autogen-studio/autogenstudio/datamodel.py
@@ -113,6 +113,7 @@ class AgentConfig:
     is_termination_msg: Optional[Union[bool, str, Callable]] = None
     code_execution_config: Optional[Union[bool, str, Dict[str, Any]]] = None
     default_auto_reply: Optional[str] = ""
+    description: Optional[str] = None
 
     def dict(self):
         result = asdict(self)
@@ -131,7 +132,6 @@ class AgentFlowSpec:
     timestamp: Optional[str] = None
     user_id: Optional[str] = None
     skills: Optional[Union[None, List[Skill]]] = None
-    description: Optional[str] = None
 
     def __post_init__(self):
         if self.timestamp is None:
@@ -175,7 +175,6 @@ class GroupChatFlowSpec:
     id: Optional[str] = None
     timestamp: Optional[str] = None
     user_id: Optional[str] = None
-    description: Optional[str] = None
     skills: Optional[Union[None, List[Skill]]] = None
 
     def __post_init__(self):
@@ -303,3 +302,16 @@ class DBWebRequestModel(object):
     agent: Optional[AgentFlowSpec] = None
     workflow: Optional[AgentWorkFlowConfig] = None
     model: Optional[Model] = None
+    message: Optional[Message] = None
+    connection_id: Optional[str] = None
+
+
+@dataclass
+class SocketMessage(object):
+    connection_id: str
+    data: Dict[str, Any]
+    type: str
+
+    def dict(self):
+        result = asdict(self)
+        return result
