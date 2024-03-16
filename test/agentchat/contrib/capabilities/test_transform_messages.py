02246417ab27a0783c67cf9b303b8e62bcf66515
Wael Karkoub <wael.karkoub96@gmail.com>
2024-03-16 18:07:37 +0100

    improves tests + adds openai contrib test

--- test/agentchat/contrib/capabilities/test_transform_messages.py
+++ test/agentchat/contrib/capabilities/test_transform_messages.py
@@ -1,6 +1,7 @@
+import copy
 import os
 import sys
-import copy
+import tempfile
 from typing import Any, Dict, List, Union
 
 import pytest
@@ -17,7 +18,17 @@ sys.path.append(os.path.join(os.path.dirname(__file__), "../../.."))
 from conftest import skip_openai  # noqa: E402
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "..", ".."))
-from test_assistant_agent import OAI_CONFIG_LIST, KEY_LOC  # noqa: E402
+from test_assistant_agent import KEY_LOC, OAI_CONFIG_LIST  # noqa: E402
+
+
+def _count_tokens(content: Union[str, List[Dict[str, Any]]]) -> int:
+    token_count = 0
+    if isinstance(content, str):
+        token_count = token_count_utils.count_token(content)
+    elif isinstance(content, list):
+        for item in content:
+            token_count += _count_tokens(item.get("text", ""))
+    return token_count
 
 
 def test_limit_token_transform():
@@ -68,7 +79,7 @@ def test_limit_token_transform():
     assert len(transformed_messages) <= len(messages)
 
 
-def test_max_message_history_length_trasnform():
+def test_max_message_history_length_transform():
     """
     Test the MessageHistoryLimiter capability to limit the number of messages.
     """
@@ -93,63 +104,54 @@ def test_transform_messages_capability():
 
     This test is a replica of test_transform_chat_history_with_agents in test_context_handling.py
     """
-    config_list = autogen.config_list_from_json(
-        OAI_CONFIG_LIST,
-        KEY_LOC,
-        filter_dict={
-            "model": "gpt-3.5-turbo",
-        },
-    )
-
-    # TODO: DELETE AFTER TESTING
-    # config_list = [
-    #     {
-    #         "model": "gpt-3.5-turbo",
-    #         "api_key": os.environ.get("OPENAI_API_KEY"),
-    #     }
-    # ]
-
-    assistant = autogen.AssistantAgent(
-        "assistant", llm_config={"config_list": config_list}, max_consecutive_auto_reply=1
-    )
-
-    context_handling = TransformMessages(
-        transforms=[
-            MessageHistoryLimiter(max_messages=10),
-            MessageTokenLimiter(max_tokens=10, max_tokens_per_message=5),
-        ]
-    )
-    context_handling.add_to_agent(assistant)
-    user = autogen.UserProxyAgent(
-        "user",
-        code_execution_config={"work_dir": "coding"},
-        human_input_mode="NEVER",
-        is_termination_msg=lambda x: "TERMINATE" in x.get("content", ""),
-        max_consecutive_auto_reply=1,
-    )
-
-    # Create a very long chat history that is bound to cause a crash
-    # for gpt 3.5
-    for i in range(1000):
-        assitant_msg = {"role": "assistant", "content": "test " * 1000}
-        user_msg = {"role": "user", "content": ""}
-
-        assistant.send(assitant_msg, user, request_reply=False)
-        user.send(user_msg, assistant, request_reply=False)
-
-    try:
-        user.initiate_chat(
-            assistant, message="Plot a chart of nvidia and tesla stock prices for the last 5 years", clear_history=False
+    with tempfile.TemporaryDirectory() as temp_dir:
+        config_list = autogen.config_list_from_json(
+            OAI_CONFIG_LIST,
+            KEY_LOC,
+            filter_dict={
+                "model": "gpt-3.5-turbo",
+            },
         )
-    except Exception as e:
-        assert False, f"Chat initiation failed with error {str(e)}"
+
+        assistant = autogen.AssistantAgent(
+            "assistant", llm_config={"config_list": config_list}, max_consecutive_auto_reply=1
+        )
+
+        context_handling = TransformMessages(
+            transforms=[
+                MessageHistoryLimiter(max_messages=10),
+                MessageTokenLimiter(max_tokens=10, max_tokens_per_message=5),
+            ]
+        )
+        context_handling.add_to_agent(assistant)
+        user = autogen.UserProxyAgent(
+            "user",
+            code_execution_config={"work_dir": temp_dir},
+            human_input_mode="NEVER",
+            is_termination_msg=lambda x: "TERMINATE" in x.get("content", ""),
+            max_consecutive_auto_reply=1,
+        )
+
+        # Create a very long chat history that is bound to cause a crash
+        # for gpt 3.5
+        for i in range(1000):
+            assitant_msg = {"role": "assistant", "content": "test " * 1000}
+            user_msg = {"role": "user", "content": ""}
+
+            assistant.send(assitant_msg, user, request_reply=False)
+            user.send(user_msg, assistant, request_reply=False)
+
+        try:
+            user.initiate_chat(
+                assistant,
+                message="Plot a chart of nvidia and tesla stock prices for the last 5 years",
+                clear_history=False,
+            )
+        except Exception as e:
+            assert False, f"Chat initiation failed with error {str(e)}"
 
 
-def _count_tokens(content: Union[str, List[Dict[str, Any]]]) -> int:
-    token_count = 0
-    if isinstance(content, str):
-        token_count = token_count_utils.count_token(content)
-    elif isinstance(content, list):
-        for item in content:
-            token_count += _count_tokens(item.get("text", ""))
-    return token_count
+if __name__ == "__main__":
+    test_limit_token_transform()
+    test_max_message_history_length_transform()
+    test_transform_messages_capability()
