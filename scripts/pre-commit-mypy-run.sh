ce7e3f9109b13f965babb84d61c26664b490204a
Davor Runje <davor@airt.ai>
2024-03-18 22:57:00 +0000

    Add mypy check in pre-commit

--- scripts/pre-commit-mypy-run.sh
+++ scripts/pre-commit-mypy-run.sh
@@ -10,14 +10,4 @@ set -o errexit
 # Change directory to the project root directory.
 cd "$(dirname "$0")"/..
 
-# Install the dependencies into the mypy env.
-# Note that this can take seconds to run.
-# In my case, I need to use a custom index URL.
-# Avoid pip spending time quietly retrying since
-# likely cause of failure is lack of VPN connection.
-# pip install --editable ".[types]"\
-#  --retries 1 \
-#  --no-input \
-#  --quiet
-
 mypy
