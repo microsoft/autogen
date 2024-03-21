319bea6fac4fb3d71eb0ab9d26bc2130027341a0
Repo-Matt <124791637+Repomano@users.noreply.github.com>
2024-03-21 15:18:28 +0100

    further detail docstring

--- autogen/coding/local_commandline_code_executor.py
+++ autogen/coding/local_commandline_code_executor.py
@@ -104,7 +104,7 @@ class LocalCommandLineCodeExecutor(CodeExecutor):
 
         Args:
             code_blocks (List[CodeBlock]): The code blocks to execute.
-            max_output_length (Optional[int]): The maximum length of the output. If needed, the output will be truncated to this length.
+            max_output_length (Optional[int]): The maximum length of the output. If needed, the output will be truncated to this length considering only the last characters assigned to this parameter.
 
         Returns:
             CommandLineCodeResult: The result of the code execution."""
