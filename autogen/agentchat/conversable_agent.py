3a35ba87cb8a1c402b4ec91d9fed2dfafa13b6ae
Jason <jtoy@grids.local>
2024-03-26 17:35:50 -0700

    use register_function and  llm_config

--- autogen/agentchat/conversable_agent.py
+++ autogen/agentchat/conversable_agent.py
@@ -2284,6 +2284,8 @@ class ConversableAgent(LLMAgent):
             self._assert_valid_name(name)
             if func is None and name not in self._function_map.keys():
                 warnings.warn(f"The function {name} to remove doesn't exist", name)
+            if name in self._function_map.keys():
+                warnings.warn(f"Function '{name}' is being overridden.", UserWarning)
         self._function_map.update(function_map)
         self._function_map = {k: v for k, v in self._function_map.items() if v is not None}
 
@@ -2320,6 +2322,9 @@ class ConversableAgent(LLMAgent):
 
             self._assert_valid_name(func_sig["name"])
             if "functions" in self.llm_config.keys():
+                if any(func["name"] == func_sig["name"] for func in self.llm_config["functions"]):
+                    warnings.warn(f"Function '{func_sig['name']}' is being overridden.", UserWarning)
+
                 self.llm_config["functions"] = [
                     func for func in self.llm_config["functions"] if func.get("name") != func_sig["name"]
                 ] + [func_sig]
@@ -2359,6 +2364,8 @@ class ConversableAgent(LLMAgent):
                     f"The tool signature must be of the type dict. Received tool signature type {type(tool_sig)}"
                 )
             self._assert_valid_name(tool_sig["function"]["name"])
+            if any(tool["function"]["name"] == tool_sig["function"]["name"] for tool in self.llm_config["tools"]):
+                warnings.warn(f"Function '{tool_sig['function']['name']}' is being overridden.", UserWarning)
             if "tools" in self.llm_config.keys():
                 self.llm_config["tools"] = [
                     tool
@@ -2551,8 +2558,6 @@ class ConversableAgent(LLMAgent):
             elif not hasattr(func, "_name"):
                 func._name = func.__name__
 
-            if (func._name in self._function_map) or (self.llm_config and "tools" in self.llm_config.keys() and func._name in [tool["function"]["name"] for tool in self.llm_config["tools"]]):
-                warnings.warn(f"Function '{func._name}' is being overridden.", UserWarning)
 
             self.register_function({func._name: self._wrap_function(func)})
 
