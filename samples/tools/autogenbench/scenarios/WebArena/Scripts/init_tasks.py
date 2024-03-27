765fcd3281d911da0cb026ca52ce30b476360b85
Adam Fourney <adamfo@microsoft.com>
2024-03-26 23:40:39 -0700

    Initial work on onboard WebArena to AutoGenBench

---
+++ samples/tools/autogenbench/scenarios/WebArena/Scripts/init_tasks.py
@@ -0,0 +1,110 @@
+#
+# Run this file to download the human_eval dataset, and create a corresponding testbed scenario:
+# (default: ../scenarios/human_eval_two_agents_gpt4.jsonl and ./scenarios/human_eval_two_agents_gpt35.jsonl)
+#
+
+import requests
+import tarfile
+import io
+import json
+import os
+import re
+import sys
+
+URL = "https://raw.githubusercontent.com/web-arena-x/webarena/main/config_files/test.raw.json"
+
+SCRIPT_PATH = os.path.realpath(__file__)
+SCRIPT_NAME = os.path.basename(SCRIPT_PATH)
+SCRIPT_DIR = os.path.dirname(SCRIPT_PATH)
+
+SCENARIO_DIR = os.path.realpath(os.path.join(SCRIPT_DIR, os.path.pardir))
+TEMPLATES_DIR = os.path.join(SCENARIO_DIR, "Templates")
+TASKS_DIR = os.path.join(SCENARIO_DIR, "Tasks")
+DOWNLOADS_DIR = os.path.join(SCENARIO_DIR, "Downloads")
+
+
+def download():
+    """Download the WebArena dataset (if not already downloaded).
+    Return a JSON list of problem instances."""
+
+    if not os.path.isdir(DOWNLOADS_DIR):
+        os.mkdir(DOWNLOADS_DIR)
+
+    json_file = os.path.join(DOWNLOADS_DIR, "test.raw.json")
+
+    if not os.path.isfile(json_file):
+        # Send a HTTP request to the URL
+        response = requests.get(URL, stream=True)
+        response.raise_for_status()
+
+        # If the HTTP request returns a status code 200, proceed
+        with open(json_file, "wb") as fh:
+            for chunk in response.iter_content(chunk_size=512):
+                fh.write(chunk)
+
+    # Load the problems
+    problems = None
+    with open(json_file, "rb") as fh:
+        problems = json.load(fh)
+    return problems
+
+
+def create_jsonl(name, tasks, template):
+    """Creates a JSONL scenario file with a given name, dictionary of MATH problems, and template path."""
+
+    # Create a task directory if it doesn't exist
+    if not os.path.isdir(TASKS_DIR):
+        os.mkdir(TASKS_DIR)
+
+    # Create the jsonl file
+    prompt_fields = ["task_id", "intent_template_id", "sites", "require_login", "start_url", "geolocation", "intent"]
+    with open(os.path.join(TASKS_DIR, name + ".jsonl"), "wt") as fh:
+        for task in tasks:
+            print(f"Converting: {name}, {task['task_id']}")
+
+            task_prompt = {}
+            for field in prompt_fields:
+                task_prompt[field] = task[field]
+
+            record = {
+                "id": str(task["task_id"]),
+                "template": template,
+                "substitutions": {
+                    "task_prompt.json.txt": {"__TASK_PROMPT__": json.dumps(task_prompt, indent=4)},
+                    "full_task.json.txt": {"__FULL_TASK__": json.dumps(task, indent=4)},
+                },
+            }
+
+            fh.write(json.dumps(record).strip() + "\n")
+
+
+###############################################################################
+def main():
+    tasks = download()
+
+    # list all directories in the Templates directory
+    # and populate a dictionary with the name and path
+    templates = {}
+    for entry in os.scandir(TEMPLATES_DIR):
+        if entry.is_dir():
+            templates[re.sub(r"\s", "", entry.name)] = entry.path
+
+    # Divide the tasks by their websites
+    page_groups = dict()
+    for task in tasks:
+        key = task["sites"][0]
+        if len(task["sites"]) > 1:
+            key = "several_sites"
+        # key = "__".join(sorted([s for s in task["sites"]]))
+        if key not in page_groups:
+            page_groups[key] = list()
+        page_groups[key].append(task)
+
+    # Create the json files
+    for t in templates.items():
+        for pg in page_groups:
+            create_jsonl(f"webarena__{pg}_{t[0]}", page_groups[pg], t[1])
+
+
+if __name__ == "__main__" and __package__ is None:
+    main()
---
+++ samples/tools/autogenbench/scenarios/WebArena/Templates/TwoAgents/scenario.py
@@ -0,0 +1,56 @@
+import os
+import json
+import testbed_utils
+import autogen
+from autogen.agentchat.contrib.multimodal_web_surfer import MultimodalWebSurferAgent
+from mmagent import MultimodalAgent
+
+testbed_utils.init()
+##############################
+
+# Read the prompt
+TASK = None
+with open("task_prompt.json.txt", "rt") as fh:
+    TASK = json.loads(fh.read())
+
+config_list = autogen.config_list_from_json("OAI_CONFIG_LIST")
+
+# Figure out the starting URL
+start_url = {
+    "__SHOPPING__": os.getenv("SHOPPING_URL"),
+    "__SHOPPING_ADMIN__": os.getenv("SHOPPING_ADMIN_URL"),
+    "__REDDIT__": os.getenv("REDDIT_URL"),
+    "__GITLAB__": os.getenv("GITLAB_URL"),
+    "__MAP__": os.getenv("MAP_URL"),
+    "__WIKIPEDIA__": os.getenv("WIKIPEDIA_URL"),
+    "__HOMEPAGE__": os.getenv("HOMEPAGE_URL"),
+}
+
+web_surfer = MultimodalWebSurferAgent(
+    "web_surfer",
+    llm_config={"config_list": config_list},
+    is_termination_msg=lambda x: x.get("content", "").find("TERMINATE") >= 0,
+    headless=True,
+    chromium_channel="chromium",
+    chromium_data_dir=None,
+    start_page=os.getenv("HOMEPAGE_URL"),
+    debug_dir=None,
+)
+
+user_proxy = MultimodalAgent(
+    "user_proxy",
+    system_message="You are a general-purpose AI assistant and can handle many questions -- but you don't have access to a we boweser. However, the user you are talking to does have a browser, and you can see the screen. Provide short direct instructions to them to take you where you need to go to answer the initial question posed to you. Once the original question or task is addressed, reply with the word TERMINATE.",
+    llm_config={"config_list": config_list},
+    human_input_mode="NEVER",
+    is_termination_msg=lambda x: str(x).find("TERMINATE") >= 0,
+    max_consecutive_auto_reply=10,
+)
+
+user_proxy.send(f"Navigate to {start_url[TASK['start_url']]}", web_surfer, request_reply=True)
+user_proxy.initiate_chat(
+    web_surfer,
+    message=TASK["intent"],
+)
+
+##############################
+testbed_utils.finalize(agents=[web_surfer, user_proxy])
