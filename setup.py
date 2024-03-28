acafed0817d4cafa9e8fdd009fb826f4518aed36
Gregory Nwosu <193151+gregnwosu@users.noreply.github.com>
2024-03-28 03:11:00 +0000

    added pytest-mock dependency

--- setup.py
+++ setup.py
@@ -55,6 +55,7 @@ setuptools.setup(
             "pre-commit",
             "pytest-asyncio",
             "pytest>=6.1.1,<8",
+            "pytest-mock",
         ],
         "blendsearch": ["flaml[blendsearch]"],
         "mathchat": ["sympy", "pydantic==1.10.9", "wolframalpha"],
