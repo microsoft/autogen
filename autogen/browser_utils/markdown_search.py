73536814911db70711a6c1e9a293ee9a4237490d
Adam Fourney <adamfo@microsoft.com>
2024-04-01 09:34:08 -0700

    Added additional documentation to markdown_search

--- autogen/browser_utils/markdown_search.py
+++ autogen/browser_utils/markdown_search.py
@@ -31,12 +31,16 @@ class AbstractMarkdownSearch(ABC):
 
 
 class BingMarkdownSearch(AbstractMarkdownSearch):
+    """
+    Provides Bing web search capabilities to Markdown browsers.
+    """
+
     def __init__(self, bing_api_key: str = None, interleave_results: bool = True):
         """
         Perform a Bing web search, and return the results formatted in Markdown.
 
         Args:
-            bing_api_key: key for the Bing search API. If omitted, and attempt is made to read the key from the BING_API_KEY environment variable. If no key is found, BingMarkdownSearch will print a warning, and will fall back to visiting and scraping the live Bing results page. Scraping is objectively worse than using the API, and thus is not recommended.
+            bing_api_key: key for the Bing search API. If omitted, an attempt is made to read the key from the BING_API_KEY environment variable. If no key is found, BingMarkdownSearch will print a warning, and will fall back to visiting and scraping the live Bing results page. Scraping is objectively worse than using the API, and thus is not recommended.
             interleave_results: When using the Bing API, results are returned based on category (web, news, videos, etc.), along with instructions for how they should be interleaved on the page. When `interleave` is set to True, these interleaving instructions are followed, and a single results list is returned by BingMarkdownSearch. When `interleave` is set to false, results are separated by category, and no interleaving is done.
         """
         super().__init__()
@@ -56,16 +60,33 @@ class BingMarkdownSearch(AbstractMarkdownSearch):
                 )
 
             logger.warning(
-                "Warning: No Bing API key provided. BingMarkdownSearch will submit an HTTP request to the Bing landing page, but results may be missing or low quality. To resolve this warning provide a Bing API key by setting the BING_API_KEY environment variable, or using the 'bing_api_key' parameter in by BingMarkdownSearch's constructor. Bing API keys can be obtained via https://www.microsoft.com/en-us/bing/apis/bing-web-search-api\n"
+                "Warning: No Bing API key provided. BingMarkdownSearch will submit an HTTP request to the Bing landing page, but results may be missing or low quality. To resolve this warning, provide a Bing API key by setting the BING_API_KEY environment variable, or using the 'bing_api_key' parameter in by BingMarkdownSearch's constructor. Bing API keys can be obtained via https://www.microsoft.com/en-us/bing/apis/bing-web-search-api\n"
             )
 
     def search(self, query: str):
+        """Search Bing and return the results formatted in Markdown. If a Bing API key is available, the API is used to perform the search. If no API key is available, the search is performed by submitting an HTTPs GET request directly to Bing. Searches performed with the API are much higher quality, and are more reliable.
+
+        Args:
+            query: The search query to issue
+
+        Returns:
+            A Markdown rendering of the search results.
+        """
+
         if self._bing_api_key is None:
             return self._fallback_search(query)
         else:
             return self._api_search(query)
 
     def _api_search(self, query: str):
+        """Search Bing using the API, and return the results formatted in Markdown.
+
+        Args:
+            query: The search query to issue
+
+        Returns:
+            A Markdown rendering of the search results.
+        """
         results = self._bing_api_call(query)
 
         snippets = dict()
@@ -200,6 +221,14 @@ class BingMarkdownSearch(AbstractMarkdownSearch):
         return f"## A Bing search for '{query}' found {idx} results:\n\n" + content.strip()
 
     def _bing_api_call(self, query: str):
+        """Make a Bing API call, and return a Python representation of the JSON response."
+
+        Args:
+            query: The search query to issue
+
+        Returns:
+            A Python representation of the Bing API's JSON response (as parsed by `json.loads()`).
+        """
         # Make sure the key was set
         if not self._bing_api_key:
             raise ValueError("Missing Bing API key.")
@@ -224,6 +253,14 @@ class BingMarkdownSearch(AbstractMarkdownSearch):
         return results
 
     def _fallback_search(self, query: str):
+        """When no Bing API key is provided, we issue a simple HTTPs GET call to the Bing landing page and convert it to Markdown.
+
+        Args:
+            query: The search query to issue
+
+        Returns:
+            The Bing search results page, converted to Markdown.
+        """
         user_agent = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36 Edg/119.0.0.0"
         headers = {"User-Agent": user_agent}
 
@@ -232,10 +269,20 @@ class BingMarkdownSearch(AbstractMarkdownSearch):
         response.raise_for_status()
         return self._mdconvert.convert_response(response).text_content
 
-    def _markdown_link(self, anchor, href):
-        """Create a Markdown hyperlink, escaping the URLs as appropriate."""
+    def _markdown_link(self, anchor: str, href: str):
+        """Create a Markdown hyperlink, escaping the URLs as appropriate.
+
+        Args:
+            anchor: The anchor text of the hyperlink
+            href: The href destination of the hyperlink
+
+        Returns:
+            A correctly-formatted Markdown hyperlink
+        """
         try:
             parsed_url = urlparse(href)
+            # URLs provided by Bing are sometimes only partially quoted, leaving in characters
+            # the conflict with Markdown. We unquote the URL, and then re-quote more completely
             href = urlunparse(parsed_url._replace(path=quote(unquote(parsed_url.path))))
             anchor = re.sub(r"[\[\]]", " ", anchor)
             return f"[{anchor}]({href})"
