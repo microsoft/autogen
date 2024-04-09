a44a4d3d0d6855be6590b05b361294606e4c006d
giorgossideris <georgesider66@yahoo.gr>
2024-04-09 14:28:46 +0300

    Remove lazy-fixture

--- test/agentchat/contrib/capabilities/test_transforms.py
+++ test/agentchat/contrib/capabilities/test_transforms.py
@@ -2,13 +2,11 @@ import copy
 from typing import Dict, List
 
 import pytest
-from pytest_lazyfixture import lazy_fixture
 
 from autogen.agentchat.contrib.capabilities.transforms import MessageHistoryLimiter, MessageTokenLimiter, _count_tokens
 
 
-@pytest.fixture
-def long_messages() -> List[Dict]:
+def get_long_messages() -> List[Dict]:
     return [
         {"role": "assistant", "content": [{"type": "text", "text": "are you doing?"}]},
         {"role": "user", "content": "very very very very very very long string"},
@@ -18,8 +16,7 @@ def long_messages() -> List[Dict]:
     ]
 
 
-@pytest.fixture
-def short_messages() -> List[Dict]:
+def get_short_messages() -> List[Dict]:
     return [
         {"role": "user", "content": "hello"},
         {"role": "assistant", "content": [{"type": "text", "text": "there"}]},
@@ -40,9 +37,7 @@ def message_token_limiter() -> MessageTokenLimiter:
 # MessageHistoryLimiter tests
 
 
-@pytest.mark.parametrize(
-    "messages, expected_len", [(lazy_fixture("long_messages"), 3), (lazy_fixture("short_messages"), 3)]
-)
+@pytest.mark.parametrize("messages, expected_len", [(get_long_messages(), 3), (get_short_messages(), 3)])
 def test_message_history_limiter_apply_transform(message_history_limiter, messages, expected_len):
     transformed_messages = message_history_limiter.apply_transform(messages)
     assert len(transformed_messages) == expected_len
@@ -51,8 +46,8 @@ def test_message_history_limiter_apply_transform(message_history_limiter, messag
 @pytest.mark.parametrize(
     "messages, expected_stats, expected_effect",
     [
-        (lazy_fixture("long_messages"), "Removed 2 messages. Number of messages reduced from 5 to 3.", True),
-        (lazy_fixture("short_messages"), "No messages were removed.", False),
+        (get_long_messages(), "Removed 2 messages. Number of messages reduced from 5 to 3.", True),
+        (get_short_messages(), "No messages were removed.", False),
     ],
 )
 def test_message_history_limiter_get_stats(message_history_limiter, messages, expected_stats, expected_effect):
@@ -66,9 +61,7 @@ def test_message_history_limiter_get_stats(message_history_limiter, messages, ex
 # MessageTokenLimiter tests
 
 
-@pytest.mark.parametrize(
-    "messages, expected_token_count", [(lazy_fixture("long_messages"), 9), (lazy_fixture("short_messages"), 3)]
-)
+@pytest.mark.parametrize("messages, expected_token_count", [(get_long_messages(), 9), (get_short_messages(), 3)])
 def test_message_token_limiter_apply_transform(message_token_limiter, messages, expected_token_count):
     transformed_messages = message_token_limiter.apply_transform(messages)
     assert sum(_count_tokens(msg["content"]) for msg in transformed_messages) == expected_token_count
@@ -77,8 +70,8 @@ def test_message_token_limiter_apply_transform(message_token_limiter, messages, 
 @pytest.mark.parametrize(
     "messages, expected_stats, expected_effect",
     [
-        (lazy_fixture("long_messages"), "Truncated 6 tokens. Number of tokens reduced from 15 to 9", True),
-        (lazy_fixture("short_messages"), "No tokens were truncated.", False),
+        (get_long_messages(), "Truncated 6 tokens. Number of tokens reduced from 15 to 9", True),
+        (get_short_messages(), "No tokens were truncated.", False),
     ],
 )
 def test_message_token_limiter_get_stats(message_token_limiter, messages, expected_stats, expected_effect):
@@ -90,18 +83,8 @@ def test_message_token_limiter_get_stats(message_token_limiter, messages, expect
 
 
 if __name__ == "__main__":
-    long_messages = [
-        {"role": "assistant", "content": [{"type": "text", "text": "are you doing?"}]},
-        {"role": "user", "content": "very very very very very very long string"},
-        {"role": "user", "content": "hello"},
-        {"role": "assistant", "content": [{"type": "text", "text": "there"}]},
-        {"role": "user", "content": "how"},
-    ]
-    short_messages = [
-        {"role": "user", "content": "hello"},
-        {"role": "assistant", "content": [{"type": "text", "text": "there"}]},
-        {"role": "user", "content": "how"},
-    ]
+    long_messages = get_long_messages()
+    short_messages = get_short_messages()
     message_history_limiter = MessageHistoryLimiter(max_messages=3)
     message_token_limiter = MessageTokenLimiter(max_tokens_per_message=3)
 
