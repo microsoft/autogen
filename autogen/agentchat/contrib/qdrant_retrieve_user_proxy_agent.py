04318ceaff8d44327b1142be44595772a58f9a46
Li Jiang <bnujli@gmail.com>
2024-04-10 08:45:26 +0800

    Add source to the answer for default prompt (#2289)
    * Add source to the answer for default prompt
    
    * Fix qdrant
    
    * Fix tests
    
    * Update docstring
    
    * Fix check files
    
    * Fix qdrant test error

--- autogen/agentchat/contrib/qdrant_retrieve_user_proxy_agent.py
+++ autogen/agentchat/contrib/qdrant_retrieve_user_proxy_agent.py
@@ -190,12 +190,12 @@ def create_qdrant_from_dir(
         client.set_model(embedding_model)
 
     if custom_text_split_function is not None:
-        chunks = split_files_to_chunks(
+        chunks, sources = split_files_to_chunks(
             get_files_from_dir(dir_path, custom_text_types, recursive),
             custom_text_split_function=custom_text_split_function,
         )
     else:
-        chunks = split_files_to_chunks(
+        chunks, sources = split_files_to_chunks(
             get_files_from_dir(dir_path, custom_text_types, recursive), max_tokens, chunk_mode, must_break_at_empty_line
         )
     logger.info(f"Found {len(chunks)} chunks.")
@@ -298,5 +298,6 @@ def query_qdrant(
     data = {
         "ids": [[result.id for result in sublist] for sublist in results],
         "documents": [[result.document for result in sublist] for sublist in results],
+        "metadatas": [[result.metadata for result in sublist] for sublist in results],
     }
     return data
