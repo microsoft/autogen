608fae1011be0a1d8b89c1f93821270792c44ead
William W Wang <107702013+wmwxwa@users.noreply.github.com>
2024-04-12 15:35:36 -0400

    Update __init__.py

--- autogen/logger/__init__.py
+++ autogen/logger/__init__.py
@@ -1,4 +1,5 @@
 from .logger_factory import LoggerFactory
 from .sqlite_logger import SqliteLogger
+from .cosmos_db_logger import CosmosDBLogger
 
-__all__ = ("LoggerFactory", "SqliteLogger")
+__all__ = ("LoggerFactory", "SqliteLogger", "CosmosDBLogger")
