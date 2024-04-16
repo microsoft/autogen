95bb8a13870df4c1c9972f1371797a3b4c9b42a0
Rajan <rajan.chari@yahoo.com>
2024-04-16 18:03:02 -0400

    User proxy must initiate chat

--- samples/apps/cap/py/autogencap/ag_adapter/agent.py
+++ samples/apps/cap/py/autogencap/ag_adapter/agent.py
@@ -4,11 +4,12 @@ from ..DebugLog import Info, Warn
 from .CAP2AG import CAP2AG
 
 class Agent:
-    def __init__(self, agent: ConversableAgent, counter_party_name="user_proxy"):
+    def __init__(self, agent: ConversableAgent, counter_party_name="user_proxy", init_chat=False):
         self._agent = agent
         self._the_other_name = counter_party_name
         self._agent_adptr = CAP2AG(
-            ag_agent=self._agent, the_other_name=self._the_other_name, init_chat=False, self_recursive=True
+            ag_agent=self._agent, the_other_name=self._the_other_name, init_chat=init_chat
+            , self_recursive=True
         )
 
     def register(self, network):
