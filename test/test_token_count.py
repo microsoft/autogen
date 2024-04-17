4f34f5b5e27529e63d6876270ddbfaa28ebcf752
Beibin Li <beibin79@gmail.com>
2024-04-17 11:18:26 -0700

    Correct token count for GPT-4v images

--- test/test_token_count.py
+++ test/test_token_count.py
@@ -10,6 +10,13 @@ from autogen.token_count_utils import (
     token_left,
 )
 
+try:
+    from autogen.agentchat.contrib.img_utils import num_tokens_from_gpt_image
+
+    img_util_imported = True
+except ImportError:
+    img_util_imported = False
+
 func1 = {
     "name": "sh",
     "description": "run a shell script and return the execution result.",
@@ -83,7 +90,38 @@ def test_model_aliases():
     assert get_max_token_limit("gpt4-32k") == get_max_token_limit("gpt-4-32k")
 
 
+@pytest.mark.skipif(not img_util_imported, reason="img_utils not imported")
+def test_num_tokens_from_gpt_image():
+    # mock num_tokens_from_gpt_image function
+
+    base64_encoded_image = (
+        "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAUAAAAFCAYAAACNbyblAAAAHElEQVQI12P4"
+        "//8/w38GIAXDIBKE0DHxgljNBAAO9TXL0Y4OHwAAAABJRU5ErkJggg=="
+    )
+
+    messages = [
+        {
+            "role": "system",
+            "content": "you are a helpful assistant. af3758 *3 33(3)",
+        },
+        {
+            "role": "user",
+            "content": [
+                {"type": "text", "text": "hello asdfjj qeweee"},
+                {"type": "image_url", "image_url": {"url": base64_encoded_image}},
+            ],
+        },
+    ]
+    tokens = count_token(messages, model="gpt-4-vision-preview")
+
+    # The total number of tokens is text + image
+    # where text = 34, as shown in the previous test case
+    # the image token is: 85 + 170 = 255
+    assert tokens == 34 + 255
+
+
 if __name__ == "__main__":
     #    test_num_tokens_from_functions()
-    #    test_count_token()
+    test_count_token()
     test_model_aliases()
+    test_num_tokens_from_gpt_image()
