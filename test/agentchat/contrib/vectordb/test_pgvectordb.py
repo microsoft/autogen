820d939a90fdb63e6bf4201520897128f433bda0
knucklessg1 <knucklessg1@gmail.com>
2024-04-17 23:10:30 -0500

    Removed deprecated tests.

--- test/agentchat/contrib/vectordb/test_pgvectordb.py
+++ test/agentchat/contrib/vectordb/test_pgvectordb.py
@@ -77,48 +77,6 @@ def test_pgvector():
     res = db.get_docs_by_ids(["1", "2"], collection_name)
     assert [r["id"] for r in res] == ["2"]  # "1" has been deleted
 
-    # test _pgvector_results_to_query_results
-    data_dict = {
-        "key1s": [[1, 2, 3], [4, 5, 6], [7, 8, 9]],
-        "key2s": [["a", "b", "c"], ["c", "d", "e"], ["e", "f", "g"]],
-        "key3s": None,
-        "key4s": [["x", "y", "z"], ["1", "2", "3"], ["4", "5", "6"]],
-        "distances": [[0.1, 0.2, 0.3], [0.4, 0.5, 0.6], [0.7, 0.8, 0.9]],
-    }
-    results = [
-        [
-            ({"key1": 1, "key2": "a", "key4": "x"}, 0.1),
-            ({"key1": 2, "key2": "b", "key4": "y"}, 0.2),
-            ({"key1": 3, "key2": "c", "key4": "z"}, 0.3),
-        ],
-        [
-            ({"key1": 4, "key2": "c", "key4": "1"}, 0.4),
-            ({"key1": 5, "key2": "d", "key4": "2"}, 0.5),
-            ({"key1": 6, "key2": "e", "key4": "3"}, 0.6),
-        ],
-        [
-            ({"key1": 7, "key2": "e", "key4": "4"}, 0.7),
-            ({"key1": 8, "key2": "f", "key4": "5"}, 0.8),
-            ({"key1": 9, "key2": "g", "key4": "6"}, 0.9),
-        ],
-    ]
-    assert db._pgvector_results_to_query_results(data_dict) == results
-
-    # test _pgvector_get_results_to_list_documents
-    data_dict = {
-        "key1s": [1, 2, 3],
-        "key2s": ["a", "b", "c"],
-        "key3s": None,
-        "key4s": ["x", "y", "z"],
-    }
-
-    results = [
-        {"key1": 1, "key2": "a", "key4": "x"},
-        {"key1": 2, "key2": "b", "key4": "y"},
-        {"key1": 3, "key2": "c", "key4": "z"},
-    ]
-    assert db._pgvector_get_results_to_list_documents(data_dict) == results
-
 
 if __name__ == "__main__":
     test_pgvector()
