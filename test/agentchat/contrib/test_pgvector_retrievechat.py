c108d2ac462e2ca790979c8334d000f5be4ee1eb
knucklessg1 <knucklessg1@gmail.com>
2024-04-17 23:13:52 -0500

    Removed deprecated tests.

--- test/agentchat/contrib/test_pgvector_retrievechat.py
+++ test/agentchat/contrib/test_pgvector_retrievechat.py
@@ -4,10 +4,10 @@ import os
 import sys
 
 import pytest
+from sentence_transformers import SentenceTransformer
 
 from autogen import config_list_from_json
 from autogen.agentchat.contrib.retrieve_assistant_agent import RetrieveAssistantAgent
-from sentence_transformers import SentenceTransformer
 
 sys.path.append(os.path.join(os.path.dirname(__file__), "../.."))
 from conftest import skip_openai  # noqa: E402
