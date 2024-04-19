3f0779edbfd50b1e56491baf1c47abb498772399
William W Wang <107702013+wmwxwa@users.noreply.github.com>
2024-04-19 14:38:15 -0400

    Update test_cosmos_db_cache.py

--- test/cache/test_cosmos_db_cache.py
+++ test/cache/test_cosmos_db_cache.py
@@ -20,6 +20,9 @@ class TestCosmosDBCache(unittest.TestCase):
         self.client = MagicMock()
         self.database_id = "test_database"
         self.container_id = "test_container"
+        self.container_client_mock = MagicMock()
+        self.client.get_database_client().get_container_client.return_value = self.container_client_mock
+
 
     @pytest.mark.skipif(skip_cosmos_tests, reason="Cosmos DB SDK not installed")
     @patch("autogen.cache.cosmos_db_cache.CosmosClient.from_connection_string", return_value=MagicMock())
@@ -33,13 +36,17 @@ class TestCosmosDBCache(unittest.TestCase):
         key = "key"
         value = "value"
         serialized_value = pickle.dumps(value)
-        self.client.get_database_client().get_container_client().read_item.return_value = {"data": serialized_value}
+        self.container_client_mock.read_item.return_value = {"data": serialized_value}
+        #self.client.get_database_client().get_container_client().read_item.return_value = {"data": serialized_value}
         cache = CosmosDBCache(self.seed, self.client, self.database_id, self.container_id)
 
         self.assertEqual(cache.get(key), value)
+        self.container_client_mock.read_item.assert_called_with(item=key, partition_key=str(self.seed))
+        """
         self.client.get_database_client().get_container_client().read_item.assert_called_with(
             item=key, partition_key=str(self.seed)
         )
+        """
 
         self.client.get_database_client().get_container_client().read_item.side_effect = Exception("not found")
         self.assertIsNone(cache.get(key))
@@ -51,9 +58,14 @@ class TestCosmosDBCache(unittest.TestCase):
         serialized_value = pickle.dumps(value)
         cache = CosmosDBCache(self.seed, self.client, self.database_id, self.container_id)
         cache.set(key, value)
+        self.container_client_mock.upsert_item.assert_called_with({
+            "id": key, "partitionKey": str(self.seed), "data": serialized_value
+        })
+        """
         self.client.get_database_client().get_container_client().upsert_item.assert_called_with(
             {"id": key, "partitionKey": str(self.seed), "data": serialized_value}
         )
+        """
 
     @pytest.mark.skipif(skip_cosmos_tests, reason="Cosmos DB SDK not installed")
     def test_context_manager(self):
