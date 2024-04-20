ebb4c8cb5f953f58e2a59cf2eef123c9a6d7ae60
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2024-04-19 18:44:32 -0700

    [.Net] Add AutoGen.Mistral package (#2330)
    * add mstral client
    
    * enable streaming support
    
    * add mistralClientAgent
    
    * add test for function call
    
    * add extension
    
    * add support for toolcall and toolcall result message
    
    * add support for aggregate message
    
    * implement streaming function call

---
+++ dotnet/src/AutoGen.Mistral/Extension/MistralAgentExtension.cs
@@ -0,0 +1,34 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// MistralAgentExtension.cs
+
+using AutoGen.Core;
+
+namespace AutoGen.Mistral.Extension;
+
+public static class MistralAgentExtension
+{
+    public static MiddlewareStreamingAgent<MistralClientAgent> RegisterMessageConnector(
+        this MistralClientAgent agent, MistralChatMessageConnector? connector = null)
+    {
+        if (connector == null)
+        {
+            connector = new MistralChatMessageConnector();
+        }
+
+        return agent.RegisterStreamingMiddleware(connector)
+                    .RegisterMiddleware(connector);
+
+    }
+
+    public static MiddlewareStreamingAgent<MistralClientAgent> RegisterMessageConnector(
+        this MiddlewareStreamingAgent<MistralClientAgent> agent, MistralChatMessageConnector? connector = null)
+    {
+        if (connector == null)
+        {
+            connector = new MistralChatMessageConnector();
+        }
+
+        return agent.RegisterStreamingMiddleware(connector)
+                    .RegisterMiddleware(connector);
+    }
+}
