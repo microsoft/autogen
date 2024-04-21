01477b19f886c5fd408c277a5c359108a0cc50e3
Gagan Bansal <gaganbansal1993@gmail.com>
2024-04-21 14:34:34 -0700

    Fix pylance errors

--- autogen/experimental/drivers/legacy_terminal.py
+++ autogen/experimental/drivers/legacy_terminal.py
@@ -1,4 +1,5 @@
 from autogen.experimental.chat import ChatOrchestratorStream
+from autogen.experimental.types import AssistantMessage, FunctionCallMessage, UserMessage
 
 
 async def legacy_run_in_terminal(chat: ChatOrchestratorStream) -> str:
@@ -6,7 +7,21 @@ async def legacy_run_in_terminal(chat: ChatOrchestratorStream) -> str:
     while not chat.done:
         step = await chat.step()
         print(f"{step.__class__.__name__}:\n")
-        print(step.content)
-        print("-" * 80)
+
+        content = None
+
+        if isinstance(step, UserMessage) or isinstance(step, AssistantMessage):
+            if isinstance(step.content, str):
+                content = step.content
+
+        elif isinstance(step, FunctionCallMessage):
+            content = ""
+            call_results = step.call_results
+            for call_result in call_results:
+                content += call_result.content + "\n"
+
+        if isinstance(content, str):
+            print(content)
+            print("-" * 80)
 
     return chat.result.summary
