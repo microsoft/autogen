562f46e5701439b2602703b98c7d28942bcd0b52
William W Wang <107702013+wmwxwa@users.noreply.github.com>
2024-04-22 16:14:40 -0400

    Update test_cache.py

--- test/cache/test_cache.py
+++ test/cache/test_cache.py
@@ -1,8 +1,4 @@
 #!/usr/bin/env python3 -m pytest
-import sys
-import subprocess
-# implement pip as a subprocess:
-subprocess.check_call([sys.executable, '-m', 'pip', 'install', '<azure-cosmos>'])
 
 import unittest
 from typing import Optional, TypedDict, Union
