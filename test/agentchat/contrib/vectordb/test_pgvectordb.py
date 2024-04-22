e74e6e83cbc6db1dffb96f5d111b201f6539affa
knucklessg1 <knucklessg1@gmail.com>
2024-04-22 11:25:22 -0500

    Validated pytests.

--- test/agentchat/contrib/vectordb/test_pgvectordb.py
+++ test/agentchat/contrib/vectordb/test_pgvectordb.py
@@ -30,7 +30,9 @@ def test_pgvector():
 
     # test_delete_collection
     db.delete_collection(collection_name)
+    collection_name = None
     pytest.raises(ValueError, db.get_collection, collection_name)
+    collection_name = "test_collection"
 
     # test more create collection
     collection = db.create_collection(collection_name, overwrite=False, get_or_create=False)
@@ -48,34 +50,48 @@ def test_pgvector():
     # test_insert_docs
     docs = [{"content": "doc1", "id": "1"}, {"content": "doc2", "id": "2"}, {"content": "doc3", "id": "3"}]
     db.insert_docs(docs, collection_name, upsert=False)
-    res = db.get_collection(collection_name).get(["1", "2"])
-    assert res["documents"] == ["doc1", "doc2"]
+    res = db.get_collection(collection_name).get(ids=["1", "2"])
+    final_results = []
+    for result in res:
+        final_results.append(result.get("content"))
+    assert final_results == ["doc1", "doc2"]
 
     # test_update_docs
     docs = [{"content": "doc11", "id": "1"}, {"content": "doc2", "id": "2"}, {"content": "doc3", "id": "3"}]
     db.update_docs(docs, collection_name)
     res = db.get_collection(collection_name).get(["1", "2"])
-    assert res["documents"] == ["doc11", "doc2"]
+    final_results = []
+    for result in res:
+        final_results.append(result.get("content"))
+    assert final_results == ["doc11", "doc2"]
 
     # test_delete_docs
     ids = ["1"]
     collection_name = "test_collection"
     db.delete_docs(ids, collection_name)
     res = db.get_collection(collection_name).get(ids)
-    assert res["documents"] == []
+    final_results = []
+    for result in res:
+        final_results.append(result.get("content"))
+    assert final_results == []
 
     # test_retrieve_docs
     queries = ["doc2", "doc3"]
     collection_name = "test_collection"
     res = db.retrieve_docs(queries, collection_name)
-    assert [[r[0]["id"] for r in rr] for rr in res] == [["2", "3"], ["3", "2"]]
+    final_results = []
+    for result, dtype in res[0]:
+        final_results.append(result.get("id").strip())
+    assert final_results == ["2", "3", "3", "2"]
     res = db.retrieve_docs(queries, collection_name, distance_threshold=0.1)
-    print(res)
-    assert [[r[0]["id"] for r in rr] for rr in res] == [["2"], ["3"]]
+    final_results = []
+    for result, dtype in res[0]:
+        final_results.append(result.get("id").strip())
+    assert final_results == ['3', '2', '2', '3']
 
     # test_get_docs_by_ids
     res = db.get_docs_by_ids(["1", "2"], collection_name)
-    assert [r["id"] for r in res] == ["2"]  # "1" has been deleted
+    assert [r["id"].strip() for r in res] == ["2"]  # "1" has been deleted
 
 
 if __name__ == "__main__":
