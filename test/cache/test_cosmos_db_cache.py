04dcc952c9f4748a06d947dbc7b879c8ee7cd937
William W Wang <107702013+wmwxwa@users.noreply.github.com>
2024-04-25 16:21:35 -0400

    Update test_cosmos_db_cache.py

--- test/cache/test_cosmos_db_cache.py
+++ test/cache/test_cosmos_db_cache.py
@@ -9,7 +9,7 @@ from azure.cosmos.exceptions import CosmosResourceNotFoundError
 
 class TestCosmosDBCache(unittest.TestCase):
     def setUp(self):
-        self.seed = "test_seed"
+        self.seed = 42
         self.connection_string = "AccountEndpoint=https://example.documents.azure.com:443/;"
         self.database_id = "autogen_cache"
         self.container_id = "TestContainer"
@@ -27,12 +27,15 @@ class TestCosmosDBCache(unittest.TestCase):
         key = "key"
         value = "value"
         serialized_value = pickle.dumps(value)
-        cache = CosmosDBCache(self.seed, {
+        cache = CosmosDBCache(
+            self.seed, 
+            {
             "connection_string": self.connection_string,
             "database_id": self.database_id,
             "container_id": self.container_id,
-            "client": self.client
-        })
+            "client": self.client,
+            }
+        )
         cache.container.read_item.return_value = {"data": serialized_value}
         self.assertEqual(cache.get(key), value)
         cache.container.read_item.assert_called_with(item=key, partition_key=str(self.seed))
