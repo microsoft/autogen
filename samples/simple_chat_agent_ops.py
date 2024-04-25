da8584b50089ebf815a12f3cdf62d4e19a264fcc
Braelyn Boynton <bboynton97@gmail.com>
2024-04-25 15:05:56 -0700

    simple example

--- samples/simple_chat_agent_ops.py
+++ samples/simple_chat_agent_ops.py
@@ -1,7 +1,23 @@
-from autogen import ConversableAgent, UserProxyAgent, config_list_from_json
+from autogen import ConversableAgent, UserProxyAgent, config_list_from_json, register_function
 import agentops
+from typing import Annotated, Literal
 
-agentops.init(api_key='6b7a1469-bdcb-4d47-85ba-c4824bc8486e', endpoint='http://localhost:8000', tags=['autogen'])
+Operator = Literal["+", "-", "*", "/"]
+agentops.init(api_key='6b7a1469-bdcb-4d47-85ba-c4824bc8486e', endpoint='http://localhost:8000', tags=['autogen-tool'])
+
+
+def calculator(a: int, b: int, operator: Annotated[Operator, "operator"]) -> int:
+    if operator == "+":
+        return a + b
+    elif operator == "-":
+        return a - b
+    elif operator == "*":
+        return a * b
+    elif operator == "/":
+        return int(a / b)
+    else:
+        raise ValueError("Invalid operator")
+
 
 def main():
     # Load LLM inference endpoints from an env variable or a file
@@ -11,14 +27,43 @@ def main():
     config_list = config_list_from_json(env_or_file="OAI_CONFIG_LIST")
 
     # Create the agent that uses the LLM.
-    assistant = ConversableAgent("agent", llm_config={"config_list": config_list})
+    assistant = ConversableAgent(
+        name="Assistant",
+        system_message="You are a helpful AI assistant. "
+                       "You can help with simple calculations. "
+                       "Return 'TERMINATE' when the task is done.",
+        llm_config={"config_list": config_list},
+    )
 
-    # Create the agent that represents the user in the conversation.
-    user_proxy = UserProxyAgent("user", code_execution_config=False)
+    # The user proxy agent is used for interacting with the assistant agent
+    # and executes tool calls.
+    user_proxy = ConversableAgent(
+        name="User",
+        llm_config=False,
+        is_termination_msg=lambda msg: msg.get("content") is not None and "TERMINATE" in msg["content"],
+        human_input_mode="NEVER",
+    )
+
+    assistant.register_for_llm(name="calculator", description="A simple calculator")(calculator)
+    user_proxy.register_for_execution(name="calculator")(calculator)
+
+    # Register the calculator function to the two agents.
+    register_function(
+        calculator,
+        caller=assistant,  # The assistant agent can suggest calls to the calculator.
+        executor=user_proxy,  # The user proxy agent can execute the calculator calls.
+        name="calculator",  # By default, the function name is used as the tool name.
+        description="A simple calculator",  # A description of the tool.
+    )
 
     # Let the assistant start the conversation.  It will end when the user types exit.
-    assistant.initiate_chat(user_proxy, message="How can I help you today?")
+    user_proxy.initiate_chat(assistant, message="What is (1423 - 123) / 3 + (32 + 23) * 5?")
+
+    agentops.end_session('Success')
 
 
 if __name__ == "__main__":
     main()
+
+
+# What is (44232 + 13312 / (232 - 32)) * 5?
