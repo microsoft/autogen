5d65d7d12c8385d5786d593d2afa78b4acced3fb
weitian <weitian@microsoft.com>
2024-05-16 11:41:54 +0000

    use HuggingFaceClient() to execute task and add VQA capability

--- autogen/agentchat/contrib/huggingface_agent.py
+++ autogen/agentchat/contrib/huggingface_agent.py
@@ -1,6 +1,6 @@
 import copy
 import json
-from enum import Enum
+from enum import Flag, auto
 from typing import Any, Callable, Dict, List, Literal, Optional, Tuple, Union
 
 from huggingface_hub import ImageToTextOutput, InferenceClient
@@ -8,14 +8,16 @@ from typing_extensions import Annotated
 
 from autogen.agentchat import Agent, AssistantAgent, ConversableAgent, UserProxyAgent
 from autogen.agentchat.contrib import img_utils
+from autogen.agentchat.contrib.huggingface_utils import HuggingFaceClient
 from autogen.oai.client import OpenAIWrapper
 
 
-class HuggingFaceCapability(Enum):
+class HuggingFaceCapability(Flag):
     # Computer vision
-    IMAGE_TO_IMAGE = "image-to-image"
-    IMAGE_TO_TEXT = "image-to-text"
-    TEXT_TO_IMAGE = "text-to-image"
+    TEXT_TO_IMAGE = auto()
+    IMAGE_TO_IMAGE = auto()
+    IMAGE_TO_TEXT = auto()
+    VISUAL_QUESTION_ANSWERING = auto()
 
 
 class HuggingFaceAgent(ConversableAgent):
@@ -27,15 +29,20 @@ If your response contains an image path, wrap it in an HTML image tag as: <img "
     DEFAULT_DESCRIPTION = """A helpful assistant with multimodal capabilities. Ask them to perform image-to-text, text-to-image, speech-to-text, text-to-speech, and more!
 """
 
-    DEFAULT_HF_CAPABILITY_LIST = list(HuggingFaceCapability)
+    DEFAULT_HF_CAPABILITY = (
+        HuggingFaceCapability.TEXT_TO_IMAGE
+        | HuggingFaceCapability.IMAGE_TO_IMAGE
+        | HuggingFaceCapability.IMAGE_TO_TEXT
+        | HuggingFaceCapability.VISUAL_QUESTION_ANSWERING
+    )
 
     def __init__(
         self,
         name: str,
         system_message: Optional[Union[str, List[str]]] = DEFAULT_PROMPT,
+        hf_capability: Optional[HuggingFaceCapability] = DEFAULT_HF_CAPABILITY,
+        hf_capability_config_map: Optional[Dict[str, Dict[str, Any]]] = {},
         llm_config: Optional[Union[Dict, Literal[False]]] = None,
-        hf_capability_list: Optional[List[Union[str, HuggingFaceCapability]]] = DEFAULT_HF_CAPABILITY_LIST,
-        hf_config: Optional[Dict[str, Union[str, Dict]]] = {},
         is_gpt4v_format: Optional[bool] = False,
         is_silent: Optional[bool] = True,
         **kwargs,
@@ -46,9 +53,6 @@ If your response contains an image path, wrap it in an HTML image tag as: <img "
             llm_config=llm_config,
             **kwargs,
         )
-
-        self._is_gpt4v_format = is_gpt4v_format
-        self._is_silent = is_silent
 
         # Set up the inner monologue
         inner_llm_config = copy.deepcopy(llm_config)
@@ -69,45 +73,56 @@ If your response contains an image path, wrap it in an HTML image tag as: <img "
         )
 
         # Set up the HF InferenceClient
-        self._hf_capability_list = [HuggingFaceCapability[t] if isinstance(t, str) else t for t in hf_capability_list]
-        self._hf_config = hf_config
-        self._check_hf_config()
+        self._hf_capability = hf_capability
+        self._hf_clients = self._load_hf_capability_config(hf_capability, hf_capability_config_map)
 
-        self._hf_client = InferenceClient(
-            token=self._hf_config.get("api_key", None),
-            **self._hf_config.get("params", {}),
-        )
+        # Other configurations
+        self._is_gpt4v_format = is_gpt4v_format
+        self._is_silent = is_silent
 
         self._register_functions()
 
         self.register_reply([Agent, None], HuggingFaceAgent.generate_huggingface_reply, position=2)
 
-    def _check_hf_config(self) -> None:
-        valid_keys = [t.value for t in HuggingFaceCapability] + ["api_key", "params"]
-        for key in self._hf_config.keys():
-            if key not in valid_keys:
-                raise ValueError(f"Invalid key in hf_config: {key}. Valid keys are: {valid_keys}.")
+    def _load_hf_capability_config(
+        self, hf_capability: HuggingFaceCapability, hf_capability_config: Dict[str, Dict[str, Any]]
+    ) -> Dict[str, HuggingFaceClient]:
+        hf_clients = {}
+        for _hf_cap in HuggingFaceCapability:
+            if not hf_capability & _hf_cap:
+                continue
+
+            _hf_cap_config = hf_capability_config.get(_hf_cap.name, {})
+            _hf_cap_config_list = _hf_cap_config.get("config_list", [])
+            if len(_hf_cap_config_list) > 0:
+                model = _hf_cap_config_list[0].get("model", None)
+                api_key = _hf_cap_config_list[0].get("api_key", None)
+                inference_mode = _hf_cap_config_list[0].get("inference_mode", "auto")
+
+                hf_clients[_hf_cap] = HuggingFaceClient(
+                    hf_api_key=api_key,
+                    model=model,
+                    inference_mode=inference_mode,
+                )
+            else:
+                hf_clients[_hf_cap] = HuggingFaceClient()
+
+        return hf_clients
 
     def _register_functions(self) -> None:
-        # Helper functions
-        def _load_capability_config(capability: HuggingFaceCapability) -> Tuple[Union[str, None], Dict]:
-            capability_config = self._hf_config.get(capability.value, {})
-            model = capability_config.get("model", None)
-            params = capability_config.get("params", {})
-            return model, params
 
-        if HuggingFaceCapability.TEXT_TO_IMAGE in self._hf_capability_list:
+        if HuggingFaceCapability.TEXT_TO_IMAGE & self._hf_capability:
 
             @self._user_proxy.register_for_execution()
             @self._assistant.register_for_llm(
-                name=HuggingFaceCapability.TEXT_TO_IMAGE.value,
+                name=HuggingFaceCapability.TEXT_TO_IMAGE.name,
                 description="Generates images from input text.",
             )
-            def _text_to_image(text: Annotated[str, "The prompt to generate an image from"]) -> str:
+            def _text_to_image(text: Annotated[str, "The prompt to generate an image from."]) -> str:
                 import tempfile
 
-                model, params = _load_capability_config(HuggingFaceCapability.TEXT_TO_IMAGE)
-                image = self._hf_client.text_to_image(text, model=model, **params)
+                client = self._hf_clients[HuggingFaceCapability.TEXT_TO_IMAGE]
+                image = client.text_to_image(text)
                 with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_file:
                     image.save(temp_file.name)
                     response = f"I generated an image with the prompt: {text}"
@@ -115,35 +130,29 @@ If your response contains an image path, wrap it in an HTML image tag as: <img "
 
                 return response
 
-        if HuggingFaceCapability.IMAGE_TO_TEXT in self._hf_capability_list:
+        if HuggingFaceCapability.IMAGE_TO_TEXT & self._hf_capability:
 
             @self._user_proxy.register_for_execution()
             @self._assistant.register_for_llm(
-                name=HuggingFaceCapability.IMAGE_TO_TEXT.value,
+                name=HuggingFaceCapability.IMAGE_TO_TEXT.name,
                 description="Outputs a text from a given image. Image captioning or optical character recognition can be considered as the most common applications of image to text.",
             )
             def _image_to_text(
                 image_file: Annotated[
-                    str, "The path to the image file, a URL to an image, or a base64-encoded string of the image"
+                    str, "The path to the image file, a URL to an image, or a base64-encoded string of the image."
                 ]
             ) -> str:
-                model, params = _load_capability_config(HuggingFaceCapability.IMAGE_TO_TEXT)
-                image_data = img_utils.get_image_data(image_file, use_b64=False)
-                raw_response = self._hf_client.post(
-                    data=image_data,
-                    model=model,
-                    task=HuggingFaceCapability.IMAGE_TO_TEXT.value,
-                )
-                generated_text = ImageToTextOutput.parse_obj_as_list(raw_response)[0].generated_text
+                client = self._hf_clients[HuggingFaceCapability.IMAGE_TO_TEXT]
+                generated_text = client.image_to_text(image_file)
                 response = f"I generated the following text from the image: {generated_text}"
 
                 return response
 
-        if HuggingFaceCapability.IMAGE_TO_IMAGE in self._hf_capability_list:
+        if HuggingFaceCapability.IMAGE_TO_IMAGE & self._hf_capability:
 
             @self._user_proxy.register_for_execution()
             @self._assistant.register_for_llm(
-                name=HuggingFaceCapability.IMAGE_TO_IMAGE.value,
+                name=HuggingFaceCapability.IMAGE_TO_IMAGE.name,
                 description="Transforms a source image to match the characteristics of a target image or a target image domain.",
             )
             def _image_to_image(
@@ -155,14 +164,32 @@ If your response contains an image path, wrap it in an HTML image tag as: <img "
             ) -> str:
                 import tempfile
 
-                model, params = _load_capability_config(HuggingFaceCapability.IMAGE_TO_IMAGE)
-                image_data = img_utils.get_image_data(image_file, use_b64=False)
-                tgt_image = self._hf_client.image_to_image(image_data, prompt=text, model=model, **params)
-
+                client = self._hf_clients[HuggingFaceCapability.IMAGE_TO_IMAGE]
+                tgt_image = client.image_to_image(image_file, text)
                 with tempfile.NamedTemporaryFile(suffix=".png", delete=False) as temp_file:
                     tgt_image.save(temp_file.name)
                     response = f"I generated an image from the input image with the prompt: {text}"
                     response += f"<img {temp_file.name}>"
+
+                return response
+
+        if HuggingFaceCapability.VISUAL_QUESTION_ANSWERING & self._hf_capability:
+
+            @self._user_proxy.register_for_execution()
+            @self._assistant.register_for_llm(
+                name=HuggingFaceCapability.VISUAL_QUESTION_ANSWERING.name,
+                description="Answers open-ended questions based on an image.",
+            )
+            def _visual_question_answering(
+                image_file: Annotated[
+                    str,
+                    "The input image for the context. It can be a path, a URL, or a base64-encoded string of the image.",
+                ],
+                question: Annotated[str, "The question to be answered."],
+            ) -> str:
+                client = self._hf_clients[HuggingFaceCapability.VISUAL_QUESTION_ANSWERING]
+                answer = client.visual_question_answering(image_file, question)
+                response = f"The answer to the question '{question}' is: {answer}"
 
                 return response
 
