6b87a635983370f99e3d78a93b7cdf0ae3cdecba
苏震巍 <zsu@senparc.com>
2024-05-17 19:24:44 +0800

    #2708 add Add a judgment to the graph constructor & added unit test

---
+++ dotnet/test/AutoGen.Core.Tests/GroupChat/GraphTests.cs
@@ -0,0 +1,17 @@
+﻿
+namespace AutoGen.Core.Tests
+{
+    [TestClass()]
+    public class GraphTests
+    {
+        [TestMethod()]
+        public void GraphTest()
+        {
+            var graph1 = new Graph();
+            Assert.IsNotNull(graph1);
+
+            var graph2 = new Graph(null);
+            Assert.IsNotNull(graph2);
+        }
+    }
+}
