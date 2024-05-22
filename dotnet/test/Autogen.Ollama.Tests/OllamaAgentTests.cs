24ceabb6ab34093f46e92accbe2a8b2ba553ef0b
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-05-22 00:28:43 -0700

    add llava test

--- dotnet/test/Autogen.Ollama.Tests/OllamaAgentTests.cs
+++ dotnet/test/Autogen.Ollama.Tests/OllamaAgentTests.cs
@@ -10,7 +10,6 @@ namespace AutoGen.Ollama.Tests;
 
 public class OllamaAgentTests
 {
-
     [ApiKeyFact("OLLAMA_HOST", "OLLAMA_MODEL_NAME")]
     public async Task GenerateReplyAsync_ReturnsValidMessage_WhenCalled()
     {
@@ -65,6 +64,72 @@ public class OllamaAgentTests
 
         var msg = new Message("user", "hey how are you");
         var messages = new IMessage[] { MessageEnvelope.Create(msg, from: modelName) };
+        IStreamingMessage? finalReply = default;
+        await foreach (IStreamingMessage message in ollamaAgent.GenerateStreamingReplyAsync(messages))
+        {
+            message.Should().NotBeNull();
+            message.From.Should().Be(ollamaAgent.Name);
+            var streamingMessage = (IMessage<ChatResponseUpdate>)message;
+            if (streamingMessage.Content.Done)
+            {
+                finalReply = message;
+                break;
+            }
+            else
+            {
+                streamingMessage.Content.Message.Should().NotBeNull();
+                streamingMessage.Content.Done.Should().BeFalse();
+            }
+        }
+
+        finalReply.Should().BeOfType<MessageEnvelope<ChatResponse>>();
+        var update = ((MessageEnvelope<ChatResponse>)finalReply!).Content;
+        update.Done.Should().BeTrue();
+        update.TotalDuration.Should().BeGreaterThan(0);
+    }
+
+    [ApiKeyFact("OLLAMA_HOST")]
+    public async Task ItReturnValidMessageUsingLLavaAsync()
+    {
+        var host = Environment.GetEnvironmentVariable("OLLAMA_HOST")
+                   ?? throw new InvalidOperationException("OLLAMA_HOST is not set.");
+        var modelName = "llava:latest";
+        var ollamaAgent = BuildOllamaAgent(host, modelName);
+        var squareImagePath = Path.Combine("images", "square.png");
+        var base64Image = Convert.ToBase64String(File.ReadAllBytes(squareImagePath));
+        var message = new Message()
+        {
+            Role = "user",
+            Value = "What's in this image?",
+            Images = [base64Image],
+        };
+
+        var messages = new IMessage[] { MessageEnvelope.Create(message, from: modelName) };
+        var reply = await ollamaAgent.GenerateReplyAsync(messages);
+
+        reply.Should().BeOfType<MessageEnvelope<ChatResponse>>();
+        var chatResponse = ((MessageEnvelope<ChatResponse>)reply).Content;
+        chatResponse.Message.Should().NotBeNull();
+    }
+
+    [ApiKeyFact("OLLAMA_HOST")]
+    public async Task ItReturnValidStreamingMessageUsingLLavaAsync()
+    {
+        var host = Environment.GetEnvironmentVariable("OLLAMA_HOST")
+                   ?? throw new InvalidOperationException("OLLAMA_HOST is not set.");
+        var modelName = "llava:latest";
+        var ollamaAgent = BuildOllamaAgent(host, modelName);
+        var squareImagePath = Path.Combine("images", "square.png");
+        var base64Image = Convert.ToBase64String(File.ReadAllBytes(squareImagePath));
+        var imageMessage = new Message()
+        {
+            Role = "user",
+            Value = "What's in this image?",
+            Images = [base64Image],
+        };
+
+        var messages = new IMessage[] { MessageEnvelope.Create(imageMessage, from: modelName) };
+
         IStreamingMessage? finalReply = default;
         await foreach (IStreamingMessage message in ollamaAgent.GenerateStreamingReplyAsync(messages))
         {
