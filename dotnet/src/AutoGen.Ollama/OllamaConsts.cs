a4d625d0bb5e9288e0ee8f15dc284afb0b640797
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-05-23 09:57:15 -0700

    add AutoGen.ollama

---
+++ dotnet/src/AutoGen.Ollama/OllamaConsts.cs
@@ -0,0 +1,12 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// OllamaConsts.cs
+
+namespace AutoGen.Ollama;
+
+public class OllamaConsts
+{
+    public const string JsonFormatType = "json";
+    public const string JsonMediaType = "application/json";
+    public const string ChatCompletionEndpoint = "/api/chat";
+    public const string EmbeddingsEndpoint = "/api/embeddings";
+}
