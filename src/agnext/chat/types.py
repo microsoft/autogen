52f6f7959187b29a77cf387b2ebf02858ec44fd4
Jack Gerrits <jackgerrits@users.noreply.github.com>
2024-05-23 16:49:01 -0400

    Add types agnostic to role (#11)

---
+++ src/agnext/chat/types.py
@@ -0,0 +1,42 @@
+from __future__ import annotations
+
+from dataclasses import dataclass
+from typing import List, Union
+
+from agnext.agent_components.image import Image
+from agnext.agent_components.types import FunctionCall
+
+
+@dataclass(kw_only=True)
+class BaseMessage:
+    # Name of the agent that sent this message
+    source: str
+
+
+@dataclass
+class TextMessage(BaseMessage):
+    content: str
+
+
+@dataclass
+class MultiModalMessage(BaseMessage):
+    content: List[Union[str, Image]]
+
+
+@dataclass
+class FunctionCallMessage(BaseMessage):
+    content: List[FunctionCall]
+
+
+@dataclass
+class FunctionExecutionResult:
+    content: str
+    call_id: str
+
+
+@dataclass
+class FunctionExecutionResultMessage(BaseMessage):
+    content: List[FunctionExecutionResult]
+
+
+Message = Union[TextMessage, MultiModalMessage, FunctionCallMessage, FunctionExecutionResultMessage]
