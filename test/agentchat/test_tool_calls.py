04780e3d2e9892d22f86827e3097fd9668640c2e
Qingyun WU <qingyun0327@gmail.com>
2024-06-12 00:24:18 -0700

    use gpt-4 as tag

--- test/agentchat/test_tool_calls.py
+++ test/agentchat/test_tool_calls.py
@@ -144,7 +144,7 @@ def test_update_tool():
     config_list_gpt4 = autogen.config_list_from_json(
         OAI_CONFIG_LIST,
         filter_dict={
-            "tags": ["tool"],
+            "tags": ["gpt-4"],
         },
         file_location=KEY_LOC,
     )
