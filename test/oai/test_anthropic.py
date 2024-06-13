1da92ccfcf8e29f87ac3c48442a21e1e98ea3d05
Hk669 <hrushi669@gmail.com>
2024-06-13 17:00:24 +0530

    update: added tests to the anthropic client

---
+++ test/oai/test_anthropic.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3 -m pytest
+
+import os
+import shutil
+import sys
+import time
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "..")))
+import pytest
+
+from autogen.oai.anthropic import AnthropicClient, calculate_cost
+
+try:
+    from openai.types.chat.chat_completion import ChatCompletionMessage
+
+    skip = False
+except ImportError:
+    AnthropicClient = object
+    InternalServerError = object
+    skip = True
+
+reason = "Anthropic dependency not installed"
+
+
+@pytest.fixture()
+def anthropic_client():
+    config = {
+        "model": "claude-3-sonnet-20240229",
+        "api_key": "dummy_api_key",
+    }
+    return AnthropicClient(config)
+
+
+@pytest.mark.skipif(skip, reason=reason)
+def test_anthropic_client():
+    with pytest.raises(AssertionError) as assertinfo:
+        AnthropicClient()  # Should raise an AssertionError due to missing api_key
+
+    assert (
+        "Please specify the 'api_key' in your config list entry for Mistral or set the MISTRAL_API_KEY env variable."
+        in str(assertinfo.value)
+    )
+
+    # Creation works
+    AnthropicClient(api_key="fake_api_key")  # Should create okay now.
+
+
+@pytest.mark.skipif(skip, reason=reason)
+def test_intialization(anthropic_client):
+    assert anthropic_client.api_key == "dummy_api_key", "`api_key` should be correctly set in the config"
