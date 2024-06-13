97de75e9be69cc9f6d551d45276b0eec9cc9fdaf
Wael Karkoub <wael.karkoub96@gmail.com>
2024-06-13 05:53:55 +0100

    human input mode annotations fixed (#2864)
    Co-authored-by: Chi Wang <wang.chi@microsoft.com>

--- autogen/agentchat/contrib/web_surfer.py
+++ autogen/agentchat/contrib/web_surfer.py
@@ -34,7 +34,7 @@ class WebSurferAgent(ConversableAgent):
         description: Optional[str] = DEFAULT_DESCRIPTION,
         is_termination_msg: Optional[Callable[[Dict[str, Any]], bool]] = None,
         max_consecutive_auto_reply: Optional[int] = None,
-        human_input_mode: Optional[str] = "TERMINATE",
+        human_input_mode: Literal["ALWAYS", "NEVER", "TERMINATE"] = "TERMINATE",
         function_map: Optional[Dict[str, Callable]] = None,
         code_execution_config: Union[Dict, Literal[False]] = False,
         llm_config: Optional[Union[Dict, Literal[False]]] = None,
@@ -111,7 +111,9 @@ class WebSurferAgent(ConversableAgent):
             self.summarizer_llm_config = summarizer_llm_config  # type: ignore[assignment]
 
         # Create the summarizer client
-        self.summarization_client = None if self.summarizer_llm_config is False else OpenAIWrapper(**self.summarizer_llm_config)  # type: ignore[arg-type]
+        self.summarization_client = (
+            None if self.summarizer_llm_config is False else OpenAIWrapper(**self.summarizer_llm_config)
+        )  # type: ignore[arg-type]
 
     def _register_functions(self) -> None:
         """Register the functions for the inner assistant and user proxy."""
@@ -250,7 +252,7 @@ class WebSurferAgent(ConversableAgent):
             def _summarize_page(
                 url: Annotated[
                     Optional[str], "[Optional] The url of the page to summarize. (Defaults to current page)"
-                ] = None
+                ] = None,
             ) -> str:
                 return _answer_from_page(url=url, question=None)
 
