fa2339e3323f20191578968f8949f20265b8aa81
Mark Sze <mark@sze.family>
2024-06-15 20:26:46 +0000

    Addition of client utilities, initially for parameter validation

---
+++ autogen/oai/client_utils.py
@@ -0,0 +1,52 @@
+"""Utilities for client classes"""
+
+import warnings
+from typing import Any, Dict, List, Tuple
+
+
+def validate_parameter(
+    params: Dict,
+    param_name: str,
+    allowed_types: Tuple,
+    allow_None: bool,
+    default_value: Any,
+    numerical_bound: Tuple,
+    allowed_values: list,
+):
+    """Validates a given config parameter, checking its type, values, and setting defaults"""
+    param_value = params.get(param_name, default_value)
+    warning = ""
+
+    if param_value is None and allow_None:
+        pass
+    elif param_value is None and not allow_None:
+        warning = "cannot be None"
+    elif not isinstance(param_value, allowed_types):
+        # Check types and list possible types if invalid
+        if isinstance(allowed_types, tuple):
+            formatted_types = "(" + ", ".join(f"{t.__name__}" for t in allowed_types) + ")"
+        else:
+            formatted_types = f"{allowed_types.__name__}"
+        warning = f"must be of type {formatted_types}{' or None' if allow_None else ''}"
+    elif numerical_bound:
+        # Check the value fits in possible bounds
+        lower_bound, upper_bound = numerical_bound
+        if (lower_bound is not None and param_value < lower_bound) or (
+            upper_bound is not None and param_value > upper_bound
+        ):
+            warning = f"has numerical bounds, {'>= ' + str(lower_bound) if lower_bound is not None else ''}{' and ' if lower_bound is not None and upper_bound is not None else ''}{'<= ' + str(upper_bound) if upper_bound is not None else ''}{', or can be None' if allow_None else ''}"
+    elif allowed_values:
+        # Check if the value matches any allowed values
+        if not (allow_None and param_value is None):
+            if param_value not in allowed_values:
+                warning = f"must be one of these values [{allowed_values}]{', or can be None' if allow_None else ''}"
+
+    # If we failed any checks, warn and set to default value
+    if warning:
+        warnings.warn(
+            f"Config error - {param_name} {warning}, defaulting to {default_value}.",
+            UserWarning,
+        )
+        param_value = default_value
+
+    return param_value
