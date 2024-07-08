9ffdc6b9c1d77c7fbab49c988a8244e5189e0cb9
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-07-08 14:42:18 -0700

    add sample project

---
+++ dotnet/sample/AutoGen.Service.Sample/Program.cs
@@ -0,0 +1,42 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Program.cs
+
+using System.Runtime.CompilerServices;
+using AutoGen.Core;
+using AutoGen.Service;
+
+var dummyAgent = new DummyAgent();
+var builder = WebApplication.CreateBuilder(args);
+// Add services to the container.
+
+// run endpoint at port 5000
+builder.WebHost.UseUrls("http://localhost:5000");
+var app = builder.Build();
+
+app.UseAgentAsOpenAIChatCompletionEndpoint(dummyAgent);
+
+app.Run();
+
+public class DummyAgent : IStreamingAgent
+{
+    public DummyAgent(string name = "dummy")
+    {
+        Name = name;
+    }
+
+    public string Name { get; }
+
+    public async Task<IMessage> GenerateReplyAsync(IEnumerable<IMessage> messages, GenerateReplyOptions? options = null, CancellationToken cancellationToken = default)
+    {
+        return new TextMessage(Role.Assistant, "I am dummy agent", this.Name);
+    }
+
+    public async IAsyncEnumerable<IMessage> GenerateStreamingReplyAsync(IEnumerable<IMessage> messages, GenerateReplyOptions? options = null, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+    {
+        var reply = "I am dummy agent";
+        foreach (var c in reply)
+        {
+            yield return new TextMessageUpdate(Role.Assistant, c.ToString(), this.Name);
+        };
+    }
+}
