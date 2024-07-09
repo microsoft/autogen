14628f2ae09b75ff89c6db40a877aafb7bb01258
afourney <adam.fourney@gmail.com>
2024-07-09 14:43:16 -0700

    add ParametersSchema to tools exports (#199)

--- python/src/agnext/components/tools/__init__.py
+++ python/src/agnext/components/tools/__init__.py
@@ -1,10 +1,11 @@
-from ._base import BaseTool, BaseToolWithState, Tool, ToolSchema
+from ._base import BaseTool, BaseToolWithState, Tool, ToolSchema, ParametersSchema
 from ._code_execution import CodeExecutionInput, CodeExecutionResult, PythonCodeExecutionTool
 from ._function_tool import FunctionTool
 
 __all__ = [
     "Tool",
     "ToolSchema",
+    "ParametersSchema",
     "BaseTool",
     "BaseToolWithState",
     "PythonCodeExecutionTool",
