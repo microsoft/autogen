6ef5c3027dee7093b49ef62e2bd11ae1760dd3c1
Li Jiang <bnujli@gmail.com>
2024-07-16 14:44:48 +0800

    Update qdrant notebook for new qdrant vectordb (#3140)
    * Add qdrant notebook, rename notebooks
    
    * Revert changes of pgvector notebook
    
    * Fix assertion error

--- autogen/agentchat/contrib/vectordb/qdrant.py
+++ autogen/agentchat/contrib/vectordb/qdrant.py
@@ -92,7 +92,7 @@ class QdrantVectorDB(VectorDB):
             collection_options: dict | The options for creating the collection.
             kwargs: dict | Additional keyword arguments.
         """
-        self.client: QdrantClient = client if client is not None else QdrantClient(location=":memory:")
+        self.client: QdrantClient = client or QdrantClient(location=":memory:")
         self.embedding_function = FastEmbedEmbeddingFunction() or embedding_function
         self.collection_options = collection_options
         self.content_payload_key = content_payload_key
@@ -102,6 +102,10 @@ class QdrantVectorDB(VectorDB):
     def create_collection(self, collection_name: str, overwrite: bool = False, get_or_create: bool = True) -> None:
         """
         Create a collection in the vector database.
+        Case 1. if the collection does not exist, create the collection.
+        Case 2. the collection exists, if overwrite is True, it will overwrite the collection.
+        Case 3. the collection exists and overwrite is False, if get_or_create is True, it will get the collection,
+            otherwise it raise a ValueError.
 
         Args:
             collection_name: str | The name of the collection.
@@ -122,6 +126,8 @@ class QdrantVectorDB(VectorDB):
                 vectors_config=models.VectorParams(size=embeddings_size, distance=models.Distance.COSINE),
                 **self.collection_options,
             )
+        elif not get_or_create:
+            raise ValueError(f"Collection {collection_name} already exists.")
 
     def get_collection(self, collection_name: str = None):
         """
