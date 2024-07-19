9950e095badf9d8b1ae06b11d080fc3aad41d329
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-07-19 11:02:59 -0700

    rename Service -> WebAPI

--- dotnet/src/AutoGen.WebAPI/OpenAI/Converter/OpenAIMessageConverter.cs
+++ dotnet/src/AutoGen.WebAPI/OpenAI/Converter/OpenAIMessageConverter.cs
@@ -5,7 +5,7 @@ using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
-namespace AutoGen.Service.OpenAI.DTO;
+namespace AutoGen.WebAPI.OpenAI.DTO;
 
 internal class OpenAIMessageConverter : JsonConverter<OpenAIMessage>
 {
