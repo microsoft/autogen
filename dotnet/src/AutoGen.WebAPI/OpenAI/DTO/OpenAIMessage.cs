924420a617dbd205690cbb34496f6c5fa1a3b28e
Xiaoyun Zhang <bigmiao.zhang@gmail.com>
2024-07-22 09:44:12 -0700

    rename Service -> WebAPI (#3177)

--- dotnet/src/AutoGen.WebAPI/OpenAI/DTO/OpenAIMessage.cs
+++ dotnet/src/AutoGen.WebAPI/OpenAI/DTO/OpenAIMessage.cs
@@ -3,7 +3,7 @@
 
 using System.Text.Json.Serialization;
 
-namespace AutoGen.Service.OpenAI.DTO;
+namespace AutoGen.WebAPI.OpenAI.DTO;
 
 [JsonConverter(typeof(OpenAIMessageConverter))]
 internal abstract class OpenAIMessage
