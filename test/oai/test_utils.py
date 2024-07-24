1c720af7339bb2e309dec69431ebca39032ea68f
gongwn1 <gongwn1@lenovo.com>
2024-07-24 19:00:11 +0800

    fix secret uncovered

--- test/oai/test_utils.py
+++ test/oai/test_utils.py
@@ -55,7 +55,7 @@ JSON_SAMPLE = """
     {
         "model": "gpt-35-turbo-v0301",
         "tags": ["gpt-3.5-turbo", "gpt35_turbo"],
-        "api_key": "testkey111111111",
+        "api_key": "keyhere",
         "base_url": "https://1111.openai.azure.com",
         "api_type": "azure",
         "api_version": "2024-02-01"
