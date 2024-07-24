2e542875e64c239be16761c4d1059d0162449b58
gongwn1 <gongwn1@lenovo.com>
2024-07-24 20:56:17 +0800

    for security checks

--- test/oai/test_utils.py
+++ test/oai/test_utils.py
@@ -55,8 +55,8 @@ JSON_SAMPLE = """
     {
         "model": "gpt-35-turbo-v0301",
         "tags": ["gpt-3.5-turbo", "gpt35_turbo"],
-        "api_key": "keyhere",
-        "base_url": "https://1111.openai.azure.com",
+        "api_key": "azureoaiapikey",
+        "base_url": "https://deployment_name.openai.azure.com",
         "api_type": "azure",
         "api_version": "2024-02-01"
     },
