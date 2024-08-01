a9ff72e29e9320c2229e4af70d9e4b2a21396078
XiaoYun Zhang <xiaoyuz@microsoft.com>
2024-08-01 10:07:04 -0700

    only add the last message to chat history in SendAsync

--- dotnet/test/AutoGen.Tests/GroupChat/GroupChatTests.cs
+++ dotnet/test/AutoGen.Tests/GroupChat/GroupChatTests.cs
@@ -4,8 +4,10 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 using FluentAssertions;
+using Moq;
 using Xunit;
 
 namespace AutoGen.Tests;
@@ -50,5 +52,37 @@ public class GroupChatTests
 
         chatHistory.Count().Should().Be(3);
         chatHistory.Last().From.Should().Be("Cathy");
+    }
+
+    [Fact]
+    public async Task ItSendAsyncDoesntAddDuplicateInitializeMessagesTest()
+    {
+        // fix #3268
+        var alice = new DefaultReplyAgent("Alice", "I am alice");
+        var bob = new DefaultReplyAgent("Bob", "I am bob");
+        var cathy = new DefaultReplyAgent("Cathy", $"I am cathy, {GroupChatExtension.TERMINATE}");
+
+        var roundRobinOrchestrator = new RoundRobinOrchestrator();
+        var orchestrator = Mock.Of<IOrchestrator>();
+        Mock.Get(orchestrator).Setup(x => x.GetNextSpeakerAsync(It.IsAny<OrchestrationContext>(), It.IsAny<CancellationToken>()))
+            .Returns((OrchestrationContext context, CancellationToken token) =>
+            {
+                // determine if initialize message is already sent and not added twice
+                context.ChatHistory.Where(x => x.From == alice.Name).Count().Should().Be(1);
+
+                return roundRobinOrchestrator.GetNextSpeakerAsync(context, token);
+            });
+
+        var groupChat = new GroupChat([alice, bob, cathy], orchestrator);
+        groupChat.AddInitializeMessage(new TextMessage(Role.User, "Hello", from: alice.Name));
+
+        var maxRound = 2;
+        var chatHistory = new List<IMessage>();
+        await foreach (var message in groupChat.SendAsync(chatHistory, maxRound))
+        {
+            chatHistory.Add(message);
+        }
+
+        chatHistory.Count().Should().Be(2);
     }
 }
