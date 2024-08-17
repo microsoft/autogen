f4c08e49423280397b5371dd5651b031d534d2ef
David Luong <davidluong98@gmail.com>
2024-08-17 13:33:26 -0400

    Fix import ordering for build

--- dotnet/src/AutoGen.Anthropic/Converters/SystemMessageConverter.cs
+++ dotnet/src/AutoGen.Anthropic/Converters/SystemMessageConverter.cs
@@ -1,8 +1,8 @@
 ﻿// Copyright (c) Microsoft Corporation. All rights reserved.
 // SystemMessageConverter.cs
 
-using System;
 using AutoGen.Anthropic.DTO;
+using System;
 using System.Text.Json;
 using System.Text.Json.Serialization;
 
