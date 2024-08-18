aaee3a24f0503e2734fe80762aaeda0cc91ffa55
David Luong <davidluong98@gmail.com>
2024-08-18 12:14:51 -0400

    [.NET] Add cache control to Anthropic Client (#3372)
    * Add cache control to anthropic client and write unit test & exampel
    
    * PR comments
    
    * Fix import ordering for build
    
    * Fix import orderings
    
    ---------
    
    Co-authored-by: Xiaoyun Zhang <bigmiao.zhang@gmail.com>

--- dotnet/src/AutoGen.Anthropic/DTO/ChatCompletionResponse.cs
+++ dotnet/src/AutoGen.Anthropic/DTO/ChatCompletionResponse.cs
@@ -70,6 +70,12 @@ public class Usage
 
     [JsonPropertyName("output_tokens")]
     public int OutputTokens { get; set; }
+
+    [JsonPropertyName("cache_creation_input_tokens")]
+    public int CacheCreationInputTokens { get; set; }
+
+    [JsonPropertyName("cache_read_input_tokens")]
+    public int CacheReadInputTokens { get; set; }
 }
 
 public class Delta
