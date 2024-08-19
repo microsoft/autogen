d56321799b2ea7e22c1788f66fb2b84d1d9837b7
Zoltan Lux <lux.zoltan.andras@gmail.com>
2024-08-19 22:16:07 +0000

    try removing allowed function names in tool config conversion

--- autogen/oai/gemini.py
+++ autogen/oai/gemini.py
@@ -647,9 +647,9 @@ class GeminiClient:
                 func_calling_config_params["mode"] = VertexAIToolConfig.FunctionCallingConfig.Mode[
                     tool_config["function_calling_config"]["mode"]
                 ]
-                if func_calling_config_params["mode"] == VertexAIToolConfig.FunctionCallingConfig.Mode.ANY:
-                    # The function names are not yet known when parsing the OAI_CONFIG_LIST
-                    func_calling_config_params["allowed_function_names"] = [tool["function_name"] for tool in tools]
+                # if func_calling_config_params["mode"] == VertexAIToolConfig.FunctionCallingConfig.Mode.ANY:
+                #    # The function names are not yet known when parsing the OAI_CONFIG_LIST
+                #    func_calling_config_params["allowed_function_names"] = [tool["function_name"] for tool in tools]
                 vertexai_tool_config = VertexAIToolConfig(
                     function_calling_config=VertexAIToolConfig.FunctionCallingConfig(**func_calling_config_params)
                 )
