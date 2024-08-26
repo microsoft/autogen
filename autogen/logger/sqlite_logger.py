34b34d02034848e83427441b3ecc51b7666cefc2
HRUSHIKESH DOKALA <96101829+Hk669@users.noreply.github.com>
2024-08-26 12:58:53 +0530

    Amazon Bedrock Client for AutoGen (#3232)
    * intial commit for aws-bedrock
    
    * format
    
    * converse setup for model req-response
    
    * Renamed to bedrock.py, updated parameter parsing, system message extraction, client class incorporation
    
    * Established Bedrock class based on @astroalek and @ChristianT's code, added ability to disable system prompt separation
    
    * Image parsing and removing access credential checks
    
    * Added tests, added additional parameter support
    
    * Amazon Bedrock documentation
    
    * Moved client parameters to init, align parameter names with Anthropic, spelling fix, remove unnecessary imports, use base and additional parameters, update documentation
    
    * Tidy up comments
    
    * Minor typo fix
    
    * Correct comment re aws_region
    
    ---------
    
    Co-authored-by: Mark Sze <mark@sze.family>
    Co-authored-by: Mark Sze <66362098+marklysze@users.noreply.github.com>
    Co-authored-by: Li Jiang <bnujli@gmail.com>

--- autogen/logger/sqlite_logger.py
+++ autogen/logger/sqlite_logger.py
@@ -19,6 +19,7 @@ from .base_logger import LLMConfig
 if TYPE_CHECKING:
     from autogen import Agent, ConversableAgent, OpenAIWrapper
     from autogen.oai.anthropic import AnthropicClient
+    from autogen.oai.bedrock import BedrockClient
     from autogen.oai.cohere import CohereClient
     from autogen.oai.gemini import GeminiClient
     from autogen.oai.groq import GroqClient
@@ -402,6 +403,7 @@ class SqliteLogger(BaseLogger):
             TogetherClient,
             GroqClient,
             CohereClient,
+            BedrockClient,
         ],
         wrapper: OpenAIWrapper,
         init_args: Dict[str, Any],
