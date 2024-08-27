bbb056ca3d9f5cda7205462928cb57dce28d6986
Aristo <6344553+randombet@users.noreply.github.com>
2024-08-27 17:38:58 +0000

    Add GraphRAG document class

--- autogen/agentchat/contrib/graph_rag/graph_rag_agent.py
+++ autogen/agentchat/contrib/graph_rag/graph_rag_agent.py
@@ -1,12 +1,12 @@
-from abc import ABC, abstractmethod
-from typing import List
+from typing import List, Protocol
 
 from autogen.agentchat import ConversableAgent
 
+from .document import Document
 from .graph_store import GraphStore
 
 
-class GraphRagAgent(ConversableAgent, ABC):
+class GraphRagAgent(ConversableAgent, Protocol):
     """
     A graph rag agent is a conversable agent which could query graph database for answers.
 
@@ -14,10 +14,41 @@ class GraphRagAgent(ConversableAgent, ABC):
     1. create a graph in the underlying database with input documents
     2. use the retrieve() method to retrieve information.
     3. use the retrieved information to generate and send back messages.
+
+    For example,
+    graph_rag_agent = GraphRagAgent(
+        name="movie knowledge graph agent",
+        human_input_mode="NEVER",
+        max_consecutive_auto_reply=3,
+        retrieve_config={
+            "docs_path": [
+                "./data/movies.txt",
+            ],
+            "llm_config" = autogen.config_list_from_json("OAI_CONFIG_LIST")
+            "database_config" = {
+                "host": "127.0.0.1",
+                "port": 6379,
+                "table_name": "movies"
+            }
+        },
+    )
+
+    # initialize database (internally)
+    # self._init_db(input_doc=[Document(doc) for doc in retrieve_config["docs_path"]])
+
+    question = "Name a few actors who've played in 'The Matrix'"
+
+    answer = graph_rag_agent.retrieve(question)
+
+    # answer:
+    # A few actors who have played in 'The Matrix' are:
+    # - Keanu Reeves
+    # - Laurence Fishburne
+    # - Carrie-Anne Moss
+    # - Hugo Weaving
     """
 
-    @abstractmethod
-    def _init_db(self, input_doc: List | None = None) -> GraphStore:
+    def _init_db(self, input_doc: List[Document] | None = None) -> GraphStore:
         """
         This method initializes graph database with the input documents or records.
         Usually, it takes the following steps,
@@ -25,18 +56,19 @@ class GraphRagAgent(ConversableAgent, ABC):
         2. extract graph nodes, edges based on input data, graph schema and etc.
         3. build indexes etc.
 
-        return: GraphStore
+        Args:
+        input_doc: a list of input documents that are used to build the graph in database.
+
+        Returns: GraphStore
         """
         pass
 
-    @abstractmethod
     def retrieve(self, question: str, **kwargs):
         """
         Retrieve answers with human readable questions.
         """
         pass
 
-    @abstractmethod
     def add_records(self, new_records: List) -> bool:
         """
         Add new records to the underlying database and add to the graph if required.
--- autogen/agentchat/contrib/graph_rag/graph_store.py
+++ autogen/agentchat/contrib/graph_rag/graph_store.py
@@ -1,24 +1,27 @@
-from abc import ABC, abstractmethod
 from dataclasses import dataclass
+from typing import List, Optional, Protocol
 
 
 @dataclass
 class GraphStoreQueryResult:
     """
     A wrapper of graph store query results.
+
+    answer: human readable answer to question/query.
+    results: intermediate results to question/query, e.g. node entities.
     """
 
-    answer: str
+    answer: Optional[str] = None
+    results: Optional[List] = []
 
 
-class GraphStore(ABC):
+class GraphStore(Protocol):
     """An abstract base class that represents a underlying graph database.
 
     This interface defines the basic methods which are required by implementing graph rag from graph database.
     """
 
-    @abstractmethod
-    def query(self, question: str, **kwargs) -> GraphStoreQueryResult:
+    def query(self, question: str, n_results: int = 1, **kwargs) -> GraphStoreQueryResult:
         """
         This method transform a string format question into database query and return the result.
         """
