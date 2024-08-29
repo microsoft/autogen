581cda0cfd59669f2e05f0216f49cad420af2bf1
Jack Gerrits <jackgerrits@users.noreply.github.com>
2024-08-29 14:37:03 -0400

    Pin grpc and gen task (#429)

--- python/packages/autogen-core/src/autogen_core/application/protos/agent_worker_pb2_grpc.py
+++ python/packages/autogen-core/src/autogen_core/application/protos/agent_worker_pb2_grpc.py
@@ -5,7 +5,7 @@ import warnings
 
 import agent_worker_pb2 as agent__worker__pb2
 
-GRPC_GENERATED_VERSION = '1.66.0'
+GRPC_GENERATED_VERSION = '1.66.1'
 GRPC_VERSION = grpc.__version__
 _version_not_supported = False
 
