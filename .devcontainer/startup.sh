2d826189c2690a89a64ac92c9bb55c3ca0270b30
Ryan Sweet <rysweet@microsoft.com>
2024-09-17 05:26:23 -0700

    make python dev setup in codespaces (#526)
    * add setup steps to codespace startup

--- .devcontainer/startup.sh
+++ .devcontainer/startup.sh
@@ -1,1 +1,12 @@
 #!/bin/bash
+
+# dotnet setup
+dotnet workload update
+
+# python setup
+pushd python
+pip install uv
+uv sync
+source .venv/bin/activate
+echo "export PATH=$PATH" >> ~/.bashrc
+popd
