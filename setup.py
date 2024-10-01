3a6b88ef8ae5a362d95c92187f8093180c6c2d59
Mark Sze <66362098+marklysze@users.noreply.github.com>
2024-10-02 08:06:52 +1000

    Ollama Client (with tool calling) (#3056)
    * Ollama client! With function calling. Initial commit, client, no docs or tests yet.
    
    * Tidy comments
    
    * Cater for missing prompt token count
    
    * Removed use of eval, added json parsing support library
    
    * Fix to the use of the JSON fix library, handling of Mixtral escape sequence
    
    * Fixed 'name' in JSON bug, catered for single function call JSON without []
    
    * removing role='tool' from inner tool result to reduce token usage.
    
    * Added Ollama documentation and updated library versions
    
    * Added Native Ollama tool calling (v0.3.0 req.) as well as hide/show tools support
    
    * Added native tool calling and hide_tools parameter to documentation
    
    * Update to Ollama 0.3.1, added tests
    
    * Tweak to manual function calling prompt to improve number handling.
    
    * Fix formatting
    
    Co-authored-by: gagb <gagb@users.noreply.github.com>
    Co-authored-by: Jack Gerrits <jackgerrits@users.noreply.github.com>
    
    * Fix formatting
    
    * Better error message
    
    ---------
    
    Co-authored-by: Eric Zhu <ekzhu@users.noreply.github.com>
    Co-authored-by: gagb <gagb@users.noreply.github.com>
    Co-authored-by: Jack Gerrits <jackgerrits@users.noreply.github.com>

--- setup.py
+++ setup.py
@@ -105,6 +105,7 @@ extra_require = {
     "mistral": ["mistralai>=1.0.1"],
     "groq": ["groq>=0.9.0"],
     "cohere": ["cohere>=5.5.8"],
+    "ollama": ["ollama>=0.3.1", "fix_busted_json>=0.0.18"],
     "bedrock": ["boto3>=1.34.149"],
 }
 
