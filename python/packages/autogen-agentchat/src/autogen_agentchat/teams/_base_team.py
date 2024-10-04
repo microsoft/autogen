ec4a01f8c1e10b2a0e4e7f35b743e572e8d89c0e
Jack Gerrits <jack@jackgerrits.com>
2024-10-04 09:53:45 -0400

    lint

--- python/packages/autogen-agentchat/src/autogen_agentchat/teams/_base_team.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/teams/_base_team.py
@@ -1,11 +1,10 @@
-from dataclasses import dataclass
 import logging
+from dataclasses import dataclass
 from typing import List, Protocol
 
 from autogen_agentchat.agents._base_chat_agent import ChatMessage
-from autogen_core.application.logging import EVENT_LOGGER_NAME
 
-from ..logging import ConsoleLogHandler, EVENT_LOGGER_NAME
+from ..logging import EVENT_LOGGER_NAME, ConsoleLogHandler
 
 logger = logging.getLogger(EVENT_LOGGER_NAME)
 logger.setLevel(logging.INFO)
--- python/packages/autogen-agentchat/src/autogen_agentchat/teams/group_chat/_round_robin_group_chat.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/teams/group_chat/_round_robin_group_chat.py
@@ -1,13 +1,13 @@
 import uuid
 from typing import Callable, List
 
-from autogen_agentchat.agents._base_chat_agent import ChatMessage
 from autogen_core.application import SingleThreadedAgentRuntime
 from autogen_core.base import AgentId, AgentInstantiationContext, AgentRuntime, AgentType, MessageContext, TopicId
 from autogen_core.components import ClosureAgent, TypeSubscription
 from autogen_core.components.tool_agent import ToolAgent
 from autogen_core.components.tools import Tool
 
+from autogen_agentchat.agents._base_chat_agent import ChatMessage
 
 from ...agents import BaseChatAgent, TextMessage
 from .._base_team import BaseTeam, TeamRunResult
