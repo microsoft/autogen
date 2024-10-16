80d514193c6eb1e8174e53d1273ca96196826723
Hussein Mozannar <hmozannar@microsoft.com>
2024-10-15 21:52:18 -0700

    added example code for magentic-one + code comments

--- python/packages/autogen-magentic-one/src/autogen_magentic_one/agents/base_orchestrator.py
+++ python/packages/autogen-magentic-one/src/autogen_magentic_one/agents/base_orchestrator.py
@@ -12,6 +12,7 @@ from .base_agent import MagenticOneBaseAgent
 
 
 class BaseOrchestrator(MagenticOneBaseAgent):
+    """Base class for orchestrator that manage a group of agents."""
     def __init__(
         self,
         agents: List[AgentProxy],
