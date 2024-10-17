06d3164e698c85521081500a610e8095a608dea4
Ryan Sweet <rysweet@microsoft.com>
2024-10-17 16:31:19 -0700

    not quite there yet but making progress.

--- dotnet/src/Microsoft.AutoGen/Agents/Agents/AIAgent/InferenceAgent.cs
+++ dotnet/src/Microsoft.AutoGen/Agents/Agents/AIAgent/InferenceAgent.cs
@@ -1,15 +1,32 @@
-
+using Microsoft.Extensions.AI;
 namespace Microsoft.AutoGen.Agents.Client;
 public abstract class InferenceAgent<T> : AgentBase where T : class, new()
 {
     protected AgentState<T> _state;
-
+    protected IChatClient ChatClient { get; }
     public InferenceAgent(
         IAgentContext context,
-        EventTypes typeRegistry
+        EventTypes typeRegistry, IChatClient client
         ) : base(context, typeRegistry)
     {
+        ChatClient = client;
         _state = new();
+    }
+
+    private Task<ChatCompletion> CompleteAsync(
+        IList<ChatMessage> chatMessages, 
+        ChatOptions? options = null, 
+        CancellationToken cancellationToken = default)
+    {
+        return ChatClient.CompleteAsync(chatMessages, options, cancellationToken);
+    }
+
+    private IAsyncEnumerable<StreamingChatCompletionUpdate> CompleteStreamingAsync(
+        IList<ChatMessage> chatMessages,
+        ChatOptions? options = null,
+        CancellationToken cancellationToken = default)
+    {
+        return ChatClient.CompleteStreamingAsync(chatMessages, options, cancellationToken);
     }
 
 }
