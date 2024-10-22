96752b8bbf8ff935bf11ef0a10863a6a8bbc2e2f
Ryan Sweet <rysweet@microsoft.com>
2024-10-21 22:47:40 -0700

    make the fiy config more obvious

--- dotnet/samples/HelloAIAgents/Program.cs
+++ dotnet/samples/HelloAIAgents/Program.cs
@@ -8,7 +8,10 @@ var builder = AgentsApp.CreateBuilder();
 // put these in your environment or appsettings.json
 builder.Configuration["HelloAIAgents:ModelType"] = "azureopenai";
 builder.Configuration["HelloAIAgents:LlmModelName"] = "gpt-3.5-turbo";
-// set AZURE_OPENAI_CONNECTION_STRING = "Endpoint=https://TODO.openai.azure.com/;Key=TODO;Deployment=TODO"
+if (Environment.GetEnvironmentVariable("AZURE_OPENAI_CONNECTION_STRING") == null)
+{
+    throw new InvalidOperationException("AZURE_OPENAI_CONNECTION_STRING not set, try something like AZURE_OPENAI_CONNECTION_STRING = \"Endpoint=https://TODO.openai.azure.com/;Key=TODO;Deployment=TODO\"");
+}
 builder.Configuration["ConectionStrings:HelloAIAgents"] = Environment.GetEnvironmentVariable("AZURE_OPENAI_CONNECTION_STRING");
 builder.AddChatCompletionService("HelloAIAgents");
 var app = await AgentsApp.PublishMessageAsync("HelloAgents", new NewMessageReceived
