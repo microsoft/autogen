839b7c0994662577420ce7070c781d41d0fc9894
Ryan Sweet <rysweet@microsoft.com>
2024-10-23 08:57:12 -0700

    update agent class name.

--- dotnet/samples/Hello/HelloAIAgents/Program.cs
+++ dotnet/samples/Hello/HelloAIAgents/Program.cs
@@ -30,7 +30,7 @@ await app.WaitForShutdownAsync();
 namespace Hello
 {
     [TopicSubscription("HelloAgents")]
-    public class HelloAgent(
+    public class HelloAIAgent(
         IAgentContext context,
         [FromKeyedServices("EventTypes")] EventTypes typeRegistry) : ConsoleAgent(
             context,
