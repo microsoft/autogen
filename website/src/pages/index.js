faa8c7ed14470353fb81af65fe8abb9575a510dd
Shaokun Zhang <shaokun.zhang@psu.edu>
2024-10-23 18:48:19 -0400

    update style (#82)

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -14,7 +14,7 @@ function HomepageHeader() {
   return (
     <header className={clsx('hero hero--primary', styles.heroBanner)}>
       <div className="container">
-        <img src={useBaseUrl("img/ag.svg")} width="180em" />
+        <img src={useBaseUrl("img/ag.svg")} width="160em" />
         <h1 className="hero__title">{siteConfig.title}</h1>
         <p className="hero__subtitle">{siteConfig.tagline}</p>
         <div className={styles.buttons}>
