bd820a0f3db445a01544c1926c865358380648b2
Ryan Sweet <rysweet@microsoft.com>
2024-10-25 21:54:46 -0700

    etags for optimistic concurrency locking - not finished?

--- dotnet/src/Microsoft.AutoGen/Runtime/WorkerAgentGrain.cs
+++ dotnet/src/Microsoft.AutoGen/Runtime/WorkerAgentGrain.cs
@@ -6,10 +6,18 @@ internal sealed class WorkerAgentGrain([PersistentState("state", "AgentStateStor
 {
     public async ValueTask<string> WriteStateAsync(AgentState newState, string eTag)
     {
-        if (string.Equals(state.Etag, eTag, StringComparison.Ordinal))
+        // if the Etag is null, its a new state
+        // if the Etag is not null, it should match the current state's Etag
+        if ((string.IsNullOrEmpty(state.Etag)) || (string.Equals(state.Etag, eTag, StringComparison.Ordinal)))
         {
             state.State = newState;
             await state.WriteStateAsync();
+        }
+        else
+        {
+            //TODO - this is probably not the correct behavior to just throw - I presume we want to somehow let the caller know that the state has changed and they need to re-read it
+            throw new ArgumentException(
+                "The provided ETag does not match the current ETag. The state has been modified by another request.");
         }
 
         return state.Etag;
