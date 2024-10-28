d197ec14c83018660b37550df46d31a888e38fac
Eric Zhu <ekzhu@users.noreply.github.com>
2024-10-28 16:58:43 -0700

    type

--- python/packages/autogen-core/src/autogen_core/application/_worker_runtime_host_servicer.py
+++ python/packages/autogen-core/src/autogen_core/application/_worker_runtime_host_servicer.py
@@ -247,7 +247,7 @@ class WorkerAgentRuntimeHostServicer(agent_worker_pb2_grpc.AgentRpcServicer):
     async def GetState(  # type: ignore
         self,
         request: agent_worker_pb2.AgentId,
-        context: grpc.aio.ServicerContext[agent_worker_pb2.AgentId, agent_worker_pb2.AgentState],
+        context: grpc.aio.ServicerContext[agent_worker_pb2.AgentId, agent_worker_pb2.GetStateResponse],
     ) -> agent_worker_pb2.GetStateResponse:  # type: ignore
         raise NotImplementedError("Method not implemented!")
 
