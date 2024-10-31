6a05677f708315652754091d6d1bcca00e739ba4
Ryan Sweet <rysweet@microsoft.com>
2024-10-30 21:15:04 -0700

    fix builds

--- dotnet/samples/Hello/Backend/Program.cs
+++ dotnet/samples/Hello/Backend/Program.cs
@@ -3,5 +3,5 @@
 
 using Microsoft.Extensions.Hosting;
 
-var app = await Microsoft.AutoGen.Runtime.Host.StartAsync(local: true);
+var app = await Microsoft.AutoGen.Agents.Host.StartAsync(local: true);
 await app.WaitForShutdownAsync();
