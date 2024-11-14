fc44ae28f0768d207045a1db920c1dd02f849e3f
Hussein Mozannar <hmozannar@microsoft.com>
2024-11-13 20:06:53 -0800

    lazy init set

--- python/packages/autogen-ext/src/autogen_ext/agents/web_surfer/_multimodal_web_surfer.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/web_surfer/_multimodal_web_surfer.py
@@ -224,6 +224,7 @@ class MultimodalWebSurfer(BaseChatAgent):
             )
         )
 
+
     async def _lazy_init(
         self,
     ) -> None:
@@ -265,6 +266,7 @@ class MultimodalWebSurfer(BaseChatAgent):
 
         # Prepare the debug directory -- which stores the screenshots generated throughout the process
         await self._set_debug_dir(self.debug_dir)
+        self.did_lazy_init = True
 
     async def _set_debug_dir(self, debug_dir: str | None) -> None:
         assert self._page is not None
