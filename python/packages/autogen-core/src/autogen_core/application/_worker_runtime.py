9450bd7d966647f6e5be394a0055bbfaa18d0b8c
Ryan Sweet <rysweet@microsoft.com>
2024-11-16 10:37:50 -0800

    send cloud event message type

--- python/packages/autogen-core/src/autogen_core/application/_worker_runtime.py
+++ python/packages/autogen-core/src/autogen_core/application/_worker_runtime.py
@@ -52,7 +52,7 @@ from ..base import (
 )
 from ..components import TypeSubscription
 from ._helpers import SubscriptionManager, get_impl
-from .protos import agent_worker_pb2, agent_worker_pb2_grpc
+from .protos import agent_worker_pb2, agent_worker_pb2_grpc, cloudevent_pb2, cloudevent_pb2_grpc
 from .telemetry import MessageRuntimeTracingConfig, TraceHelper, get_telemetry_grpc_metadata
 
 if TYPE_CHECKING:
@@ -380,16 +380,12 @@ class WorkerAgentRuntime(AgentRuntime):
             )
             telemetry_metadata = get_telemetry_grpc_metadata()
             runtime_message = agent_worker_pb2.Message(
-                event=agent_worker_pb2.Event(
-                    topic_type=topic_id.type,
-                    topic_source=topic_id.source,
+                cloudEvent=cloudevent_pb2.CloudEvent(
+                    spec_version="1.0",
+                    type=topic_id.type,
                     source=agent_worker_pb2.AgentId(type=sender.type, key=sender.key) if sender is not None else None,
                     metadata=telemetry_metadata,
-                    payload=agent_worker_pb2.Payload(
-                        data_type=message_type,
-                        data=serialized_message,
-                        data_content_type=JSON_DATA_CONTENT_TYPE,
-                    ),
+                    text_data=serialized_message,
                 )
             )
 
