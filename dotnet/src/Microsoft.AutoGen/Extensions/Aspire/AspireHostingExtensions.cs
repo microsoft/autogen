09cdc9f388e0b96bb59562e3163ae179c59a1be4
Ryan Sweet <rysweet@microsoft.com>
2024-11-20 10:38:00 -0800

    more renaming and adding tags

--- dotnet/src/Microsoft.AutoGen/Extensions/Aspire/AspireHostingExtensions.cs
+++ dotnet/src/Microsoft.AutoGen/Extensions/Aspire/AspireHostingExtensions.cs
@@ -15,7 +15,7 @@ namespace Microsoft.Extensions.Hosting;
 // Adds common .NET Aspire services: service discovery, resilience, health checks, and OpenTelemetry.
 // This project should be referenced by each service project in your solution.
 // To learn more about using this project, see https://aka.ms/dotnet/aspire/service-defaults
-public static class Extensions
+public static class AspireHostingExtensions
 {
     public static IHostApplicationBuilder AddServiceDefaults(this IHostApplicationBuilder builder)
     {
