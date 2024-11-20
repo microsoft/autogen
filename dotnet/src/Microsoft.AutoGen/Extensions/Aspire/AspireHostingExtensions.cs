4c109bc28a5110976a0fa1e4bc17df22bfdf93e0
Ryan Sweet <rysweet@microsoft.com>
2024-11-20 11:12:28 -0800

    fix header

--- dotnet/src/Microsoft.AutoGen/Extensions/Aspire/AspireHostingExtensions.cs
+++ dotnet/src/Microsoft.AutoGen/Extensions/Aspire/AspireHostingExtensions.cs
@@ -1,5 +1,5 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
-// Extensions.cs
+// AspireHostingExtensions.cs
 
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Diagnostics.HealthChecks;
