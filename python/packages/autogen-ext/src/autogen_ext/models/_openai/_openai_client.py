ef81ed08d16808d0fddc010fe87da5ef58e26ed6
Mohammad Mazraeh <mazraeh.mohammad@gmail.com>
2024-11-21 23:47:55 -0800

    add tolerance for empty choices
    Signed-off-by: Mohammad Mazraeh <mazraeh.mohammad@gmail.com>

--- python/packages/autogen-ext/src/autogen_ext/models/_openai/_openai_client.py
+++ python/packages/autogen-ext/src/autogen_ext/models/_openai/_openai_client.py
@@ -556,6 +556,7 @@ class BaseOpenAIChatCompletionClient(ChatCompletionClient):
         json_output: Optional[bool] = None,
         extra_create_args: Mapping[str, Any] = {},
         cancellation_token: Optional[CancellationToken] = None,
+        max_empty_consequative_chunk_tolerance: Optional[int] = 0,
     ) -> AsyncGenerator[Union[str, CreateResult], None]:
         """
         Creates an AsyncGenerator that will yield a  stream of chat completions based on the provided messages and tools.
@@ -566,6 +567,7 @@ class BaseOpenAIChatCompletionClient(ChatCompletionClient):
             json_output (Optional[bool], optional): If True, the output will be in JSON format. Defaults to None.
             extra_create_args (Mapping[str, Any], optional): Additional arguments for the creation process. Default to `{}`.
             cancellation_token (Optional[CancellationToken], optional): A token to cancel the operation. Defaults to None.
+            max_empty_consequative_chunk_tolerance (int, optional): The maximum number of consecutive empty chunks to tolerate before stopping the stream. Defaults to 3.
 
         Yields:
             AsyncGenerator[Union[str, CreateResult], None]: A generator yielding the completion results as they are produced.
@@ -636,12 +638,28 @@ class BaseOpenAIChatCompletionClient(ChatCompletionClient):
         full_tool_calls: Dict[int, FunctionCall] = {}
         completion_tokens = 0
         logprobs: Optional[List[ChatCompletionTokenLogprob]] = None
+        empty_chunk_count = 0
+
         while True:
             try:
                 chunk_future = asyncio.ensure_future(anext(stream))
                 if cancellation_token is not None:
                     cancellation_token.link_future(chunk_future)
                 chunk = await chunk_future
+
+                # This is to address a bug in AzureOpenAIChatCompletionClient. OpenAIChatCompletionClient works fine.
+                #  https://github.com/microsoft/autogen/issues/4213
+                if len(chunk.choices) == 0:
+                    empty_chunk_count += 1
+                    if max_empty_consequative_chunk_tolerance is None or max_empty_consequative_chunk_tolerance == 0:
+                        raise ValueError(
+                            "max_empty_consequative_chunk_tolerance is set to 0, increasing it might help!"
+                        )
+                    elif empty_chunk_count >= max_empty_consequative_chunk_tolerance:
+                        raise ValueError("Exceeded the threshold of receiving consecutive empty chunks")
+                    continue
+                else:
+                    empty_chunk_count = 0
 
                 # to process usage chunk in streaming situations
                 # add    stream_options={"include_usage": True} in the initialization of OpenAIChatCompletionClient(...)
