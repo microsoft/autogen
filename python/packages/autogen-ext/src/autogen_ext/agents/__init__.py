3ffd56eefeefe9d09a229886af284af66f41a303
Adam Fourney <adamfo@microsoft.com>
2024-11-25 14:01:42 -0800

    Initial commit of FileSurfer in AgentChat/ext. WIP

--- python/packages/autogen-ext/src/autogen_ext/agents/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/__init__.py
@@ -1,4 +1,5 @@
 from ._openai_assistant_agent import OpenAIAssistantAgent
+from .file_surfer._file_surfer import FileSurfer
 from .web_surfer._multimodal_web_surfer import MultimodalWebSurfer
 
-__all__ = ["MultimodalWebSurfer", "OpenAIAssistantAgent"]
+__all__ = ["MultimodalWebSurfer", "OpenAIAssistantAgent", "FileSurfer"]
