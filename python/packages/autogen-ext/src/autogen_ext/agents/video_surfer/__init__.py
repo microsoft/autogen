27011ddf7178901ba53024dc281ff64f62dd7bdb
gagb <gagb@users.noreply.github.com>
2024-11-27 23:27:09 -0800

    Expose the tools

--- python/packages/autogen-ext/src/autogen_ext/agents/video_surfer/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/video_surfer/__init__.py
@@ -1,3 +1,4 @@
 from ._video_surfer import VideoSurferAgent
+from ._action_space import extract_audio, transcribe_audio_with_timestamps, get_video_length, save_screenshot, openai_transcribe_video_screenshot, get_screenshot_at
 
-__all__ = ["VideoSurferAgent"]
+__all__ = ["VideoSurferAgent", "extract_audio", "transcribe_audio_with_timestamps", "get_video_length", "save_screenshot", "openai_transcribe_video_screenshot", "get_screenshot_at"]
