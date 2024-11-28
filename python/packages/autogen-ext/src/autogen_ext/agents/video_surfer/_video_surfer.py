9c37e48e3a469446e1a6f75bdf970c2e967c31b0
gagb <gagb@users.noreply.github.com>
2024-11-27 22:17:12 -0800

    Refine action space

--- python/packages/autogen-ext/src/autogen_ext/agents/video_surfer/_video_surfer.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/video_surfer/_video_surfer.py
@@ -1,6 +1,6 @@
 from autogen_agentchat.agents import AssistantAgent
 
-from ._action_space import extract_audio, get_video_length, transcribe_audio_with_timestamps, get_screenshot_at, save_screenshot, openai_transcribe_video_screenshot, download_youtube_video
+from ._action_space import extract_audio, get_video_length, transcribe_audio_with_timestamps, get_screenshot_at, save_screenshot, openai_transcribe_video_screenshot
 
 
 class VideoSurferAgent(AssistantAgent):
@@ -14,16 +14,16 @@ class VideoSurferAgent(AssistantAgent):
                             openai_transcribe_video_screenshot,
                             extract_audio,
                             transcribe_audio_with_timestamps,
-                            download_youtube_video
                         ],
                         system_message = """
 You are a helpful agent that is an expert at answering questions from a video.
     
 When asked to answer a question about a video, you should:
-1. Use the transcription to find which part of the video the question is referring to.
-2. Optionally use screenshots from those timestamps
-3. Provide a detailed answer to the question.
+1. Check if that video is available locally.
+2. Use the transcription to find which part of the video the question is referring to.
+3. Optionally use screenshots from those timestamps
+4. Provide a detailed answer to the question.
 Reply with TERMINATE when the task has been completed.
 """,
-description="An agent that can answer questions about a video.",
+description="An agent that can answer questions about a local video.",
         )
