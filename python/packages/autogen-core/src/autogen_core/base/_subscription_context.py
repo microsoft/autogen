68b31006f51dcb34bafd087eacdb58101b8f20f5
Jack Gerrits <jackgerrits@users.noreply.github.com>
2024-12-02 17:36:00 -0500

    Core doc edits, mostly visibility of private methods (#4457)

--- python/packages/autogen-core/src/autogen_core/base/_subscription_context.py
+++ python/packages/autogen-core/src/autogen_core/base/_subscription_context.py
@@ -11,21 +11,22 @@ class SubscriptionInstantiationContext:
             "SubscriptionInstantiationContext cannot be instantiated. It is a static class that provides context management for subscription instantiation."
         )
 
-    SUBSCRIPTION_CONTEXT_VAR: ClassVar[ContextVar[AgentType]] = ContextVar("SUBSCRIPTION_CONTEXT_VAR")
+    _SUBSCRIPTION_CONTEXT_VAR: ClassVar[ContextVar[AgentType]] = ContextVar("_SUBSCRIPTION_CONTEXT_VAR")
 
     @classmethod
     @contextmanager
     def populate_context(cls, ctx: AgentType) -> Generator[None, Any, None]:
-        token = SubscriptionInstantiationContext.SUBSCRIPTION_CONTEXT_VAR.set(ctx)
+        """:meta private:"""
+        token = SubscriptionInstantiationContext._SUBSCRIPTION_CONTEXT_VAR.set(ctx)
         try:
             yield
         finally:
-            SubscriptionInstantiationContext.SUBSCRIPTION_CONTEXT_VAR.reset(token)
+            SubscriptionInstantiationContext._SUBSCRIPTION_CONTEXT_VAR.reset(token)
 
     @classmethod
     def agent_type(cls) -> AgentType:
         try:
-            return cls.SUBSCRIPTION_CONTEXT_VAR.get()
+            return cls._SUBSCRIPTION_CONTEXT_VAR.get()
         except LookupError as e:
             raise RuntimeError(
                 "SubscriptionInstantiationContext.runtime() must be called within an instantiation context such as when the AgentRuntime is instantiating an agent. Mostly likely this was caused by directly instantiating an agent instead of using the AgentRuntime to do so."
