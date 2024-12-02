962fcb0a3a6499147b41eabacad3fbb6cc7b504c
Peter Chang <petchang@microsoft.com>
2024-12-02 15:35:57 -0500

    Fix bugs

--- python/packages/autogen-agentchat/src/autogen_agentchat/agents/_user_proxy_agent.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/agents/_user_proxy_agent.py
@@ -14,12 +14,13 @@ SyncInputFunc = Callable[[str], str]
 AsyncInputFunc = Callable[[str, Optional[CancellationToken]], Awaitable[str]]
 InputFuncType = Union[SyncInputFunc, AsyncInputFunc]
 
-
 # TODO: ainput doesn't seem to play nicely with jupyter.
+#       No input window appears in this case.
 async def cancellable_input(prompt: str, cancellation_token: Optional[CancellationToken]) -> Awaitable[str]:
     task = asyncio.create_task(ainput(prompt))
-    cancellation_token.link_future(task)
-    return await task
+    if cancellation_token is not None:
+        cancellation_token.link_future(task)
+    return str(await task)
 
 
 class UserProxyAgent(BaseChatAgent):
