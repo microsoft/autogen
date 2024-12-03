f7457a66a8fc532cd297697cdc682b982ce1e4a5
Adam Fourney <adamfo@microsoft.com>
2024-12-03 15:05:49 -0800

    Added the coder used by MagenticOne.

--- python/packages/autogen-ext/src/autogen_ext/agents/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/__init__.py
@@ -1,3 +1,4 @@
 from ._openai_assistant_agent import OpenAIAssistantAgent
+from ._magentic_one_coder_agent import MagenticOneCoderAgent
 
-__all__ = ["OpenAIAssistantAgent"]
+__all__ = ["OpenAIAssistantAgent", "MagenticOneCoderAgent"]
