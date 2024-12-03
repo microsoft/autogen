f7c7e3a2e70c985d19847adaaf736db5dd7c66d8
Kosta Petan <kostapetan@gmail.com>
2024-12-03 14:18:51 +0100

    refactor paths, add test projects for Grpc runtime and client

---
+++ dotnet/test/Microsoft.AutoGen.Core.Grpc.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// UnitTest1.cs
+
+namespace Microsoft.AutoGen.Core.Grpc.Tests;
+
+public class UnitTest1
+{
+    [Fact]
+    public void Test1()
+    {
+
+    }
+}
---
+++ dotnet/test/Microsoft.AutoGen.Runtime.Grpc.Tests/Class1.cs
@@ -0,0 +1,9 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Class1.cs
+
+namespace Microsoft.AutoGen.Runtime.Grpc.Tests;
+
+public class Class1
+{
+
+}
