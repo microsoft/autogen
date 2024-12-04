31cb50bce935ccc50bd0207d0c0fa72117b33efc
afourney <adamfo@microsoft.com>
2024-12-03 16:08:18 -0800

    Added the coder used by MagenticOne. (#4511)
    * Added the coder used by MagenticOne.
    
    * Fixed formatting.
    
    ---------
    
    Co-authored-by: gagb <gagb@users.noreply.github.com>

--- python/packages/autogen-ext/src/autogen_ext/agents/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/__init__.py
@@ -1,3 +1,4 @@
+from ._magentic_one_coder_agent import MagenticOneCoderAgent
 from ._openai_assistant_agent import OpenAIAssistantAgent
 
-__all__ = ["OpenAIAssistantAgent"]
+__all__ = ["OpenAIAssistantAgent", "MagenticOneCoderAgent"]
