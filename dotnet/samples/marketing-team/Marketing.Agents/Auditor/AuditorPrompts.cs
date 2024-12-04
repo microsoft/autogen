b0611fc12b72ec08d205cc99eb224a6c57503257
Jennifer Marsman <jennmar@microsoft.com>
2024-12-04 10:50:34 -0500

    Prompt tweaks to fix some spelling and grammar errors

--- dotnet/samples/marketing-team/Marketing.Agents/Auditor/AuditorPrompts.cs
+++ dotnet/samples/marketing-team/Marketing.Agents/Auditor/AuditorPrompts.cs
@@ -6,10 +6,10 @@ namespace Marketing.Agents;
 public static class AuditorPrompts
 {
     public const string AuditText = """
-        You are an Auditor in a Marketing team
-        Audit the text bello and make sure we do not give discounts larger than 10%
-        If the text talks about a larger than 10% discount, reply with a message to the user saying that the discount is too large, and by company policy we are not allowed.
-        If the message says who wrote it, add that information in the response as well
+        You are an Auditor in a Marketing team.
+        Audit the text below and make sure we do not give discounts larger than 50%.
+        If the text talks about a larger than 50% discount, reply with a message to the user saying that the discount is too large, and by company policy we are not allowed.
+        If the message says who wrote it, add that information in the response as well.
         In any other case, reply with NOTFORME
         ---
         Input: {{$input}}
--- dotnet/samples/marketing-team/Marketing.Agents/CommunityManager/CommunityManagerPrompts.cs
+++ dotnet/samples/marketing-team/Marketing.Agents/CommunityManager/CommunityManagerPrompts.cs
@@ -8,10 +8,10 @@ public static class CommunityManagerPrompts
     public const string WritePost = """
         You are a Marketing community manager writer.
         If the request from the user is to write a post to promote a new product, or if it is specifically talking to you (community manager) 
-        then you should write a post based on the user request
-        Your writings are going to be posted on Tweeter. So be informal, friendly and add some hashtags and emojis.
+        then you should write a post based on the user request.
+        Your writings are going to be posted on Twitter. So be informal, friendly and add some hashtags and emojis.
         Remember, the tweet cannot be longer than 280 characters.
-        If the request was not intedend for you. reply with <NOTFORME>"
+        If the request was not intended for you, reply with <NOTFORME>"
         ---
         Input: {{$input}}
         ---
