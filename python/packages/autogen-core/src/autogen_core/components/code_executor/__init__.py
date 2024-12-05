e615059345f48f5aaaf34be62904c68451bc98b9
Jack Gerrits <jackgerrits@users.noreply.github.com>
2024-12-04 17:13:43 -0800

    Remove extract_markdown_code_blocks from core API (#4554)

--- python/packages/autogen-core/src/autogen_core/components/code_executor/__init__.py
+++ python/packages/autogen-core/src/autogen_core/components/code_executor/__init__.py
@@ -12,7 +12,6 @@ from ._func_with_reqs import (
 from ._impl.command_line_code_result import CommandLineCodeResult
 from ._impl.local_commandline_code_executor import LocalCommandLineCodeExecutor
 from ._impl.utils import get_file_name_from_content, get_required_packages, lang_to_cmd, silence_pip
-from ._utils import extract_markdown_code_blocks
 
 __all__ = [
     "LocalCommandLineCodeExecutor",
@@ -27,7 +26,6 @@ __all__ = [
     "FunctionWithRequirementsStr",
     "with_requirements",
     "to_stub",
-    "extract_markdown_code_blocks",
     "get_required_packages",
     "build_python_functions_file",
     "get_required_packages",
