1fa132e7c36ed7c121019462da29cd9512068255
Eric Zhu <ekzhu@users.noreply.github.com>
2024-12-06 01:59:28 -0800

    Update references in docs (#4590)
    * Update agent doc
    
    * Remove outdated doc
    
    * Update references
    
    * Update readme
    
    * Update readme

--- python/packages/autogen-magentic-one/examples/example_coder.py
+++ python/packages/autogen-magentic-one/examples/example_coder.py
@@ -7,8 +7,7 @@ round-robin orchestrator agent. The code snippets are executed inside a docker c
 import asyncio
 import logging
 
-from autogen_core import AgentId, AgentProxy, SingleThreadedAgentRuntime
-from autogen_core.application.logging import EVENT_LOGGER_NAME
+from autogen_core import EVENT_LOGGER_NAME, AgentId, AgentProxy, SingleThreadedAgentRuntime
 from autogen_core.code_executor import CodeBlock
 from autogen_ext.code_executors.docker import DockerCommandLineCodeExecutor
 from autogen_magentic_one.agents.coder import Coder, Executor
