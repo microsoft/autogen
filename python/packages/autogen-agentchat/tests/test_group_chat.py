9cc9a4efe553a601a3473a3388397f30abffad47
Eric Zhu <ekzhu@users.noreply.github.com>
2024-12-09 14:43:08 -0800

    fix pyright

--- python/packages/autogen-agentchat/tests/test_group_chat.py
+++ python/packages/autogen-agentchat/tests/test_group_chat.py
@@ -34,7 +34,6 @@ from autogen_agentchat.teams._group_chat._swarm_group_chat import SwarmGroupChat
 from autogen_agentchat.ui import Console
 from autogen_core import AgentId, CancellationToken
 from autogen_core.components.tools import FunctionTool
-from autogen_core.models import FunctionExecutionResult
 from autogen_ext.code_executors.local import LocalCommandLineCodeExecutor
 from autogen_ext.models import OpenAIChatCompletionClient, ReplayChatCompletionClient
 from openai.resources.chat.completions import AsyncCompletions
