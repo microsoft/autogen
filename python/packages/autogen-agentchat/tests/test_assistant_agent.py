ef1dc34a2823cf91ceae5c5f5d1384eeb44eacf2
Hussein Mozannar <hmozannar@microsoft.com>
2024-12-09 19:03:31 -0800

    Fix AssistantAgent Tool Call Behavior (#4602)
    * 1 tool call iteration default
    
    * handoff first
    
    * return_only_response
    
    * add and remove tools
    
    * print out tool calls
    
    * pass checks
    
    * fix issues
    
    * add test
    
    * add unit tests
    
    * remove extra print
    
    * Update python/packages/autogen-agentchat/src/autogen_agentchat/agents/_assistant_agent.py
    
    Co-authored-by: Eric Zhu <ekzhu@users.noreply.github.com>
    
    * documentation and none max_tools_calls
    
    * Always limit # tool call to 1
    
    * Update notebooks for the changing behavior of assistant agent.
    
    * Merge branch 'main' into assistant_Agent_tools
    
    * add reflect_on_tool_use parameter to format the tool call result
    
    * wip
    
    * wip
    
    * fix pyright
    
    * Add unit tests
    
    * Merge remote-tracking branch 'origin/main' into assistant_Agent_tools
    
    * Update with custom formatting of tool call summary
    
    * format
    
    * Merge branch 'main' into assistant_Agent_tools

--- python/packages/autogen-agentchat/tests/test_assistant_agent.py
+++ python/packages/autogen-agentchat/tests/test_assistant_agent.py
@@ -118,6 +118,7 @@ async def test_run_with_tools(monkeypatch: pytest.MonkeyPatch) -> None:
         tools=[_pass_function, _fail_function, FunctionTool(_echo_function, description="Echo")],
     )
     result = await agent.run(task="task")
+
     assert len(result.messages) == 4
     assert isinstance(result.messages[0], TextMessage)
     assert result.messages[0].models_usage is None
@@ -128,6 +129,106 @@ async def test_run_with_tools(monkeypatch: pytest.MonkeyPatch) -> None:
     assert isinstance(result.messages[2], ToolCallResultMessage)
     assert result.messages[2].models_usage is None
     assert isinstance(result.messages[3], TextMessage)
+    assert result.messages[3].content == "pass"
+    assert result.messages[3].models_usage is None
+
+    # Test streaming.
+    mock._curr_index = 0  # pyright: ignore
+    index = 0
+    async for message in agent.run_stream(task="task"):
+        if isinstance(message, TaskResult):
+            assert message == result
+        else:
+            assert message == result.messages[index]
+        index += 1
+
+    # Test state saving and loading.
+    state = await agent.save_state()
+    agent2 = AssistantAgent(
+        "tool_use_agent",
+        model_client=OpenAIChatCompletionClient(model=model, api_key=""),
+        tools=[_pass_function, _fail_function, FunctionTool(_echo_function, description="Echo")],
+    )
+    await agent2.load_state(state)
+    state2 = await agent2.save_state()
+    assert state == state2
+
+
+@pytest.mark.asyncio
+async def test_run_with_tools_and_reflection(monkeypatch: pytest.MonkeyPatch) -> None:
+    model = "gpt-4o-2024-05-13"
+    chat_completions = [
+        ChatCompletion(
+            id="id1",
+            choices=[
+                Choice(
+                    finish_reason="tool_calls",
+                    index=0,
+                    message=ChatCompletionMessage(
+                        content=None,
+                        tool_calls=[
+                            ChatCompletionMessageToolCall(
+                                id="1",
+                                type="function",
+                                function=Function(
+                                    name="_pass_function",
+                                    arguments=json.dumps({"input": "task"}),
+                                ),
+                            )
+                        ],
+                        role="assistant",
+                    ),
+                )
+            ],
+            created=0,
+            model=model,
+            object="chat.completion",
+            usage=CompletionUsage(prompt_tokens=10, completion_tokens=5, total_tokens=0),
+        ),
+        ChatCompletion(
+            id="id2",
+            choices=[
+                Choice(finish_reason="stop", index=0, message=ChatCompletionMessage(content="Hello", role="assistant"))
+            ],
+            created=0,
+            model=model,
+            object="chat.completion",
+            usage=CompletionUsage(prompt_tokens=10, completion_tokens=5, total_tokens=0),
+        ),
+        ChatCompletion(
+            id="id2",
+            choices=[
+                Choice(
+                    finish_reason="stop", index=0, message=ChatCompletionMessage(content="TERMINATE", role="assistant")
+                )
+            ],
+            created=0,
+            model=model,
+            object="chat.completion",
+            usage=CompletionUsage(prompt_tokens=10, completion_tokens=5, total_tokens=0),
+        ),
+    ]
+    mock = _MockChatCompletion(chat_completions)
+    monkeypatch.setattr(AsyncCompletions, "create", mock.mock_create)
+    agent = AssistantAgent(
+        "tool_use_agent",
+        model_client=OpenAIChatCompletionClient(model=model, api_key=""),
+        tools=[_pass_function, _fail_function, FunctionTool(_echo_function, description="Echo")],
+        reflect_on_tool_use=True,
+    )
+    result = await agent.run(task="task")
+
+    assert len(result.messages) == 4
+    assert isinstance(result.messages[0], TextMessage)
+    assert result.messages[0].models_usage is None
+    assert isinstance(result.messages[1], ToolCallMessage)
+    assert result.messages[1].models_usage is not None
+    assert result.messages[1].models_usage.completion_tokens == 5
+    assert result.messages[1].models_usage.prompt_tokens == 10
+    assert isinstance(result.messages[2], ToolCallResultMessage)
+    assert result.messages[2].models_usage is None
+    assert isinstance(result.messages[3], TextMessage)
+    assert result.messages[3].content == "Hello"
     assert result.messages[3].models_usage is not None
     assert result.messages[3].models_usage.completion_tokens == 5
     assert result.messages[3].models_usage.prompt_tokens == 10
