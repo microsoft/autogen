e90b9aa92678d7e7cc418510d3c981cbe4622241
milkmeat <milkmeat@gmail.com>
2024-12-12 09:33:57 +0800

    fix "keep_first_message" to make sure messages are in correct order, (#4653)
    described in https://github.com/microsoft/autogen/pull/3178#issuecomment-2530740564
    
    Co-authored-by: Eric Zhu <ekzhu@users.noreply.github.com>

--- autogen/agentchat/contrib/capabilities/transforms.py
+++ autogen/agentchat/contrib/capabilities/transforms.py
@@ -96,7 +96,7 @@ class MessageHistoryLimiter:
             if remaining_count == 1:
                 # If there's only 1 slot left and it's a 'tools' message, ignore it.
                 if messages[i].get("role") != "tool":
-                    truncated_messages.insert(1, messages[i])
+                    truncated_messages.insert(1 if self._keep_first_message else 0, messages[i])
 
             remaining_count -= 1
             if remaining_count == 0:
