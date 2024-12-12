c5a7b819718d4f982b281a64df537a9c6fc9a47d
Ryan Sweet <rysweet@microsoft.com>
2024-12-11 22:45:01 -0800

    move IagentWorker

--- dotnet/src/Microsoft.AutoGen/Agents/IAgentWorker.cs
+++ dotnet/src/Microsoft.AutoGen/Agents/IAgentWorker.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // IAgentWorker.cs
-namespace Microsoft.AutoGen.Abstractions;
+using Microsoft.AutoGen.Abstractions;
+namespace Microsoft.AutoGen.Agents;
 
 public interface IAgentWorker
 {
