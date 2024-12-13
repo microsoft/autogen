2989783e7ea61eb610e604b9d854a0451a4dd0d3
Ryan Sweet <rysweet@microsoft.com>
2024-12-12 20:44:05 -0800

    merge fixup

--- dotnet/src/Microsoft.AutoGen/Contracts/IHandle.cs
+++ dotnet/src/Microsoft.AutoGen/Contracts/IHandle.cs
@@ -1,12 +1,6 @@
 // Copyright (c) Microsoft Corporation. All rights reserved.
 // IHandle.cs
-
-<<<<<<<< HEAD:dotnet/src/Microsoft.AutoGen/Agents/IHandle.cs
-namespace Microsoft.AutoGen.Agents;
-========
 namespace Microsoft.AutoGen.Contracts;
->>>>>>>> main:dotnet/src/Microsoft.AutoGen/Contracts/IHandle.cs
-
 public interface IHandle
 {
     Task HandleObject(object item);
