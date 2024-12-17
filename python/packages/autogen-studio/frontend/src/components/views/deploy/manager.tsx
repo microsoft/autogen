1d18c5d99a35007f414b683ef0cdf035ae8f5422
Victor Dibia <victor.dibia@gmail.com>
2024-12-17 11:07:57 -0800

    add basic structure for deploy

---
+++ python/packages/autogen-studio/frontend/src/components/views/deploy/manager.tsx
@@ -0,0 +1,7 @@
+import React, { useState } from "react";
+
+export const DeployManager: React.FC = () => {
+  return <div className="relative flex h-full w-full">{/* Sidebar */}</div>;
+};
+
+export default DeployManager;
