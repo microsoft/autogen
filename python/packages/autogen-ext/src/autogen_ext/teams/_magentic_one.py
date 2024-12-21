04bab6aee7d9d336b51bb354aab4271da8413e4c
gagb <gagb@users.noreply.github.com>
2024-12-20 22:11:21 -0800

    Add CodeExecutorAgent to MagenticOne for enhanced task execution

--- python/packages/autogen-ext/src/autogen_ext/teams/_magentic_one.py
+++ python/packages/autogen-ext/src/autogen_ext/teams/_magentic_one.py
@@ -1,10 +1,12 @@
 from autogen_agentchat.teams import MagenticOneGroupChat
+from autogen_agentchat.agents import CodeExecutorAgent
 from autogen_agentchat.ui import Console
 
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 from autogen_ext.agents.file_surfer import FileSurfer
 from autogen_ext.agents.web_surfer import MultimodalWebSurfer
 from autogen_ext.agents.magentic_one import MagenticOneCoderAgent
+from autogen_ext.code_executors.local import LocalCommandLineCodeExecutor
 
 
 class MagenticOne:
@@ -13,7 +15,8 @@ class MagenticOne:
         self.fs = FileSurfer("FileSurfer", model_client=client)
         self.ws = MultimodalWebSurfer("WebSurfer", model_client=client)
         self.coder = MagenticOneCoderAgent("Coder", model_client=client)
-        self.team = MagenticOneGroupChat([self.fs, self.ws, self.coder], model_client=client)
+        self.executor = CodeExecutorAgent("Executor", code_executor=LocalCommandLineCodeExecutor())
+        self.team = MagenticOneGroupChat([self.fs, self.ws, self.coder, self.executor], model_client=client)
 
     async def run(self, task: str):
         return await Console(self.team.run_stream(task=task))
