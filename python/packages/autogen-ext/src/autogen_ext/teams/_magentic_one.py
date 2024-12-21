ed1fbd2cd5a76502e105a4e11219354d829e9676
gagb <gagb@users.noreply.github.com>
2024-12-20 22:26:21 -0800

    Refactor MagenticOne class to inherit from MagenticOneGroupChat and streamline initialization

--- python/packages/autogen-ext/src/autogen_ext/teams/_magentic_one.py
+++ python/packages/autogen-ext/src/autogen_ext/teams/_magentic_one.py
@@ -9,14 +9,11 @@ from autogen_ext.agents.magentic_one import MagenticOneCoderAgent
 from autogen_ext.code_executors.local import LocalCommandLineCodeExecutor
 
 
-class MagenticOne:
+class MagenticOne(MagenticOneGroupChat):
     def __init__(self, client: OpenAIChatCompletionClient):
         self.client = client
-        self.fs = FileSurfer("FileSurfer", model_client=client)
-        self.ws = MultimodalWebSurfer("WebSurfer", model_client=client)
-        self.coder = MagenticOneCoderAgent("Coder", model_client=client)
-        self.executor = CodeExecutorAgent("Executor", code_executor=LocalCommandLineCodeExecutor())
-        self.team = MagenticOneGroupChat([self.fs, self.ws, self.coder, self.executor], model_client=client)
-
-    async def run(self, task: str):
-        return await Console(self.team.run_stream(task=task))
+        fs = FileSurfer("FileSurfer", model_client=client)
+        ws = MultimodalWebSurfer("WebSurfer", model_client=client)
+        coder = MagenticOneCoderAgent("Coder", model_client=client)
+        executor = CodeExecutorAgent("Executor", code_executor=LocalCommandLineCodeExecutor())
+        super().__init__([fs, ws, coder, executor], model_client=client)
