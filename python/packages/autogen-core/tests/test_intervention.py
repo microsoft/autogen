8447511b25cff3130bee9517ce7f6c9fe6a3742c
Jack Gerrits <jack@jackgerrits.com>
2025-01-06 11:43:31 -0500

    Fix pyright

--- python/packages/autogen-core/tests/test_intervention.py
+++ python/packages/autogen-core/tests/test_intervention.py
@@ -1,10 +1,15 @@
 from typing import Any
 
 import pytest
-from autogen_core import AgentId, DefaultInterventionHandler, DropMessage, MessageContext, SingleThreadedAgentRuntime
-from autogen_core._default_subscription import DefaultSubscription
-from autogen_core._default_topic import DefaultTopicId
-from autogen_core._type_subscription import TypeSubscription
+from autogen_core import (
+    AgentId,
+    DefaultInterventionHandler,
+    DefaultSubscription,
+    DefaultTopicId,
+    DropMessage,
+    MessageContext,
+    SingleThreadedAgentRuntime,
+)
 from autogen_core.exceptions import MessageDroppedException
 from autogen_test_utils import LoopbackAgent, MessageType
 
@@ -13,20 +18,20 @@ from autogen_test_utils import LoopbackAgent, MessageType
 async def test_intervention_count_messages() -> None:
     class DebugInterventionHandler(DefaultInterventionHandler):
         def __init__(self) -> None:
-            self._num_send_messages = 0
-            self._num_publish_messages = 0
-            self._num_response_messages = 0
+            self.num_send_messages = 0
+            self.num_publish_messages = 0
+            self.num_response_messages = 0
 
         async def on_send(self, message: Any, *, message_context: MessageContext, recipient: AgentId) -> Any:
-            self._num_send_messages += 1
+            self.num_send_messages += 1
             return message
 
         async def on_publish(self, message: Any, *, message_context: MessageContext) -> Any:
-            self._num_publish_messages += 1
+            self.num_publish_messages += 1
             return message
 
         async def on_response(self, message: Any, *, sender: AgentId, recipient: AgentId | None) -> Any:
-            self._num_response_messages += 1
+            self.num_response_messages += 1
             return message
 
     handler = DebugInterventionHandler()
@@ -39,8 +44,8 @@ async def test_intervention_count_messages() -> None:
 
     await runtime.stop_when_idle()
 
-    assert handler._num_send_messages == 1
-    assert handler._num_response_messages == 1
+    assert handler.num_send_messages == 1
+    assert handler.num_response_messages == 1
     loopback_agent = await runtime.try_get_underlying_agent_instance(loopback, type=LoopbackAgent)
     assert loopback_agent.num_calls == 1
 
@@ -51,7 +56,7 @@ async def test_intervention_count_messages() -> None:
 
     await runtime.stop_when_idle()
     assert loopback_agent.num_calls == 2
-    assert handler._num_publish_messages == 1
+    assert handler.num_publish_messages == 1
 
 
 @pytest.mark.asyncio
