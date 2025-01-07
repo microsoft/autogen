2e1d24f90b766a60db8d253654d206f62f5c7eb5
Ryan Sweet <rysweet@microsoft.com>
2025-01-07 11:31:38 -0800

    rename some wierd methods

--- dotnet/test/Microsoft.AutoGen.Core.Tests/AgentTests.cs
+++ dotnet/test/Microsoft.AutoGen.Core.Tests/AgentTests.cs
@@ -143,7 +143,7 @@ public class AgentTests(InMemoryAgentRuntimeFixture fixture)
     }
 
     [Fact]
-    public async Task ItInvokeRightHandlerTestAsync()
+    public async Task InvokeCorrectHandler()
     {
         var agent = new TestAgent(new AgentsMetadata(TypeRegistry.Empty, new Dictionary<string, Type>(), new Dictionary<Type, HashSet<string>>(), new Dictionary<Type, HashSet<string>>()), new Logger<Agent>(new LoggerFactory()));
 
@@ -156,23 +156,23 @@ public class AgentTests(InMemoryAgentRuntimeFixture fixture)
     }
 
     [Fact]
-    public async Task ItDelegateMessageToTestAgentAsync()
+    public async Task DelegateMessageToTestAgentAsync()
     {
         var client = _fixture.AppHost.Services.GetRequiredService<Client>();
         await client.PublishMessageAsync(new TextMessage()
         {
-            Source = nameof(ItDelegateMessageToTestAgentAsync),
+            Source = nameof(DelegateMessageToTestAgentAsync),
             TextMessage_ = "buffer"
         }, token: CancellationToken.None);
 
         // wait for 10 seconds
         var cts = new CancellationTokenSource(TimeSpan.FromSeconds(10));
-        while (!TestAgent.ReceivedMessages.ContainsKey(nameof(ItDelegateMessageToTestAgentAsync)) && !cts.Token.IsCancellationRequested)
+        while (!TestAgent.ReceivedMessages.ContainsKey(nameof(DelegateMessageToTestAgentAsync)) && !cts.Token.IsCancellationRequested)
         {
             await Task.Delay(100);
         }
 
-        TestAgent.ReceivedMessages[nameof(ItDelegateMessageToTestAgentAsync)].Should().NotBeNull();
+        TestAgent.ReceivedMessages[nameof(DelegateMessageToTestAgentAsync)].Should().NotBeNull();
     }
 
     /// <summary>
