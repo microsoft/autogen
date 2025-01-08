973c8b63302df5b13ddbb83628d7cabd09a960fa
Jack Gerrits <jackgerrits@users.noreply.github.com>
2025-01-08 08:56:08 -0500

    Remove deprecated items for release (#4927)

--- python/packages/autogen-agentchat/src/autogen_agentchat/agents/__init__.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/agents/__init__.py
@@ -3,21 +3,16 @@ This module initializes various pre-defined agents provided by the package.
 BaseChatAgent is the base class for all agents in AgentChat.
 """
 
-from ._assistant_agent import AssistantAgent, Handoff  # type: ignore
+from ._assistant_agent import AssistantAgent
 from ._base_chat_agent import BaseChatAgent
 from ._code_executor_agent import CodeExecutorAgent
-from ._coding_assistant_agent import CodingAssistantAgent
 from ._society_of_mind_agent import SocietyOfMindAgent
-from ._tool_use_assistant_agent import ToolUseAssistantAgent
 from ._user_proxy_agent import UserProxyAgent
 
 __all__ = [
     "BaseChatAgent",
     "AssistantAgent",
-    "Handoff",
     "CodeExecutorAgent",
-    "CodingAssistantAgent",
-    "ToolUseAssistantAgent",
     "SocietyOfMindAgent",
     "UserProxyAgent",
 ]
