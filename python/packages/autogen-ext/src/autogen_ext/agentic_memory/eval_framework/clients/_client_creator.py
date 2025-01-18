be081b387ca959e1ebd8d9fb55cd7d0f42d5c61e
Ricky Loynd <riloynd@microsoft.com>
2025-01-18 12:51:06 -0800

    simplification of client creation code

--- python/packages/autogen-ext/src/autogen_ext/agentic_memory/eval_framework/clients/_client_creator.py
+++ python/packages/autogen-ext/src/autogen_ext/agentic_memory/eval_framework/clients/_client_creator.py
@@ -10,16 +10,33 @@ class ClientCreator:
         self.page_log = page_log
 
     def create_client(self):
+        # A few args are shared by all clients.
+        args = {}
+        args["model"] = self.settings["model"]
+        args["max_completion_tokens"] = self.settings["max_completion_tokens"]
+        args["max_retries"] = self.settings["max_retries"]
+
+        # The following args don't apply to the 'o1' family of models.
+        if not args["model"].startswith("o1"):
+            args["temperature"] = self.settings["temperature"]
+            args["presence_penalty"] = self.settings["presence_penalty"]
+            args["frequency_penalty"] = self.settings["frequency_penalty"]
+            args["top_p"] = self.settings["top_p"]
+
         client = None
         provider = self.settings["provider"]
         if provider == "openai":
-            client = self.create_oai_client()
+            client, source = self.create_oai_client(args)
         elif provider == "azure_openai":
-            client = self.create_aoai_client()
+            client, source = self.create_aoai_client(args)
         elif provider == "trapi":
-            client = self.create_trapi_client()
+            client, source = self.create_trapi_client(args)
         else:
             assert False, "Invalid client provider"
+
+        # Log some details.
+        self.page_log.append_entry_line("Client:  {}".format(client._resolved_model))
+        self.page_log.append_entry_line(source)
 
         # Check if the client should be wrapped.
         if "ClientWrapper" in self.settings:
@@ -28,38 +45,18 @@ class ClientCreator:
                 # Wrap the client.
                 client = ClientWrapper(
                     client, wrapper_settings["mode"], wrapper_settings["session_name"], self.page_log)
-
         return client
 
-    def add_shared_args(self, args):
-        args["model"] = self.settings["model"]
-        args["max_completion_tokens"] = self.settings["max_completion_tokens"]
-        args["max_retries"] = self.settings["max_retries"]
-
-    def add_family_args(self, args):
-        # Does the model name start with 'o1'?
-        if not args["model"].startswith("o1"):
-            # No. A few more things can be specified.
-            args["temperature"] = self.settings["temperature"]
-            args["presence_penalty"] = self.settings["presence_penalty"]
-            args["frequency_penalty"] = self.settings["frequency_penalty"]
-            args["top_p"] = self.settings["top_p"]
-
-    def create_oai_client(self):
+    def create_oai_client(self, args):
         # Create an OpenAI client
-        args = {"api_key": self.settings["api_key"]}
-        self.add_shared_args(args)
-        self.add_family_args(args)
-
+        args["api_key"] = self.settings["api_key"]
         client = OpenAIChatCompletionClient(**args)
-
-        self.page_log.append_entry_line("Client:  {}".format(client._resolved_model))
-        self.page_log.append_entry_line("  created through OpenAI")
-        return client
+        return client, "  created through OpenAI"
 
 
-    def create_aoai_client(self):
+    def create_aoai_client(self, args):
         # Create an Azure OpenAI client
+        token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
         model = self.settings["model"]
         if model == "gpt-4o-2024-08-06":
             azure_deployment = 'gpt-4o-2024-08-06-eval'  # This is DeploymentName in the table at https://aka.ms/trapi/models
@@ -72,28 +69,16 @@ class ClientCreator:
             azure_endpoint = "https://agentic1.openai.azure.com/"
         else:
             assert False, "Unsupported model"
-
-        token_provider = get_bearer_token_provider(DefaultAzureCredential(),
-                                                   "https://cognitiveservices.azure.com/.default")
         api_version = '2024-12-01-preview'  # From https://learn.microsoft.com/en-us/azure/ai-services/openai/api-version-deprecation#latest-ga-api-release
-
-        args = {
-            "azure_ad_token_provider": token_provider,
-            "azure_deployment": azure_deployment,
-            "azure_endpoint": azure_endpoint,
-            "api_version": api_version,
-        }
-        self.add_shared_args(args)
-        self.add_family_args(args)
-
+        args["azure_ad_token_provider"] = token_provider
+        args["azure_deployment"] = azure_deployment
+        args["azure_endpoint"] = azure_endpoint
+        args["api_version"] = api_version
         client = AzureOpenAIChatCompletionClient(**args)
-
-        self.page_log.append_entry_line("Client:  {}".format(client._resolved_model))
-        self.page_log.append_entry_line("  created through Azure OpenAI")
-        return client
+        return client, "  created through Azure OpenAI"
 
 
-    def create_trapi_client(self):
+    def create_trapi_client(self, args):
         # Create an Azure OpenAI client through TRAPI
         token_provider = get_bearer_token_provider(ChainedTokenCredential(
             AzureCliCredential(),
@@ -109,7 +94,6 @@ class ClientCreator:
                 # managed_identity_client_id=os.environ.get("DEFAULT_IDENTITY_CLIENT_ID"),  # See the TRAPI docs
             )
         ), "api://trapi/.default")
-        api_version = '2024-12-01-preview'  # From https://learn.microsoft.com/en-us/azure/ai-services/openai/api-version-deprecation#latest-ga-api-release
         model = self.settings["model"]
         if model == "gpt-4o-2024-08-06":
             azure_deployment = 'gpt-4o_2024-08-06'  # This is DeploymentName in the table at https://aka.ms/trapi/models
@@ -123,19 +107,10 @@ class ClientCreator:
             assert False, "Unsupported model"
         trapi_suffix = 'msraif/shared'  # This is TRAPISuffix (without /openai) in the table at https://aka.ms/trapi/models
         endpoint = f'https://trapi.research.microsoft.com/{trapi_suffix}'
-
-        args = {
-            "azure_ad_token_provider": token_provider,
-            "azure_deployment": azure_deployment,
-            "azure_endpoint": endpoint,
-            "api_version": api_version,
-        }
-        self.add_shared_args(args)
-        self.add_family_args(args)
-
+        api_version = '2024-12-01-preview'  # From https://learn.microsoft.com/en-us/azure/ai-services/openai/api-version-deprecation#latest-ga-api-release
+        args["azure_ad_token_provider"] = token_provider
+        args["azure_deployment"] = azure_deployment
+        args["azure_endpoint"] = endpoint
+        args["api_version"] = api_version
         client = AzureOpenAIChatCompletionClient(**args)
-
-        self.page_log.append_entry_line("Client:  {}".format(client._resolved_model))
-        self.page_log.append_entry_line("  created through TRAPI")
-        return client
-
+        return client, "  created through TRAPI"
