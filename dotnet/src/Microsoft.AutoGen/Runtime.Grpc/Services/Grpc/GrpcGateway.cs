702cc132c005a31c4d013c17f3f519e2dd258deb
Ryan Sweet <rysweet@microsoft.com>
2025-01-21 15:09:04 -0800

    ensure its initialized

--- dotnet/src/Microsoft.AutoGen/Runtime.Grpc/Services/Grpc/GrpcGateway.cs
+++ dotnet/src/Microsoft.AutoGen/Runtime.Grpc/Services/Grpc/GrpcGateway.cs
@@ -215,7 +215,8 @@ public sealed class GrpcGateway : BackgroundService, IGateway
 
         var registry = _clusterClient.GetGrain<IRegistryGrain>(0);
         //intentionally blocking
-        var targetAgentTypes = await registry.GetSubscribedAndHandlingAgents(evt.Source, evt.Type).ConfigureAwait(true);
+        var targetAgentTypes = new List<string>();
+        targetAgentTypes = await registry.GetSubscribedAndHandlingAgents(evt.Source, evt.Type).ConfigureAwait(true);
         // alternate path    
         // get the event type and then send to all agents that are subscribed to that event type
         var eventType = evt.Type;
