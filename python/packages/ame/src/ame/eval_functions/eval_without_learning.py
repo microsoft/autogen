9b3f77da3d2f181ee33a17932834b5f81859b233
Ricky Loynd <riloynd@microsoft.com>
2025-01-22 16:13:31 -0800

    merge from main

--- python/packages/ame/src/ame/eval_functions/eval_without_learning.py
+++ python/packages/ame/src/ame/eval_functions/eval_without_learning.py
@@ -16,6 +16,6 @@ async def eval_without_learning(fast_learner, evaluator, client, logger, setting
         fast_learner=fast_learner, task_description=task_description, expected_answer=expected_answer,
         num_trials=num_trials, use_memory=True, client=client, logger=logger)
     success_rate = round((num_successes / num_trials) * 100)
-    logger.info("\nSuccess rate:  {}%\n".format(success_rate), flush=True)
+    logger.info("\nSuccess rate:  {}%\n".format(success_rate))
 
     logger.leave_function()
