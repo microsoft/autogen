0d45ddc75893624dfeb9efce37c281254f12217f
Mohammad Mazraeh <mazraeh.mohammad@gmail.com>
2025-01-26 14:06:25 -0800

    update empty chunk tolerance param in create_streams
    Signed-off-by: Mohammad Mazraeh <mazraeh.mohammad@gmail.com>

--- python/packages/autogen-core/src/autogen_core/models/_model_client.py
+++ python/packages/autogen-core/src/autogen_core/models/_model_client.py
@@ -74,6 +74,7 @@ class ChatCompletionClient(ComponentBase[BaseModel], ABC):
         json_output: Optional[bool] = None,
         extra_create_args: Mapping[str, Any] = {},
         cancellation_token: Optional[CancellationToken] = None,
+        max_consecutive_empty_chunk_tolerance: int = 0,
     ) -> AsyncGenerator[Union[str, CreateResult], None]: ...
 
     @abstractmethod
--- python/packages/autogen-ext/src/autogen_ext/models/cache/_chat_completion_cache.py
+++ python/packages/autogen-ext/src/autogen_ext/models/cache/_chat_completion_cache.py
@@ -120,6 +120,7 @@ class ChatCompletionCache(ChatCompletionClient):
         json_output: Optional[bool] = None,
         extra_create_args: Mapping[str, Any] = {},
         cancellation_token: Optional[CancellationToken] = None,
+        max_consecutive_empty_chunk_tolerance: int = 0,
     ) -> CreateResult:
         """
         Cached version of ChatCompletionClient.create.
@@ -152,6 +153,7 @@ class ChatCompletionCache(ChatCompletionClient):
         json_output: Optional[bool] = None,
         extra_create_args: Mapping[str, Any] = {},
         cancellation_token: Optional[CancellationToken] = None,
+        max_consecutive_empty_chunk_tolerance: int = 0,
     ) -> AsyncGenerator[Union[str, CreateResult], None]:
         """
         Cached version of ChatCompletionClient.create_stream.
