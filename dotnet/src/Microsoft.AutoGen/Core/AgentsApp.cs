248f3e166b9d436aa22af87ebb5ed703c0f2caa9
Jack Gerrits <jack@jackgerrits.com>
2025-01-27 15:57:01 -0500

    fix namespace

--- dotnet/src/Microsoft.AutoGen/Core/AgentsApp.cs
+++ dotnet/src/Microsoft.AutoGen/Core/AgentsApp.cs
@@ -5,7 +5,7 @@ using Microsoft.AutoGen.Contracts;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
-namespace Microsoft.AutoGen.CoreEquiv;
+namespace Microsoft.AutoGen.Core;
 
 public class AgentsAppBuilder
 {
