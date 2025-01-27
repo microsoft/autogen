60b06583ebb375b23f0e376954d2af514e72eacb
Jacob Alber <jaalber@microsoft.com>
2025-01-27 13:33:52 -0500

    fix: Get InProcessRuntime building
    * Sanitize APIs more

--- dotnet/src/Microsoft.AutoGen/Contracts/PythonEquiv/IUnboundSubscriptionDefinition.cs
+++ dotnet/src/Microsoft.AutoGen/Contracts/PythonEquiv/IUnboundSubscriptionDefinition.cs
@@ -1,6 +1,5 @@
-﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Copyright (c) Microsoft Corporation. All rights reserved.
 // IUnboundSubscriptionDefinition.cs
-
 
 namespace Microsoft.AutoGen.Contracts.Python;
 
