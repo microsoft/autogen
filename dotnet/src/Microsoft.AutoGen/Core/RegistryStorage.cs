61ae013e19a686c659b4f1b73183619be2d6e4a7
Ryan Sweet <rysweet@microsoft.com>
2025-01-27 00:05:03 -0800

    read async

--- dotnet/src/Microsoft.AutoGen/Core/RegistryStorage.cs
+++ dotnet/src/Microsoft.AutoGen/Core/RegistryStorage.cs
@@ -21,8 +21,7 @@ public class RegistryStorage(ILogger<IRegistryStorage> logger) : IRegistryStorag
     {
         string json;
         var state = new AgentsRegistryState();
-        if (File.Exists(FilePath)) { json = File.ReadAllText(FilePath); }
-        else
+        if (File.Exists(FilePath)) { json = await File.ReadAllTextAsync(FilePath, cancellationToken).ConfigureAwait(false); }        else
         {
             _logger.LogWarning("Registry file {FilePath} does not exist, starting with an empty Registry", FilePath);
             await WriteStateAsync(state, cancellationToken, true).ConfigureAwait(false);
