cf4c65cd1994ccf06ba7dd99fdf85acae7401e07
Griffin Bassman <griffinbassman@gmail.com>
2025-01-27 12:40:08 -0500

    fix comment and agentProxy -- build succeeds

--- dotnet/src/Microsoft.AutoGen/Contracts/PythonEquiv/IAgentRuntime.cs
+++ dotnet/src/Microsoft.AutoGen/Contracts/PythonEquiv/IAgentRuntime.cs
@@ -15,7 +15,7 @@ public interface IAgentRuntime : ISaveState<IAgentRuntime>
     public ValueTask<AgentId> GetAgentAsync(AgentType agentType, string key = "default", bool lazy = true/*, CancellationToken? = default*/);
     public ValueTask<AgentId> GetAgentAsync(string agent, string key = "default", bool lazy = true/*, CancellationToken? = default*/);
 
-    public ValueTask<StateDict> SaveAgentStateAsync(AgentId agentId, /*CancellationToken? cancellationToken = default*/);
+    public ValueTask<StateDict> SaveAgentStateAsync(AgentId agentId/*, CancellationToken? cancellationToken = default*/);
     public ValueTask LoadAgentStateAsync(AgentId agentId, StateDict state/*, CancellationToken? cancellationToken = default*/);
 
     public ValueTask<AgentMetadata> GetAgentMetadataAsync(AgentId agentId/*, CancellationToken? cancellationToken = default*/);
