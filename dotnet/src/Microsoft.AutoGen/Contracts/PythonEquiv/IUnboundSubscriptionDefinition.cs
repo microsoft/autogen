b0d268fae23bb9231ca6b5fbb14b109869e2098b
Jack Gerrits <jack@jackgerrits.com>
2025-01-27 12:25:04 -0500

    Add IUnboundSubscriptionDefinition

---
+++ dotnet/src/Microsoft.AutoGen/Contracts/PythonEquiv/IUnboundSubscriptionDefinition.cs
@@ -0,0 +1,10 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// IUnboundSubscriptionDefinition.cs
+
+
+namespace Microsoft.AutoGen.Contracts.Python;
+
+public interface IUnboundSubscriptionDefinition
+{
+    public ISubscriptionDefinition Bind(AgentType agentType);
+}
