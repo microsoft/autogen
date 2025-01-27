13ca3476608254334e79fb886fbef822452191f1
Jack Gerrits <jack@jackgerrits.com>
2025-01-27 11:59:57 -0500

    add python equiv in core

---
+++ dotnet/src/Microsoft.AutoGen/Core/PythonEquiv/IHandle.cs
@@ -0,0 +1,30 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// IHandle.cs
+
+using Google.Protobuf;
+
+namespace Microsoft.AutoGen.Core.Python;
+
+/// <summary>
+/// Defines a handler interface for processing items of type <typeparamref name="T"/>.
+/// </summary>
+/// <typeparam name="T">The type of item to be handled, which must implement <see cref="IMessage"/>.</typeparam>
+public interface IHandle<in T> where T : IMessage
+{
+    /// <summary>
+    /// Handles the specified item asynchronously.
+    /// </summary>
+    /// <param name="item">The item to be handled.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task Handle(T item, CancellationToken cancellationToken = default);
+}
+
+public interface IHandle<in InT, OutT> where InT : IMessage where OutT : IMessage
+{
+    /// <summary>
+    /// Handles the specified item asynchronously.
+    /// </summary>
+    /// <param name="item">The item to be handled.</param>
+    /// <returns>A task that represents the asynchronous operation.</returns>
+    Task<OutT> Handle(InT item, CancellationToken cancellationToken = default);
+}
