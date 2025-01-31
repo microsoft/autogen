8dc600b07f48d8d32b98ec790c77fd0867edb555
Eric Zhu <ekzhu@users.noreply.github.com>
2025-01-31 11:51:49 -0800

    sample: Update chainlit sample to separate agent and team examples; add streaming output; refactor to better use the chainlit API

---
+++ python/samples/agentchat_chainlit/app_agent.py
@@ -0,0 +1,66 @@
+import chainlit as cl
+import yaml
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.base import Response
+from autogen_agentchat.messages import ModelClientStreamingChunkEvent, TextMessage
+from autogen_core import CancellationToken
+from autogen_core.models import ChatCompletionClient
+
+
+@cl.set_starters
+async def set_starts() -> list[cl.Starter]:
+    return [
+        cl.Starter(
+            label="Greetings",
+            message="Hello! What can you help me with today?",
+        ),
+        cl.Starter(
+            label="Weather",
+            message="Find the weather in New York City.",
+        ),
+    ]
+
+
+@cl.step(type="tool")
+async def get_weather(city: str) -> str:
+    return f"The weather in {city} is 73 degrees and Sunny."
+
+
+@cl.on_chat_start  # type: ignore
+async def start_chat():
+    # Load model configuration and create the model client.
+    with open("model_config.yaml", "r") as f:
+        model_config = yaml.safe_load(f)
+    model_client = ChatCompletionClient.load_component(model_config)
+
+    # Create the assistant agent with the get_weather tool.
+    assistant = AssistantAgent(
+        name="assistant",
+        tools=[get_weather],
+        model_client=model_client,
+        system_message="You are a helpful assistant",
+        model_client_stream=True,  # Enable model client streaming.
+        reflect_on_tool_use=True,  # Reflect on tool use.
+    )
+
+    # Set the assistant agent in the user session.
+    cl.user_session.set("prompt_history", "")  # type: ignore
+    cl.user_session.set("agent", assistant)  # type: ignore
+
+
+@cl.on_message  # type: ignore
+async def chat(message: cl.Message):
+    # Get the assistant agent from the user session.
+    agent = cl.user_session.get("agent")  # type: ignore
+    # Construct the response message.
+    response = cl.Message(content="")
+    async for msg in agent.on_messages_stream(
+        messages=[TextMessage(content=message.content, source="user")],
+        cancellation_token=CancellationToken(),
+    ):
+        if isinstance(msg, ModelClientStreamingChunkEvent):
+            # Stream the model client response to the user.
+            await response.stream_token(msg.content)
+        elif isinstance(msg, Response):
+            # Done streaming the model client response. Send the message.
+            await response.send()
