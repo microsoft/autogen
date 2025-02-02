127b3198e67814b5773ab9d88f7ad5b1d06af870
Richard Gyiko <gyiko.richard@outlook.com>
2025-02-02 21:23:09 +0100

    Prevent exposing the config classes from the module

--- python/packages/autogen-ext/src/autogen_ext/tools/mcp/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/tools/mcp/__init__.py
@@ -1,14 +1,12 @@
 from ._config import McpServerParams, SseServerParams, StdioServerParams
 from ._factory import mcp_server_tools
-from ._sse import SseMcpToolAdapter, SseMcpToolAdapterConfig
-from ._stdio import StdioMcpToolAdapter, StdioMcpToolAdapterConfig
+from ._sse import SseMcpToolAdapter
+from ._stdio import StdioMcpToolAdapter
 
 __all__ = [
     "StdioMcpToolAdapter",
-    "StdioMcpToolAdapterConfig",
     "StdioServerParams",
     "SseMcpToolAdapter",
-    "SseMcpToolAdapterConfig",
     "SseServerParams",
     "McpServerParams",
     "mcp_server_tools",
