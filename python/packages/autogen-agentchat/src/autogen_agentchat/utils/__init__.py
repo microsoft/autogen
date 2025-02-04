cf6fa7727371a64d74058203becf29afa602f12b
afourney <adamfo@microsoft.com>
2025-02-04 08:25:48 -0800

    Add text-only model support to M1 (#5344)
    Modify M1 agents to support text-only settings.
    This allows M1 to be used with models like o3-mini and Llama3.1+

--- python/packages/autogen-agentchat/src/autogen_agentchat/utils/__init__.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/utils/__init__.py
@@ -2,6 +2,6 @@
 This module implements various utilities common to AgentChat agents and teams.
 """
 
-from ._utils import remove_images
+from ._utils import content_to_str, remove_images
 
-__all__ = ["remove_images"]
+__all__ = ["content_to_str", "remove_images"]
