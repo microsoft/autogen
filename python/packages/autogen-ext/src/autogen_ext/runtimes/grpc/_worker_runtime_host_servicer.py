28cd56230a8deab6a91420b55ce417bb50d8c32d
Jack Gerrits <jack@jackgerrits.com>
2025-02-06 11:18:28 -0500

    undo removal

--- python/packages/autogen-ext/src/autogen_ext/runtimes/grpc/_worker_runtime_host_servicer.py
+++ python/packages/autogen-ext/src/autogen_ext/runtimes/grpc/_worker_runtime_host_servicer.py
@@ -288,3 +288,17 @@ class GrpcWorkerAgentRuntimeHostServicer(agent_worker_pb2_grpc.AgentRpcServicer)
     ) -> agent_worker_pb2.GetSubscriptionsResponse:
         _client_id = await get_client_id_or_abort(context)
         raise NotImplementedError("Method not implemented.")
+
+    async def GetState(  # type: ignore
+        self,
+        request: agent_worker_pb2.AgentId,
+        context: grpc.aio.ServicerContext[agent_worker_pb2.AgentId, agent_worker_pb2.GetStateResponse],
+    ) -> agent_worker_pb2.GetStateResponse:
+        raise NotImplementedError("Method not implemented!")
+
+    async def SaveState(  # type: ignore
+        self,
+        request: agent_worker_pb2.AgentState,
+        context: grpc.aio.ServicerContext[agent_worker_pb2.AgentId, agent_worker_pb2.SaveStateResponse],
+    ) -> agent_worker_pb2.SaveStateResponse:
+        raise NotImplementedError("Method not implemented!")
