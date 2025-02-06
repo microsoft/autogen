4fd3b17aa7ee98145213f7b6f4e4ffdab9475a62
Adam Fourney <adamfo@microsoft.com>
2025-02-05 16:58:53 -0800

    Fixed summarization.

--- python/packages/autogen-ext/src/autogen_ext/agents/web_surfer/_multimodal_web_surfer.py
+++ python/packages/autogen-ext/src/autogen_ext/agents/web_surfer/_multimodal_web_surfer.py
@@ -865,19 +865,21 @@ class MultimodalWebSurfer(BaseChatAgent, Component[MultimodalWebSurferConfig]):
         buffer = ""
         # for line in re.split(r"([\r\n]+)", page_markdown):
         for line in page_markdown.splitlines():
-            message = UserMessage(
-                # content=[
-                content=prompt + buffer + line,
-                #    ag_image,
-                # ],
-                source=self.name,
-            )
+            trial_message = UserMessage( content=prompt + buffer + line, source=self.name,)
 
-            remaining = self._model_client.remaining_tokens(messages + [message])
-            if remaining > self.SCREENSHOT_TOKENS:
-                buffer += line
-            else:
+            try:
+                remaining = self._model_client.remaining_tokens(messages + [trial_message])
+            except KeyError:
+                # Default to 128k if the model isn't found
+                remaining = 128000 - self._model_client.count_tokens(messages + [trial_message])
+
+            if self._model_client.model_info["vision"] and remaining <= 0:
                 break
+
+            if self._model_client.model_info["vision"] and remaining <= self.SCREENSHOT_TOKENS:
+                break
+
+            buffer += line
 
         # Nothing to do
         buffer = buffer.strip()
@@ -885,15 +887,20 @@ class MultimodalWebSurfer(BaseChatAgent, Component[MultimodalWebSurferConfig]):
             return "Nothing to summarize."
 
         # Append the message
-        messages.append(
-            UserMessage(
-                content=[
-                    prompt + buffer,
-                    ag_image,
-                ],
-                source=self.name,
+        if self._model_client.model_info["vision"]:
+            # Multimodal
+            messages.append(
+                UserMessage(
+                    content=[
+                        prompt + buffer,
+                        ag_image,
+                    ],
+                    source=self.name,
+                )
             )
-        )
+        else:
+            # Text only
+            messages.append(UserMessage( content=prompt + buffer, source=self.name,))
 
         # Generate the response
         response = await self._model_client.create(messages, cancellation_token=cancellation_token)
