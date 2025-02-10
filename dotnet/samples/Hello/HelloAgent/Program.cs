eaf0cbd92c18e14861d5bf863a94b6f8adfc372e
Ryan Sweet <rysweet@microsoft.com>
2025-02-10 13:44:26 -0800

    bringing back grpc to helloagent

--- dotnet/samples/Hello/HelloAgent/Program.cs
+++ dotnet/samples/Hello/HelloAgent/Program.cs
@@ -3,12 +3,19 @@
 
 using Microsoft.AutoGen.Contracts;
 using Microsoft.AutoGen.Core;
+using Microsoft.AutoGen.Core.Grpc;
 using Samples;
-
-// Set up app builder for in-process runtime, allow message delivery to self, and add the Hello agent
-AgentsAppBuilder appBuilder = new AgentsAppBuilder()
-    .UseInProcessRuntime(deliverToSelf: true)
-    .AddAgent<HelloAgent>("HelloAgent");
+var appBuilder = new AgentsAppBuilder(); // Create app builder
+// if we are using distributed, we need the AGENT_HOST var defined and then we will use the grpc runtime
+if (Environment.GetEnvironmentVariable("AGENT_HOST") != null)
+{
+    appBuilder.AddGrpcAgentWorker(Environment.GetEnvironmentVariable("AGENT_HOST"));
+}
+else
+{
+    // Set up app builder for in-process runtime, allow message delivery to self, and add the Hello agent
+    appBuilder.UseInProcessRuntime(deliverToSelf: true).AddAgent<HelloAgent>("HelloAgent");
+}
 var app = await appBuilder.BuildAsync(); // Build the app
 // Create a custom message type from proto and define message
 NewMessageReceived message = new NewMessageReceived { Message = "Hello World!" };
