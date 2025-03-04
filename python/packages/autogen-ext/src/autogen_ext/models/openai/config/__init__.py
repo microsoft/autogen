383962c37efc877bd602df825189252a12c41193
Avinash Mihani <avinashmihani@gmail.com>
2025-03-04 11:20:16 +0530

    Added http_client option to the BaseOpenAIClientConfiguration class
    To support SSL verification option in model_client intiation.

--- python/packages/autogen-ext/src/autogen_ext/models/openai/config/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/models/openai/config/__init__.py
@@ -43,7 +43,7 @@ class BaseOpenAIClientConfiguration(CreateArguments, total=False):
     add_name_prefixes: bool
     """What functionality the model supports, determined by default from model name but is overriden if value passed."""
     default_headers: Dict[str, str] | None
-    http_client: httpx.AsyncClient
+    http_client: httpx.AsyncClient | None = None
 
 
 # See OpenAI docs for explanation of these parameters
