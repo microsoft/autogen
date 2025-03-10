d5e549b6b4e70d018805984e75de0ac2ccc0ea95
Eric Zhu <ekzhu@users.noreply.github.com>
2025-03-10 15:51:22 -0700

    minor

--- python/packages/autogen-ext/src/autogen_ext/models/llama_cpp/_llama_cpp_completion_client.py
+++ python/packages/autogen-ext/src/autogen_ext/models/llama_cpp/_llama_cpp_completion_client.py
@@ -197,7 +197,7 @@ class LlamaCppChatCompletionClient(ChatCompletionClient):
             async def main():
                 llama_client = LlamaCppChatCompletionClient(model_path="/path/to/your/model.gguf")
                 result = await llama_client.create([UserMessage(content="What is the capital of France?", source="user")])
-                return result
+                print(result)
 
 
             asyncio.run(main())
@@ -217,7 +217,7 @@ class LlamaCppChatCompletionClient(ChatCompletionClient):
                     repo_id="unsloth/phi-4-GGUF", filename="phi-4-Q2_K_L.gguf", n_gpu_layers=-1, seed=1337, n_ctx=5000
                 )
                 result = await llama_client.create([UserMessage(content="What is the capital of France?", source="user")])
-                return result
+                print(result)
 
 
             asyncio.run(main())
