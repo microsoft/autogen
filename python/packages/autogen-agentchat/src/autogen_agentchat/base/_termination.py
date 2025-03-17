c7ff8baab5b04c51311dc48bb9b6bdd148896b2b
zhanluxianshen <zhanluxianshen@163.com>
2025-03-17 18:33:43 +0800

    code optimization
    Signed-off-by: zhanluxianshen <zhanluxianshen@163.com>

--- python/packages/autogen-agentchat/src/autogen_agentchat/base/_termination.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/base/_termination.py
@@ -157,9 +157,10 @@ class OrTerminationCondition(TerminationCondition, Component[OrTerminationCondit
         if self.terminated:
             raise RuntimeError("Termination condition has already been reached")
         stop_messages = await asyncio.gather(*[condition(messages) for condition in self._conditions])
-        if any(stop_message is not None for stop_message in stop_messages):
-            content = ", ".join(stop_message.content for stop_message in stop_messages if stop_message is not None)
-            source = ", ".join(stop_message.source for stop_message in stop_messages if stop_message is not None)
+        stop_messages_filter = [stop_message for stop_message in stop_messages if stop_message is not None]
+        if len(stop_messages_filter)>0:
+            content = ", ".join(stop_message.content for stop_message in stop_messages_filter)
+            source = ", ".join(stop_message.source for stop_message in stop_messages_filter)
             return StopMessage(content=content, source=source)
         return None
 
