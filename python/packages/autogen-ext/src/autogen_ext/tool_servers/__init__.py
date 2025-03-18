e83a10912cc850d65c796bb88ab4b098ef2eb337
Peter Jausovec <peter.jausovec@solo.io>
2025-03-18 17:58:16 +0100

    wip
    Signed-off-by: Peter Jausovec <peter.jausovec@solo.io>

--- python/packages/autogen-ext/src/autogen_ext/tool_servers/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/tool_servers/__init__.py
@@ -1,5 +1,3 @@
 from ._tool_server import ToolServer
-from ._stdiomcptoolserver import StdioMcpToolServerConfig, StdioMcpToolServer
-from ._ssemcptoolserver import SseMcpToolServerConfig, SseMcpToolServer
 
-__all__ = ["ToolServer", "StdioMcpToolServerConfig", "StdioMcpToolServer", "SseMcpToolServerConfig", "SseMcpToolServer"]
+__all__ = ["ToolServer"]
