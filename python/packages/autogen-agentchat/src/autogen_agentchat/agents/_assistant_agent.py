0caa9cd8e9abc61624c42e23ff473bf53b8ad313
Kurok1 <khanc.dev@gmail.com>
2025-03-23 11:12:54 +0800

    take the output of the tool and use that to create the HandoffMessage.content.

--- python/packages/autogen-agentchat/src/autogen_agentchat/agents/_assistant_agent.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/agents/_assistant_agent.py
@@ -981,11 +981,11 @@ class AssistantAgent(BaseChatAgent, Component[AssistantAgentConfig]):
 
     @staticmethod
     def _check_and_handle_handoff(
-        model_result: CreateResult,
-        executed_calls_and_results: List[Tuple[FunctionCall, FunctionExecutionResult]],
-        inner_messages: List[AgentEvent | ChatMessage],
-        handoffs: Dict[str, HandoffBase],
-        agent_name: str,
+            model_result: CreateResult,
+            executed_calls_and_results: List[Tuple[FunctionCall, FunctionExecutionResult]],
+            inner_messages: List[AgentEvent | ChatMessage],
+            handoffs: Dict[str, HandoffBase],
+            agent_name: str,
     ) -> Optional[Response]:
         """
         Detect handoff calls, generate the HandoffMessage if needed, and return a Response.
@@ -1011,10 +1011,14 @@ class AssistantAgent(BaseChatAgent, Component[AssistantAgentConfig]):
             # Collect normal tool calls (not handoff) into the handoff context
             tool_calls: List[FunctionCall] = []
             tool_call_results: List[FunctionExecutionResult] = []
+            # Collect the results returned by handoff_tool. By default, the message attribute will returned.
+            selected_handoff_message = selected_handoff.message
             for exec_call, exec_result in executed_calls_and_results:
                 if exec_call.name not in handoffs:
                     tool_calls.append(exec_call)
                     tool_call_results.append(exec_result)
+                elif exec_call.name == selected_handoff.name:
+                    selected_handoff_message = exec_result.content
 
             handoff_context: List[LLMMessage] = []
             if len(tool_calls) > 0:
@@ -1031,7 +1035,7 @@ class AssistantAgent(BaseChatAgent, Component[AssistantAgentConfig]):
             # Return response for the first handoff
             return Response(
                 chat_message=HandoffMessage(
-                    content=selected_handoff.message,
+                    content=selected_handoff_message,
                     target=selected_handoff.target,
                     source=agent_name,
                     context=handoff_context,
