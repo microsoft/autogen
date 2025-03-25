260f0ed42f502417e9a9c1255a61422490fd984c
abhinav-aegis <abhinav@aegis-enterprise.com>
2025-03-25 20:41:01 +0000

    Utils init to export Json to Pydantic Class

--- python/packages/autogen-agentchat/src/autogen_agentchat/utils/__init__.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/utils/__init__.py
@@ -2,6 +2,7 @@
 This module implements various utilities common to AgentChat agents and teams.
 """
 
+from ._structured_message_utils import JSONSchemaToPydantic
 from ._utils import content_to_str, remove_images
 
-__all__ = ["content_to_str", "remove_images"]
+__all__ = ["content_to_str", "remove_images", "JSONSchemaToPydantic"]
