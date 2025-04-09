344aabc3df0a74e791f4b48fa9b5376096c680d1
LuluZhu <luluzhu2004@gmail.com>
2025-04-09 14:59:41 -0400

    edit message_handler

--- python/samples/core_chainlit/app_group.py
+++ python/samples/core_chainlit/app_group.py
@@ -25,16 +25,15 @@ from autogen_core.models import (
     FunctionExecutionResult,
     FunctionExecutionResultMessage
 )
+from pydantic import BaseModel
 from autogen_core.tools import FunctionTool, Tool
 from autogen_ext.models.openai import OpenAIChatCompletionClient
 
 from autogen_core.model_context import BufferedChatCompletionContext
 from autogen_core.models import AssistantMessage, ChatCompletionClient, SystemMessage, UserMessage
 
-@dataclass
-class Message:
-    content: str
-    source: str
+class GroupChatMessage(BaseModel):
+    body: UserMessage
 
 class WeatherAgent(RoutedAgent):
     def __init__(self, model_client: ChatCompletionClient, tool_schema: List[Tool]) -> None:
@@ -45,47 +44,13 @@ class WeatherAgent(RoutedAgent):
             self._model_context = BufferedChatCompletionContext(buffer_size=5)
 
     @message_handler
-    async def handle_user_message(self, message: Message, ctx: MessageContext) -> Message:
-        # Create a session of messages.
-        session: List[LLMMessage] = self._system_messages + [UserMessage(content=message.content, source="user")]
-
-        # Add message to model context.
-        await self._model_context.add_message(UserMessage(content=message.content, source="user"))
-
-        # Run the chat completion with the tools.
-        create_result = await self._model_client.create(
-            messages=session,
-            tools=self._tools,
-            cancellation_token=ctx.cancellation_token,
+    async def handle_message(self, message: GroupChatMessage, ctx: MessageContext) -> None:
+        self._chat_history.extend(
+            [
+                UserMessage(content=f"Transferred to {message.body.source}", source="system"),
+                message.body,
+            ]
         )
-
-        # If there are no tool calls, return the result.
-        if isinstance(create_result.content, str):
-            return Message(content=create_result.content)
-        assert isinstance(create_result.content, list) and all(
-            isinstance(call, FunctionCall) for call in create_result.content
-        )
-
-        # Add the first model create result to the session.
-        session.append(AssistantMessage(content=create_result.content, source="assistant"))
-
-        # Execute the tool calls.
-        results = await asyncio.gather(
-            *[self._execute_tool_call(call, ctx.cancellation_token) for call in create_result.content]
-        )
-
-        # Add the function execution results to the session.
-        session.append(FunctionExecutionResultMessage(content=results))
-
-        # Run the chat completion again to reflect on the history and function execution results.
-        create_result = await self._model_client.create(
-            messages=session,
-            cancellation_token=ctx.cancellation_token,
-        )
-        assert isinstance(create_result.content, str)
-
-        # Return the result as a message.
-        return Message(content=create_result.content)
 
 
 
@@ -97,47 +62,14 @@ class FoodAgent(RoutedAgent):
             self._tools = tool_schema
             self._model_context = BufferedChatCompletionContext(buffer_size=5)
     @message_handler
-    async def handle_user_message(self, message: Message, ctx: MessageContext) -> Message:
-        # Create a session of messages.
-        session: List[LLMMessage] = self._system_messages + [UserMessage(content=message.content, source="user")]
-
-        # Add message to model context.
-        await self._model_context.add_message(UserMessage(content=message.content, source="user"))
-
-        # Run the chat completion with the tools.
-        create_result = await self._model_client.create(
-            messages=session,
-            tools=self._tools,
-            cancellation_token=ctx.cancellation_token,
+    async def handle_message(self, message: GroupChatMessage, ctx: MessageContext) -> None:
+        self._chat_history.extend(
+            [
+                UserMessage(content=f"Transferred to {message.body.source}", source="system"),
+                message.body,
+            ]
         )
 
-        # If there are no tool calls, return the result.
-        if isinstance(create_result.content, str):
-            return Message(content=create_result.content)
-        assert isinstance(create_result.content, list) and all(
-            isinstance(call, FunctionCall) for call in create_result.content
-        )
-
-        # Add the first model create result to the session.
-        session.append(AssistantMessage(content=create_result.content, source="assistant"))
-
-        # Execute the tool calls.
-        results = await asyncio.gather(
-            *[self._execute_tool_call(call, ctx.cancellation_token) for call in create_result.content]
-        )
-
-        # Add the function execution results to the session.
-        session.append(FunctionExecutionResultMessage(content=results))
-
-        # Run the chat completion again to reflect on the history and function execution results.
-        create_result = await self._model_client.create(
-            messages=session,
-            cancellation_token=ctx.cancellation_token,
-        )
-        assert isinstance(create_result.content, str)
-
-        # Return the result as a message.
-        return Message(content=create_result.content)
 
 class DateAgent(RoutedAgent):
     def __init__(self, model_client: ChatCompletionClient, tool_schema: List[Tool]) -> None:
