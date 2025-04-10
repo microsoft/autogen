1612675f9d7203c5f3a33440df285d9bd4d7a1f8
Ricky Loynd <riloynd@microsoft.com>
2025-04-10 15:33:33 -0700

    format fix

--- python/packages/autogen-ext/src/autogen_ext/experimental/task_centric_memory/utils/__init__.py
+++ python/packages/autogen-ext/src/autogen_ext/experimental/task_centric_memory/utils/__init__.py
@@ -4,4 +4,12 @@ from .grader import Grader
 from .page_logger import PageLogger, PageLoggerConfig
 from .teachability import Teachability
 
-__all__ = ["Apprentice", "ChatCompletionClientRecorder", "Grader", "PageLogger", "Teachability", "ApprenticeConfig", "PageLoggerConfig"]
+__all__ = [
+    "Apprentice",
+    "ChatCompletionClientRecorder",
+    "Grader",
+    "PageLogger",
+    "Teachability",
+    "ApprenticeConfig",
+    "PageLoggerConfig",
+]
