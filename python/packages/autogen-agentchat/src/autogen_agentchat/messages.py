db125fbd2d4287ae562cf161b3fcdf402a753d0c
Sungjun.Kim <kimsungjun@linecorp.com>
2025-05-23 14:29:24 +0900

    Add created_at to BaseChatMessage and BaseAgentEvent (#6557)
    ## Why are these changes needed?
    
    I added `created_at` to both BaseChatMessage and BaseAgentEvent classes
    that store the time these Pydantic model instances are generated. And
    then users will be able to use `created_at` to build up a customized
    external persisting state management layer for their case.
    
    ## Related issue number
    
    
    https://github.com/microsoft/autogen/discussions/6169#discussioncomment-13151540
    
    ## Checks
    
    - [x] I've included any doc changes needed for
    <https://microsoft.github.io/autogen/>. See
    <https://github.com/microsoft/autogen/blob/main/CONTRIBUTING.md> to
    build and test documentation locally.
    - [x] I've added tests (if relevant) corresponding to the changes
    introduced in this PR.
    - [x] I've made sure all auto checks have passed.
    
    ---------
    
    Co-authored-by: Jack Gerrits <jackgerrits@users.noreply.github.com>
    Co-authored-by: Eric Zhu <ekzhu@users.noreply.github.com>

--- python/packages/autogen-agentchat/src/autogen_agentchat/messages.py
+++ python/packages/autogen-agentchat/src/autogen_agentchat/messages.py
@@ -5,6 +5,7 @@ class and includes specific fields relevant to the type of message being sent.
 """
 
 from abc import ABC, abstractmethod
+from datetime import datetime, timezone
 from typing import Any, Dict, Generic, List, Literal, Mapping, Optional, Type, TypeVar
 
 from autogen_core import Component, ComponentBase, FunctionCall, Image
@@ -85,6 +86,9 @@ class BaseChatMessage(BaseMessage, ABC):
     metadata: Dict[str, str] = {}
     """Additional metadata about the message."""
 
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    """The time when the message was created."""
+
     @abstractmethod
     def to_model_text(self) -> str:
         """Convert the content of the message to text-only representation.
@@ -153,6 +157,9 @@ class BaseAgentEvent(BaseMessage, ABC):
 
     metadata: Dict[str, str] = {}
     """Additional metadata about the message."""
+
+    created_at: datetime = Field(default_factory=lambda: datetime.now(timezone.utc))
+    """The time when the message was created."""
 
 
 StructuredContentType = TypeVar("StructuredContentType", bound=BaseModel, covariant=True)
