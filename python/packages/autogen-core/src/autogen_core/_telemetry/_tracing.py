a3b1e38138ba3e97ee1cc69528fb42a6dbaeae90
Eitan Yarmush <eitan.yarmush@solo.io>
2025-06-18 12:43:56 +0000

    mypy
    Signed-off-by: Eitan Yarmush <eitan.yarmush@solo.io>

--- python/packages/autogen-core/src/autogen_core/_telemetry/_tracing.py
+++ python/packages/autogen-core/src/autogen_core/_telemetry/_tracing.py
@@ -27,7 +27,7 @@ class TraceHelper(Generic[Operation, Destination, ExtraAttributes]):
 
         disable_runtime_tracing = os.environ.get("AUTOGEN_DISABLE_RUNTIME_TRACING") == "true"
         if disable_runtime_tracing:
-            self.tracer_provider = NoOpTracerProvider()
+            self.tracer_provider: TracerProvider = NoOpTracerProvider()
             self.tracer = self.tracer_provider.get_tracer(f"autogen {instrumentation_builder_config.name}")
             return
 
