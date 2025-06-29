5cead801e8ed01587891222f0f9e8195c0827751
Eric Zhu <ekzhu@users.noreply.github.com>
2025-06-30 00:15:38 +0900

    fix import

--- python/packages/autogen-core/src/autogen_core/memory/_list_memory.py
+++ python/packages/autogen-core/src/autogen_core/memory/_list_memory.py
@@ -1,6 +1,6 @@
 from typing import Any, List
 
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 from typing_extensions import Self
 
 from .._cancellation_token import CancellationToken
