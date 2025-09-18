f09bac3afa826afa15ae7306dc35a9f58d6125fa
Eric Zhu <ekzhu@users.noreply.github.com>
2025-09-17 18:03:26 -0700

    fix type

--- python/packages/autogen-ext/src/autogen_ext/models/openai/_openai_client.py
+++ python/packages/autogen-ext/src/autogen_ext/models/openai/_openai_client.py
@@ -397,13 +397,13 @@ def count_tokens_openai(
                             tool_tokens -= 3
                             for o in v["anyOf"]:  # type: ignore
                                 tool_tokens += 3
-                                tool_tokens += len(encoding.encode(str(o["type"])))
+                                tool_tokens += len(encoding.encode(str(o["type"])))  # pyright: ignore
                         elif field == "default":
                             tool_tokens += 2
                             tool_tokens += len(encoding.encode(json.dumps(v["default"])))
                         elif field == "title":
                             tool_tokens += 2
-                            tool_tokens += len(encoding.encode(str(v["title"])))
+                            tool_tokens += len(encoding.encode(str(v["title"])))  # pyright: ignore
                         elif field == "enum":
                             tool_tokens -= 3
                             for o in v["enum"]:  # pyright: ignore
