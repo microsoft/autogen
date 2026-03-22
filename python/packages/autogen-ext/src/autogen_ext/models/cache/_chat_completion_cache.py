b0689bc816d1e2aa21427e36669ac2dc71217153
hshgogogo <2081920847@qq.com>
2026-03-22 18:21:15 +0800

    docs: fix RedisStore class name in cache examples

--- python/packages/autogen-ext/src/autogen_ext/models/cache/_chat_completion_cache.py
+++ python/packages/autogen-ext/src/autogen_ext/models/cache/_chat_completion_cache.py
@@ -65,7 +65,7 @@ class ChatCompletionCache(ChatCompletionClient, Component[ChatCompletionCacheCon
                 # from autogen_ext.cache_store.redis import RedisStore
                 # import redis
                 # redis_instance = redis.Redis()
-                # cache_store = RedisCacheStore[CHAT_CACHE_VALUE_TYPE](redis_instance)
+                # cache_store = RedisStore[CHAT_CACHE_VALUE_TYPE](redis_instance)
                 cache_store = DiskCacheStore[CHAT_CACHE_VALUE_TYPE](Cache(tmpdirname))
                 cache_client = ChatCompletionCache(openai_model_client, cache_store)
 
